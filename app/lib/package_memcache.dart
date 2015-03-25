d287e3322b19a5eb1148b6149edca9dab5810b92
Martin Kustermann <kustermann@google.com>
2015-03-25 14:10:11 +0100

    Ignore memcache API call errors [but do log them]
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org/1021793003

--- app/lib/package_memcache.dart
+++ app/lib/package_memcache.dart
@@ -25,7 +25,8 @@ class AppEnginePackageMemcache implements PackageCache {
           ? KEY_PREFIX : 'ns_${namespace}_$KEY_PREFIX';
 
   Future<List<int>> getPackageData(String package) async {
-    var result = await memcache.get(_packageKey(package), asBinary: true);
+    var result =
+        await _ignoreErrors(memcache.get(_packageKey(package), asBinary: true));
 
     if (result != null) _logger.info('memcache["$package"] found');
     else _logger.info('memcache["$package"] not found');
@@ -35,17 +36,31 @@ class AppEnginePackageMemcache implements PackageCache {
 
   Future setPackageData(String package, List<int> data) {
     _logger.info('memcache["$package"] setting to new data');
-    return memcache.set(_packageKey(package), data, expiration: EXPIRATION);
+    return _ignoreErrors(
+        memcache.set(_packageKey(package), data, expiration: EXPIRATION));
   }
 
   Future invalidatePackageData(String package) {
     _logger.info('memcache["$package"] invalidate entry');
-    return Future.wait([
+    return _ignoreErrors(Future.wait([
         // TODO: Once the Python version is retired, we can remove this.
         memcache.remove('package_json_$package'),
         memcache.remove(_packageKey(package)),
-    ]);
+    ]));
   }
 
   String _packageKey(String package) => '$keyPrefix$package';
+
+  // We are ignoring any memcache errors and just return `null` in this case.
+  //
+  // NOTE: The worst what can happen is that up to `EXPIRATION` time passes
+  // before a value gets automatically evicted from memcache
+  //    => The duration for inconsistency is limited to 60 minutes ATM.
+  Future _ignoreErrors(Future f) {
+    return f.catchError((error, stackTrace) {
+      _logger.warning(
+          'Ignoring failed memcache API call (error: $error)',
+          error, stackTrace);
+    });
+  }
 }
