da527f3b13c5019b008728b40eb0648ab8c9f91a
Kevin Moore <kevmoo@google.com>
2017-08-23 09:47:21 -0700

    add copyright to mock_scores.dart

--- app/lib/shared/mock_scores.dart
+++ app/lib/shared/mock_scores.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 library pub_dartlang_org.shared.mock_scores;
 
 import 'package:json_serializable/annotations.dart';
