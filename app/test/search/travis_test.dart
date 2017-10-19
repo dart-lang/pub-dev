bf7e977b7f0635086f959c05a43a466f6d1610da
István Soós <isoos@users.noreply.github.com>
2017-10-19 18:20:57 +0200

    Baseline test for tuning 'travis' search. #428 (#450)

---
+++ app/test/search/travis_test.dart
@@ -0,0 +1,93 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:test/test.dart';
+
+import 'package:pub_dartlang_org/search/index_simple.dart';
+import 'package:pub_dartlang_org/search/text_utils.dart';
+import 'package:pub_dartlang_org/shared/search_service.dart';
+
+void main() {
+  group('travis', () {
+    SimplePackageIndex index;
+
+    setUpAll(() async {
+      index = new SimplePackageIndex();
+      final packageNames = [
+        'rainbow_vis',
+        'mongo_dart_query',
+        'angular_aria',
+        'dartemis_transformer',
+        'w_transport',
+        'sass_transformer',
+      ];
+      await index.addPackage(new PackageDocument(
+        package: 'travis',
+        version: '0.0.1-dev',
+        description: compactDescription(
+            'A starting point for Dart libraries or applications.'),
+      ));
+      for (String packageName in packageNames) {
+        await index.addPackage(new PackageDocument(
+          package: packageName,
+          version: '1.0.0',
+          description:
+              compactDescription('$packageName a package for $packageName'),
+          readme: compactReadme('''
+# $packageName
+
+[![Pub](https://img.shields.io/pub/v/$packageName.svg?maxAge=2592000?style=flat-square)](https://pub.dartlang.org/packages/$packageName)
+[![Travis](https://img.shields.io/travis/$packageName/$packageName.svg?maxAge=2592000?style=flat-square)](https://travis-ci.org/$packageName/$packageName)
+
+## Usage
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+'''),
+        ));
+      }
+      await index.merge();
+    });
+
+    test('travis', () async {
+      final PackageSearchResult result = await index
+          .search(new SearchQuery('travis', order: SearchOrder.text));
+      expect(JSON.decode(JSON.encode(result)), {
+        'indexUpdated': isNotNull,
+        'totalCount': 7,
+        'packages': [
+          {
+            'package': 'travis',
+            'score': closeTo(60.0, 0.1),
+          },
+          {
+            'package': 'rainbow_vis',
+            'score': closeTo(11.3, 0.1),
+          },
+          {
+            'package': 'dartemis_transformer',
+            'score': closeTo(11.1, 0.1),
+          },
+          {
+            'package': 'w_transport',
+            'score': closeTo(11.0, 0.1),
+          },
+          {
+            'package': 'sass_transformer',
+            'score': closeTo(11.0, 0.1),
+          },
+          {
+            'package': 'mongo_dart_query',
+            'score': closeTo(10.2, 0.1),
+          },
+          {
+            'package': 'angular_aria',
+            'score': closeTo(10.2, 0.1),
+          },
+        ],
+      });
+    });
+  });
+}
