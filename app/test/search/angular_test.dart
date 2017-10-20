2eaf5dde087c16718d8fd4c092ebbe0cbfefed7a
István Soós <isoos@users.noreply.github.com>
2017-10-20 11:06:24 +0200

    Baseline test for tuning 'angular' search. #451. (#452)

---
+++ app/test/search/angular_test.dart
@@ -0,0 +1,51 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:test/test.dart';
+
+import 'package:pub_dartlang_org/search/index_simple.dart';
+import 'package:pub_dartlang_org/search/text_utils.dart';
+import 'package:pub_dartlang_org/shared/search_service.dart';
+
+void main() {
+  group('angular', () {
+    SimplePackageIndex index;
+
+    setUpAll(() async {
+      index = new SimplePackageIndex();
+      await index.addPackage(new PackageDocument(
+        package: 'angular',
+        version: '4.0.0',
+        description: compactDescription('Fast and productive web framework.'),
+      ));
+      await index.addPackage(new PackageDocument(
+        package: 'angular_ui',
+        version: '0.6.5',
+        description: compactDescription('Port of Angular-UI to Dart.'),
+      ));
+      await index.merge();
+    });
+
+    test('angular', () async {
+      final PackageSearchResult result = await index
+          .search(new SearchQuery('angular', order: SearchOrder.text));
+      expect(JSON.decode(JSON.encode(result)), {
+        'indexUpdated': isNotNull,
+        'totalCount': 2,
+        'packages': [
+          {
+            'package': 'angular_ui',
+            'score': closeTo(76.6, 0.1),
+          },
+          {
+            'package': 'angular',
+            'score': closeTo(60.0, 0.1),
+          },
+        ],
+      });
+    });
+  });
+}
