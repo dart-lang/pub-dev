0be4522f141f27b47f1d48244ebf1ac1bafbcbcc
István Soós <isoos@users.noreply.github.com>
2017-10-24 17:28:15 +0200

    Upgrade Flutter to 0.0.17 (#459)

--- app/lib/analyzer/versions.dart
+++ app/lib/analyzer/versions.dart
@@ -9,5 +9,5 @@ final String panaVersion = '0.5.0';
 final Version semanticPanaVersion = new Version.parse(panaVersion);
 
 // keep in-sync with app/script/setup-flutter.sh
-final String flutterVersion = '0.0.13';
+final String flutterVersion = '0.0.17';
 final Version semanticFlutterVersion = new Version.parse(flutterVersion);
