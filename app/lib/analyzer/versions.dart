9a1303eaf208d8adcd0d88d333d9191807963fb3
Kevin Moore <kevmoo@google.com>
2017-10-28 13:19:05 -0700

    latest version of pana

--- app/lib/analyzer/versions.dart
+++ app/lib/analyzer/versions.dart
@@ -5,7 +5,7 @@
 import 'package:pub_semver/pub_semver.dart';
 
 // keep in-sync with app/pubspec.yaml
-final String panaVersion = '0.5.0';
+final String panaVersion = '0.5.1';
 final Version semanticPanaVersion = new Version.parse(panaVersion);
 
 // keep in-sync with app/script/setup-flutter.sh
