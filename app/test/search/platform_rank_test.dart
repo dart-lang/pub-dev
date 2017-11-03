886ad840ffb9ad04977a8e0d0d066a12387fdf68
István Soós <isoos@users.noreply.github.com>
2017-11-03 19:53:59 +0100

    Rank by platform specificity. Fixes #461. (#515)

---
+++ app/test/search/platform_rank_test.dart
@@ -0,0 +1,90 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:test/test.dart';
+
+import 'package:pub_dartlang_org/search/index_simple.dart';
+import 'package:pub_dartlang_org/shared/platform.dart';
+import 'package:pub_dartlang_org/shared/search_service.dart';
+
+void main() {
+  group('platform rank', () {
+    SimplePackageIndex index;
+
+    setUpAll(() async {
+      index = new SimplePackageIndex();
+      await index.addPackage(new PackageDocument(
+        package: 'json_0',
+      ));
+      await index.addPackage(new PackageDocument(
+        package: 'json_1',
+        platforms: ['flutter'],
+      ));
+      await index.addPackage(new PackageDocument(
+        package: 'json_2',
+        platforms: ['flutter', 'server'],
+      ));
+      await index.addPackage(new PackageDocument(
+        package: 'json_3',
+        platforms: KnownPlatforms.all,
+      ));
+      await index.merge();
+    });
+
+    test('text search without platform', () async {
+      final PackageSearchResult withoutPlatform =
+          await index.search(new SearchQuery('json'));
+      expect(JSON.decode(JSON.encode(withoutPlatform)), {
+        'indexUpdated': isNotNull,
+        'totalCount': 4,
+        'packages': [
+          {
+            'package': 'json_0',
+            'score': closeTo(33.75, 0.01),
+          },
+          {
+            'package': 'json_1',
+            'score': closeTo(33.75, 0.01),
+          },
+          {
+            'package': 'json_2',
+            'score': closeTo(33.75, 0.01),
+          },
+          {
+            'package': 'json_3',
+            'score': closeTo(33.75, 0.01),
+          },
+        ],
+      });
+    });
+
+    test('text search with platform', () async {
+      final PackageSearchResult withPlatform =
+          await index.search(new SearchQuery(
+        'json',
+        platformPredicate: new PlatformPredicate(required: ['flutter']),
+      ));
+      expect(JSON.decode(JSON.encode(withPlatform)), {
+        'indexUpdated': isNotNull,
+        'totalCount': 3,
+        'packages': [
+          {
+            'package': 'json_1',
+            'score': closeTo(33.75, 0.01),
+          },
+          {
+            'package': 'json_2',
+            'score': closeTo(30.38, 0.01),
+          },
+          {
+            'package': 'json_3',
+            'score': closeTo(27.00, 0.01),
+          },
+        ],
+      });
+    });
+  });
+}
