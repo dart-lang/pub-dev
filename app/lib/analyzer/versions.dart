5f1035f2fb8f2cda93084d7a15dbc95b0df233f9
Istvan Soos <istvan.soos@gmail.com>
2017-11-28 00:11:58 +0100

    Upgrade to pana 0.7.1

--- app/lib/analyzer/versions.dart
+++ app/lib/analyzer/versions.dart
@@ -5,7 +5,7 @@
 import 'package:pub_semver/pub_semver.dart';
 
 // keep in-sync with app/pubspec.yaml
-final String panaVersion = '0.7.0+1';
+final String panaVersion = '0.7.1';
 final Version semanticPanaVersion = new Version.parse(panaVersion);
 
 // keep in-sync with app/script/setup-flutter.sh
