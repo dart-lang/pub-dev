77efc6507d2e9d1aa702fe20f3ec8478ef542d02
István Soós <isoos@users.noreply.github.com>
2017-12-07 16:36:58 +0100

    Format platform names on analysis tab too. Fixes #754 (#759)

--- app/lib/frontend/templates.dart
+++ app/lib/frontend/templates.dart
@@ -185,7 +185,10 @@ class TemplateService {
       'dart_sdk_version': analysis.dartSdkVersion,
       'pana_version': analysis.panaVersion,
       'flutter_version': analysis.flutterVersion,
-      'platforms_html': analysis.platforms?.join(', ') ?? '<i>unsure</i>',
+      'platforms_html': analysis.platforms
+              ?.map((p) => getPlatformDict(p, nullIfMissing: true)?.name ?? p)
+              ?.join(', ') ??
+          '<i>unsure</i>',
       'platforms_reason_html': markdownToHtml(analysis.platformsReason, null),
       'hasSuggestions': suggestions != null && suggestions.isNotEmpty,
       'suggestions': suggestions,
