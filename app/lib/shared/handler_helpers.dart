f6ec2beab09db95860321e5eeb127495e79dd781
Istvan Soos <istvan.soos@gmail.com>
2017-12-15 11:40:24 +0100

    Log all requests in services.

--- app/lib/shared/handler_helpers.dart
+++ app/lib/shared/handler_helpers.dart
@@ -6,12 +6,30 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:appengine/appengine.dart';
+import 'package:logging/logging.dart';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
-Future runHandler(shelf.Handler handler, {bool shared: false}) =>
-    runAppEngine((HttpRequest request) => handleRequest(request, handler),
-        shared: shared ?? false);
+Future runHandler(Logger logger, shelf.Handler handler, {bool shared: false}) {
+  final wrappedHandler = _logRequestWrapper(logger, handler);
+  return runAppEngine(
+      (HttpRequest request) => handleRequest(request, wrappedHandler),
+      shared: shared ?? false);
+}
 
 Future handleRequest(HttpRequest request, shelf.Handler handler) =>
     shelf_io.handleRequest(request, handler);
+
+shelf.Handler _logRequestWrapper(Logger logger, shelf.Handler handler) {
+  return (shelf.Request request) async {
+    logger.info('Handling request: ${request.requestedUri}');
+    try {
+      return await handler(request);
+    } catch (error, st) {
+      logger.severe('Request handler failed', error, st);
+      return new shelf.Response.internalServerError();
+    } finally {
+      logger.info('Request handler done.');
+    }
+  };
+}
