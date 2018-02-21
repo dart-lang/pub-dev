8b63c60eb3dbc17451425dcb8236aba4f4492347
Istvan Soos <istvan.soos@gmail.com>
2018-02-21 10:07:12 +0100

    Customization skeleton.

---
+++ app/lib/dartdoc/customization.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+
+class DartdocCustomizer {
+  final String packageName;
+  final String packageVersion;
+
+  DartdocCustomizer(this.packageName, this.packageVersion);
+
+  Future<bool> customizeFile(File file) async {
+    final String oldContent = await file.readAsString();
+    final String newContent = customizeHtml(oldContent);
+    if (newContent != null && oldContent != newContent) {
+      await file.writeAsString(newContent);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  String customizeHtml(String html) {
+    return null;
+  }
+}
+
