3f5fc2280acd6b90135e1629f9b9ca5fbc8689c3
Istvan Soos <istvan.soos@gmail.com>
2018-02-22 20:29:25 +0100

    Add noindex meta header for discontinued packages.

--- app/test/frontend/handlers_test_utils.dart
+++ app/test/frontend/handlers_test_utils.dart
@@ -183,6 +183,7 @@ class TemplateMock implements TemplateService {
     String platform,
     SearchQuery searchQuery,
     bool includeSurvey: true,
+    bool noIndex: false,
   }) =>
       _response;
 
