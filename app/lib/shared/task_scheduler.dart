8e5735f71816a8b3b89ff3ae37b5c913f56e56d1
István Soós <isoos@users.noreply.github.com>
2018-06-15 10:50:23 +0200

    Remove PrioritizedStreamIterator (#1373)

--- app/lib/shared/task_scheduler.dart
+++ app/lib/shared/task_scheduler.dart
@@ -3,9 +3,9 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:collection';
 
 import 'package:logging/logging.dart';
+import 'package:pool/pool.dart';
 
 import 'utils.dart';
 
@@ -50,14 +50,7 @@ class TaskScheduler {
   TaskScheduler(this.taskRunner, this.sources);
 
   Future run() async {
-    final PrioritizedStreamIterator<Task> taskIterator =
-        new PrioritizedStreamIterator(
-      sources.map((TaskSource ts) => ts.startStreaming()).toList(),
-      deduplicateWaiting: true,
-    );
-    while (await taskIterator.moveNext()) {
-      final Task task = taskIterator.current;
-      _pendingCount = taskIterator.pendingCount;
+    Future runTask(Task task) async {
       final Stopwatch sw = new Stopwatch()..start();
       try {
         TaskTargetStatus status;
@@ -69,7 +62,7 @@ class TaskScheduler {
           _logger.info('Skipping scheduled task: $task: ${status.reason}.');
           _statusTracker.add('skip');
           _allLatencyTracker.add(sw.elapsedMilliseconds);
-          continue;
+          return;
         }
         final bool raceDetected = await taskRunner.runTask(task);
         _statusTracker.add(raceDetected ? 'race' : 'normal');
@@ -80,6 +73,22 @@ class TaskScheduler {
       _workLatencyTracker.add(sw.elapsedMilliseconds);
       _allLatencyTracker.add(sw.elapsedMilliseconds);
     }
+
+    final Pool pool = new Pool(1);
+    final futures = sources
+        .map((TaskSource ts) {
+          final stream = ts.startStreaming();
+          return stream.listen((task) {
+            _pendingCount++;
+            final f = pool.withResource(() => runTask(task));
+            f.whenComplete(() {
+              _pendingCount--;
+            });
+          });
+        })
+        .map((subscription) => subscription.asFuture())
+        .toList();
+    await Future.wait(futures);
   }
 
   Map stats() {
@@ -139,118 +148,4 @@ class TaskTargetStatus {
         reason = null;
 
   TaskTargetStatus.skip(this.reason) : shouldSkip = true;
-}
-
-/// A pull-based interface for accessing events from multiple streams, in the
-/// priority order of the streams provided.
-class PrioritizedStreamIterator<T> implements StreamIterator<T> {
-  final bool deduplicateWaiting;
-  List<Set<T>> _priorityQueues;
-  List<StreamSubscription> _subscriptions;
-  bool _hasMoved = false;
-  bool _isClosed = false;
-  T _current;
-  Completer<bool> _hasNextCompleter;
-
-  PrioritizedStreamIterator(List<Stream<T>> sources,
-      {this.deduplicateWaiting: true}) {
-    _priorityQueues =
-        new List.generate(sources.length, (_) => new LinkedHashSet());
-    _subscriptions = new List(sources.length);
-
-    // Listen on the streams and put items into their own queues.
-    for (int i = 0; i < sources.length; i++) {
-      final Stream<T> source = sources[i];
-      final Set<T> queue = _priorityQueues[i];
-      _subscriptions[i] = source.listen(
-        (T item) {
-          if (!deduplicateWaiting || !queue.contains(item)) {
-            queue.add(item);
-            _triggerComplete();
-          }
-        },
-        onDone: () {
-          _subscriptions[i] = null;
-          _cancelWhenAllDone();
-        },
-        cancelOnError: true,
-      );
-    }
-  }
-
-  /// The number of pending items in the queues.
-  int get pendingCount =>
-      _priorityQueues.fold(0, (int sum, Set queue) => sum + queue.length);
-
-  /// Moves to the next element.
-  /// Returns whether the iterator has another item.
-  @override
-  Future<bool> moveNext() async {
-    if (_hasNextCompleter != null) {
-      throw new StateError('Another moveNext() is underway.');
-    }
-    if (_isClosed) return false;
-    final Set<T> queue = _firstQueue();
-    if (queue != null) {
-      _current = queue.first;
-      queue.remove(_current);
-      _hasMoved = true;
-      return true;
-    } else {
-      _hasNextCompleter ??= new Completer();
-      _cancelWhenAllDone();
-      return _hasNextCompleter.future;
-    }
-  }
-
-  // The current element in the iterator.
-  @override
-  T get current {
-    if (_isClosed) throw new StateError('StreamIterator closed.');
-    if (!_hasMoved) throw new StateError('moveNext() has not been called.');
-    return _current;
-  }
-
-  /// Close the source streams and don't accept new requests.
-  @override
-  Future cancel() async {
-    if (_hasNextCompleter != null) {
-      _hasNextCompleter.complete(false);
-      _hasNextCompleter = null;
-    }
-    for (int i = 0; i < _subscriptions.length; i++) {
-      final StreamSubscription s = _subscriptions[i];
-      if (s != null) {
-        s.cancel();
-        _subscriptions[i] = null;
-      }
-    }
-    _isClosed = true;
-  }
-
-  Set<T> _firstQueue() {
-    if (_isClosed) {
-      throw new StateError('StreamIterator closed');
-    }
-    return _priorityQueues.firstWhere((q) => q.isNotEmpty, orElse: () => null);
-  }
-
-  void _triggerComplete() {
-    if (_hasNextCompleter != null) {
-      final Set<T> queue = _firstQueue();
-      _current = queue.first;
-      queue.remove(_current);
-      _hasMoved = true;
-      _hasNextCompleter.complete(true);
-      _hasNextCompleter = null;
-    }
-    _cancelWhenAllDone();
-  }
-
-  void _cancelWhenAllDone() {
-    if (_isClosed) return;
-    if (_subscriptions.any((s) => s != null)) return;
-    if (_firstQueue() != null) return;
-    cancel();
-  }
 }
