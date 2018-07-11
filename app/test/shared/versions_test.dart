233e5d62541dc11aac0bb79ad4233d42dd8a2467
Istvan Soos <istvan.soos@gmail.com>
2018-07-11 12:48:36 +0200

    Upgrade pana to 0.11.6

--- app/test/shared/versions_test.dart
+++ app/test/shared/versions_test.dart
@@ -20,7 +20,7 @@ void main() {
       dartdocVersion,
       customizationVersion,
     ].join('//').hashCode;
-    expect(hash, 836425494);
+    expect(hash, 25323727);
   });
 
   test('sdk version should match travis and dockerfile', () async {
