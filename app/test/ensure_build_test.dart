52403397208978966a26552aa62af5eb0b02d521
Istvan Soos <istvan.soos@gmail.com>
2018-07-20 10:28:53 +0200

    Update with dev.63 dartfmt

--- app/test/ensure_build_test.dart
+++ app/test/ensure_build_test.dart
@@ -47,7 +47,8 @@ final _whitespace = new RegExp(r'\s');
 Set<String> _changedGeneratedFiles() {
   final output = _runProc('git', ['status', '--porcelain']);
 
-  return LineSplitter.split(output)
+  return LineSplitter
+      .split(output)
       .map((line) => line.split(_whitespace).last)
       .where((path) => path.endsWith('.dart'))
       .toSet();
