09107ee45a0dcfa6b38c7c77fe4b8fd53085b4a6
Istvan Soos <istvan.soos@gmail.com>
2018-09-18 09:35:37 +0200

    Store more data in PanaReport.

--- app/lib/scorecard/models.g.dart
+++ app/lib/scorecard/models.g.dart
@@ -49,6 +49,13 @@ Map<String, dynamic> _$ScoreCardDataToJson(ScoreCardData instance) =>
 
 PanaReport _$PanaReportFromJson(Map<String, dynamic> json) {
   return PanaReport(
+      timestamp: json['timestamp'] == null
+          ? null
+          : DateTime.parse(json['timestamp'] as String),
+      panaRuntimeInfo: json['panaRuntimeInfo'] == null
+          ? null
+          : PanaRuntimeInfo.fromJson(
+              json['panaRuntimeInfo'] as Map<String, dynamic>),
       reportStatus: json['reportStatus'] as String,
       healthScore: (json['healthScore'] as num)?.toDouble(),
       maintenanceScore: (json['maintenanceScore'] as num)?.toDouble(),
@@ -60,6 +67,11 @@ PanaReport _$PanaReportFromJson(Map<String, dynamic> json) {
               ? null
               : PkgDependency.fromJson(e as Map<String, dynamic>))
           ?.toList(),
+      licenses: (json['licenses'] as List)
+          ?.map((e) => e == null
+              ? null
+              : LicenseFile.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
       suggestions: (json['suggestions'] as List)
           ?.map((e) =>
               e == null ? null : Suggestion.fromJson(e as Map<String, dynamic>))
@@ -68,13 +80,16 @@ PanaReport _$PanaReportFromJson(Map<String, dynamic> json) {
 
 Map<String, dynamic> _$PanaReportToJson(PanaReport instance) =>
     <String, dynamic>{
+      'timestamp': instance.timestamp?.toIso8601String(),
+      'panaRuntimeInfo': instance.panaRuntimeInfo,
       'reportStatus': instance.reportStatus,
       'healthScore': instance.healthScore,
       'maintenanceScore': instance.maintenanceScore,
       'platformTags': instance.platformTags,
       'platformReason': instance.platformReason,
       'pkgDependencies': instance.pkgDependencies,
-      'suggestions': instance.suggestions
+      'suggestions': instance.suggestions,
+      'licenses': instance.licenses
     };
 
 DartdocReport _$DartdocReportFromJson(Map<String, dynamic> json) {
