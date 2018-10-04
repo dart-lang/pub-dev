d4311cbe20dbce639b9b032b8193ea896e2ece43
Istvan Soos <istvan.soos@gmail.com>
2018-10-04 16:31:26 +0200

    Use authorization header const

--- app/lib/frontend/auth.dart
+++ app/lib/frontend/auth.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:io' show HttpHeaders;
 
 import 'package:gcloud/service_scope.dart' as ss;
 import 'package:shelf/shelf.dart' as shelf;
@@ -18,7 +19,7 @@ String get loggedInUser => ss.lookup(#_logged_in_user) as String;
 /// Looks at [request] and if the 'Authorization' header was set tries to get
 /// the user email address and registers it.
 Future registerLoggedInUserIfPossible(shelf.Request request) async {
-  final authorization = request.headers['authorization'];
+  final authorization = request.headers[HttpHeaders.authorizationHeader];
   if (authorization != null) {
     final parts = authorization.split(' ');
     if (parts.length == 2 && parts.first.trim().toLowerCase() == 'bearer') {
