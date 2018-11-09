6749d146e2b19cfe84a619358c096cbd91835ea1
Istvan Soos <istvan.soos@gmail.com>
2018-11-09 18:27:49 +0100

    New tool: sync_staging.

---
+++ app/bin/tools/sync_staging.dart
@@ -0,0 +1,61 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:gcloud/db.dart';
+import 'package:http/http.dart';
+import 'package:pool/pool.dart';
+
+import 'package:pub_dartlang_org/frontend/models.dart';
+import 'package:pub_dartlang_org/frontend/pub_client.dart';
+import 'package:pub_dartlang_org/frontend/service_utils.dart';
+
+void main() async {
+  await withProdServices(() async {
+    final pubClient = new PubClient(new Client());
+
+    int processIdx = 0;
+    int missingPackages = 0;
+    int missingVersions = 0;
+    final pool = new Pool(4);
+    final futures = <Future>[];
+
+    final packages = await pubClient.listPackages();
+    for (String package in packages) {
+      final data = await pubClient.getPackageData(package);
+
+      final f = pool.withResource(() async {
+        final pkgKey = dbService.emptyKey.append(Package, id: package);
+        final versions = await dbService
+            .query<PackageVersion>(ancestorKey: pkgKey)
+            .run()
+            .toList();
+        final versionsExisting = versions.map((pv) => pv.version).toSet();
+        final versionsToCreate =
+            data.versions.where((v) => !versionsExisting.contains(v));
+
+        final idx = processIdx++;
+        final status = versionsToCreate.isEmpty
+            ? 'OK'
+            : 'missing ${versionsToCreate.length} versions';
+        print('Package [$idx/${packages.length}] $package: $status');
+
+        missingVersions += versionsToCreate.length;
+        if (versionsToCreate.isNotEmpty) {
+          final Package p = (await dbService.lookup([pkgKey])).single;
+          if (p == null) {
+            missingPackages++;
+          }
+          // TODO: update staging Datastore
+        }
+      });
+      futures.add(f);
+    }
+    await Future.wait(futures);
+    await pool.close();
+
+    print(
+        'Total missing: $missingPackages packages $missingVersions versions.');
+    await pubClient.close();
+  });
+}
