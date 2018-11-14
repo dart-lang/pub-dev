11da0071babef0a5487e2546e42fbfb51e91842e
István Soós <isoos@users.noreply.github.com>
2018-11-14 15:48:14 +0100

    Refactor atom feed handler. (#1778)

---
+++ app/test/frontend/handlers_atom_feed_test.dart
@@ -0,0 +1,72 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library pub_dartlang_org.handlers_test;
+
+import 'dart:async';
+
+import 'package:test/test.dart';
+
+import 'package:pub_dartlang_org/frontend/backend.dart';
+import 'package:pub_dartlang_org/frontend/templates.dart';
+
+import '../shared/utils.dart';
+
+import 'handlers_test_utils.dart';
+import 'utils.dart';
+
+void tScopedTest(String name, Future func()) {
+  scopedTest(name, () {
+    registerTemplateService(new TemplateMock());
+    return func();
+  });
+}
+
+void main() {
+  final pageSize = 10;
+
+  group('feeds', () {
+    tScopedTest('/feed.atom', () async {
+      final backend =
+          new BackendMock(latestPackageVersionsFun: ({offset, limit}) {
+        expect(offset, 0);
+        expect(limit, pageSize);
+        return [testPackageVersion];
+      });
+      registerBackend(backend);
+      await expectAtomXmlResponse(await issueGet('/feed.atom'), regexp: '''
+<\\?xml version="1.0" encoding="UTF-8"\\?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+        <id>https://pub.dartlang.org/feed.atom</id>
+        <title>Pub Packages for Dart</title>
+        <updated>(.*)</updated>
+        <author>
+          <name>Dart Team</name>
+        </author>
+        <link href="https://pub.dartlang.org/" rel="alternate" />
+        <link href="https://pub.dartlang.org/feed.atom" rel="self" />
+        <generator version="0.1.0">Pub Feed Generator</generator>
+        <subtitle>Last Updated Packages</subtitle>
+(\\s*)
+        <entry>
+          <id>urn:uuid:d4fe7eb8-fc0e-5515-a5e5-5868b339d660</id>
+          <title>v0.1.1\\+5 of foobar_pkg</title>
+          <updated>${testPackageVersion.created.toIso8601String()}</updated>
+          <author><name>Hans Juergen &lt;hans@juergen.com&gt;</name></author>
+          <content type="html">&lt;h1&gt;Test Package&lt;&#47;h1&gt;
+&lt;p&gt;This is a readme file.&lt;&#47;p&gt;
+&lt;pre&gt;&lt;code class=&quot;language-dart&quot;&gt;void main\\(\\) {
+}
+&lt;&#47;code&gt;&lt;&#47;pre&gt;
+</content>
+          <link href="https://pub.dartlang.org/packages/foobar_pkg"
+                rel="alternate"
+                title="foobar_pkg" />
+        </entry>
+(\\s*)
+</feed>
+''');
+    });
+  });
+}
--- app/test/frontend/handlers_test.dart
+++ app/test/frontend/handlers_test.dart
@@ -399,48 +399,6 @@ void main() {
         expectRedirectResponse(await issueGet('/search?q=foobar&page=2'),
             'https://pub.dartlang.org/packages?q=foobar&page=2');
       });
-
-      tScopedTest('/feed.atom', () async {
-        final backend =
-            new BackendMock(latestPackageVersionsFun: ({offset, limit}) {
-          expect(offset, 0);
-          expect(limit, pageSize);
-          return [testPackageVersion];
-        });
-        registerBackend(backend);
-        await expectAtomXmlResponse(await issueGet('/feed.atom'), regexp: '''
-<\\?xml version="1.0" encoding="UTF-8"\\?>
-<feed xmlns="http://www.w3.org/2005/Atom">
-        <id>https://pub.dartlang.org/feed.atom</id>
-        <title>Pub Packages for Dart</title>
-        <updated>(.*)</updated>
-        <author>
-          <name>Dart Team</name>
-        </author>
-        <link href="https://pub.dartlang.org/" rel="alternate" />
-        <link href="https://pub.dartlang.org/feed.atom" rel="self" />
-        <generator version="0.1.0">Pub Feed Generator</generator>
-        <subtitle>Last Updated Packages</subtitle>
-(\\s*)
-        <entry>
-          <id>urn:uuid:d4fe7eb8-fc0e-5515-a5e5-5868b339d660</id>
-          <title>v0.1.1\\+5 of foobar_pkg</title>
-          <updated>${testPackageVersion.created.toIso8601String()}</updated>
-          <author><name>Hans Juergen &lt;hans@juergen.com&gt;</name></author>
-          <content type="html">&lt;h1&gt;Test Package&lt;&#47;h1&gt;
-&lt;p&gt;This is a readme file.&lt;&#47;p&gt;
-&lt;pre&gt;&lt;code class=&quot;language-dart&quot;&gt;void main\\(\\) {
-}
-&lt;&#47;code&gt;&lt;&#47;pre&gt;
-</content>
-          <link href="https://pub.dartlang.org/packages/foobar_pkg"
-                rel="alternate"
-                title="foobar_pkg" />
-        </entry>
-(\\s*)
-</feed>
-''');
-      });
     });
 
     group('/documentation', () {
