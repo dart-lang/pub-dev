86d2407e629080baa8c42eb8c94a98f6580adbbc
Istvan Soos <istvan.soos@gmail.com>
2018-11-27 09:50:30 +0100

    Remove Analysis serving endpoint.

--- app/lib/analyzer/handlers.dart
+++ app/lib/analyzer/handlers.dart
@@ -6,10 +6,7 @@ import 'dart:async';
 
 import 'package:shelf/shelf.dart' as shelf;
 
-import '../shared/analyzer_service.dart';
 import '../shared/handlers.dart';
-
-import 'backend.dart';
 
 /// Handlers for the analyzer service.
 Future<shelf.Response> analyzerServiceHandler(shelf.Request request) async {
@@ -21,8 +18,6 @@ Future<shelf.Response> analyzerServiceHandler(shelf.Request request) async {
 
   if (handler != null) {
     return await handler(request);
-  } else if (path.startsWith('/packages/')) {
-    return _packageHandler(request);
   } else {
     return notFoundHandler(request);
   }
@@ -30,35 +25,3 @@ Future<shelf.Response> analyzerServiceHandler(shelf.Request request) async {
 
 /// Handler /debug requests
 shelf.Response _debugHandler(shelf.Request request) => debugResponse();
-
-/// Handles requests for:
-///   - /packages/<package>
-///   - /packages/<package>/<version>
-///   - /packages/<package>/<version>/<analysis>
-Future<shelf.Response> _packageHandler(shelf.Request request) async {
-  final String path = request.requestedUri.path.substring('/packages/'.length);
-  final List<String> pathParts = path.split('/');
-  if (path.length == 0 || pathParts.length > 3) {
-    return notFoundHandler(request);
-  }
-  final String package = pathParts[0];
-  final String version = pathParts.length == 1 ? null : pathParts[1];
-  final int analysisId =
-      pathParts.length <= 2 ? null : int.tryParse(pathParts[2] ?? '-1') ?? -1;
-  if (analysisId == -1) {
-    return notFoundHandler(request);
-  }
-  final String panaVersion = request.url.queryParameters['panaVersion'];
-
-  final String requestMethod = request.method?.toUpperCase();
-  if (requestMethod == 'GET') {
-    final AnalysisData data = await analysisBackend.getAnalysis(package,
-        version: version, analysis: analysisId, panaVersion: panaVersion);
-    if (data == null) {
-      return notFoundHandler(request);
-    }
-    return jsonResponse(data.toJson(), pretty: isPrettyJson(request));
-  }
-
-  return notFoundHandler(request);
-}
