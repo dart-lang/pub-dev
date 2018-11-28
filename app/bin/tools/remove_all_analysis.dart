e37b20e23643fa4538e62a36cb31c3bcfe26bcf6
István Soós <isoos@users.noreply.github.com>
2018-11-28 16:04:10 +0100

    Tool to remove all *Analysis entities. (#1823)
    * Tool to remove all *Analysis entities.
    
    * dartdoc

---
+++ app/bin/tools/remove_all_analysis.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:gcloud/db.dart';
+
+import 'package:pub_dartlang_org/analyzer/models.dart';
+import 'package:pub_dartlang_org/frontend/service_utils.dart';
+
+/// The tool is intended to remove all of the obsolete *Analysis entities from
+/// Datastore. Run this after the actively running runtimeVersion is after
+/// 2018.11.22 and also remove this tool (along with model references).
+Future main(List<String> arguments) async {
+  await withProdServices(() async {
+    await _deleteWithQuery('Analysis', dbService.query<Analysis>());
+    await _deleteWithQuery(
+        'PackageVersionAnalysis', dbService.query<PackageVersionAnalysis>());
+    await _deleteWithQuery(
+        'PackageAnalysis', dbService.query<PackageAnalysis>());
+  });
+}
+
+Future _deleteWithQuery<T>(String name, Query query) async {
+  final deletes = <Key>[];
+  int total = 0;
+  await for (Model m in query.run()) {
+    deletes.add(m.key);
+    total++;
+    if (deletes.length >= 100) {
+      print('Deleting ${deletes.length} $name (total: $total)...');
+      await dbService.commit(deletes: deletes);
+      deletes.clear();
+    }
+  }
+  if (deletes.isNotEmpty) {
+    print('Deleting ${deletes.length} $name (total: $total)...');
+    await dbService.commit(deletes: deletes);
+    deletes.clear();
+  }
+}
