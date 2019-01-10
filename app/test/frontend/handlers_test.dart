60c4053f151f46cccf641713361b9cd666ecb38c
Istvan Soos <istvan.soos@gmail.com>
2019-01-10 15:21:35 +0100

    Move package-related tests to handlers/package_test.dart

--- app/test/frontend/handlers_test.dart
+++ app/test/frontend/handlers_test.dart
@@ -8,13 +8,9 @@ import 'dart:async';
 
 import 'package:test/test.dart';
 
-import 'package:pub_dartlang_org/frontend/backend.dart';
 import 'package:pub_dartlang_org/frontend/models.dart';
 import 'package:pub_dartlang_org/frontend/search_service.dart';
 import 'package:pub_dartlang_org/frontend/static_files.dart';
-import 'package:pub_dartlang_org/scorecard/backend.dart';
-import 'package:pub_dartlang_org/shared/analyzer_client.dart';
-import 'package:pub_dartlang_org/shared/dartdoc_client.dart';
 import 'package:pub_dartlang_org/shared/search_service.dart';
 
 import '../shared/handlers_test_utils.dart';
@@ -54,93 +50,6 @@ Future main() async {
     });
 
     group('ui', () {
-      tScopedTest('/packages/foobar_pkg - found', () async {
-        final backend = new BackendMock(lookupPackageFun: (String packageName) {
-          expect(packageName, 'foobar_pkg');
-          return testPackage;
-        }, versionsOfPackageFun: (String package) {
-          expect(package, testPackage.name);
-          return [testPackageVersion];
-        }, downloadUrlFun: (String package, String version) {
-          return Uri.parse('http://blobstore/$package/$version');
-        });
-        registerBackend(backend);
-        registerAnalyzerClient(new AnalyzerClientMock());
-        registerDartdocClient(new DartdocClientMock());
-        registerScoreCardBackend(new ScoreCardBackendMock());
-        await expectHtmlResponse(await issueGet('/packages/foobar_pkg'));
-      });
-
-      tScopedTest('/packages/foobar_pkg - not found', () async {
-        final backend = new BackendMock(lookupPackageFun: (String packageName) {
-          expect(packageName, 'foobar_pkg');
-          return null;
-        });
-        registerBackend(backend);
-        await expectRedirectResponse(
-            await issueGet('/packages/foobar_pkg'), '/packages?q=foobar_pkg');
-      });
-
-      tScopedTest('/packages/foobar_pkg/versions - found', () async {
-        final backend = new BackendMock(versionsOfPackageFun: (String package) {
-          expect(package, testPackage.name);
-          return [testPackageVersion];
-        }, downloadUrlFun: (String package, String version) {
-          return Uri.parse('http://blobstore/$package/$version');
-        });
-        registerBackend(backend);
-        registerDartdocClient(new DartdocClientMock());
-        await expectHtmlResponse(
-            await issueGet('/packages/foobar_pkg/versions'));
-      });
-
-      tScopedTest('/packages/foobar_pkg/versions - not found', () async {
-        final backend = new BackendMock(versionsOfPackageFun: (String package) {
-          expect(package, testPackage.name);
-          return [];
-        });
-        registerBackend(backend);
-        await expectRedirectResponse(
-            await issueGet('/packages/foobar_pkg/versions'),
-            '/packages?q=foobar_pkg');
-      });
-
-      tScopedTest('/packages/foobar_pkg/versions/0.1.1 - found', () async {
-        final backend = new BackendMock(lookupPackageFun: (String package) {
-          expect(package, testPackage.name);
-          return testPackage;
-        }, versionsOfPackageFun: (String package) {
-          expect(package, testPackage.name);
-          return [testPackageVersion];
-        }, downloadUrlFun: (String package, String version) {
-          return Uri.parse('http://blobstore/$package/$version');
-        });
-        registerBackend(backend);
-        registerAnalyzerClient(new AnalyzerClientMock());
-        registerDartdocClient(new DartdocClientMock());
-        registerScoreCardBackend(new ScoreCardBackendMock());
-        await expectHtmlResponse(
-            await issueGet('/packages/foobar_pkg/versions/0.1.1+5'));
-      });
-
-      tScopedTest('/packages/foobar_pkg/versions/0.1.2 - not found', () async {
-        final backend = new BackendMock(lookupPackageFun: (String package) {
-          expect(package, testPackage.name);
-          return testPackage;
-        }, versionsOfPackageFun: (String package) {
-          expect(package, testPackage.name);
-          return [testPackageVersion];
-        }, downloadUrlFun: (String package, String version) {
-          return Uri.parse('http://blobstore/$package/$version');
-        });
-        registerBackend(backend);
-        registerAnalyzerClient(new AnalyzerClientMock());
-        registerDartdocClient(new DartdocClientMock());
-        await expectRedirectResponse(
-            await issueGet('/packages/foobar_pkg/versions/0.1.2'),
-            '/packages/foobar_pkg#-versions-tab-');
-      });
-
       tScopedTest('/authorized', () async {
         await expectHtmlResponse(await issueGet('/authorized'));
       });
