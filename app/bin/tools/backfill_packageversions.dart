a3df05e9789cc63f6891d9e20604472edc458521
István Soós <isoos@users.noreply.github.com>
2019-02-12 13:46:11 +0100

    Concurrent package backfill for PackageVersion[Info|Pubspec] (#2037)
    * Concurrent package backfill for PackageVersion[Info|Pubspec]
    
    * Help flag and info

--- app/bin/tools/backfill_packageversions.dart
+++ app/bin/tools/backfill_packageversions.dart
@@ -7,15 +7,30 @@ import 'dart:async';
 import 'package:appengine/appengine.dart';
 import 'package:args/args.dart';
 import 'package:gcloud/db.dart';
+import 'package:pool/pool.dart';
 
 import 'package:pub_dartlang_org/frontend/models.dart';
 import 'package:pub_dartlang_org/frontend/service_utils.dart';
 
 final _argParser = new ArgParser()
-  ..addOption('package', abbr: 'p', help: 'The package to backfill.');
+  ..addOption('concurrency',
+      abbr: 'c', defaultsTo: '1', help: 'Number of concurrent processing.')
+  ..addOption('package', abbr: 'p', help: 'The package to backfill.')
+  ..addFlag('help', abbr: 'h', defaultsTo: false, help: 'Show help.');
 
 Future main(List<String> args) async {
   final argv = _argParser.parse(args);
+  if (argv['help'] as bool == true) {
+    print('Usage: dart backfill_packageversions.dart');
+    print(
+        'Ensures a matching PackageVersionPubspec entity exists for each PackageVersion.');
+    print(
+        'Ensures a matching PackageVersionInfo entity exists for each PackageVersion.');
+    print(_argParser.usage);
+    return;
+  }
+
+  final concurrency = int.parse(argv['concurrency'] as String);
   final package = argv['package'] as String;
 
   useLoggingPackageAdaptor();
@@ -23,9 +38,16 @@ Future main(List<String> args) async {
     if (package != null) {
       await _backfillPackage(package);
     } else {
+      final pool = Pool(concurrency);
+      final futures = <Future>[];
+
       await for (Package p in dbService.query<Package>().run()) {
-        await _backfillPackage(p.name);
+        final f = pool.withResource(() => _backfillPackage(p.name));
+        futures.add(f);
       }
+
+      await Future.wait(futures);
+      await pool.close();
     }
   });
 }
