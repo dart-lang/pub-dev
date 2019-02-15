6b664468f04ba6d63cc2d50460a8c864a9f48f29
Istvan Soos <istvan.soos@gmail.com>
2019-02-15 12:34:25 +0100

    Metrics API: show metrics for (older) versions too.

--- app/lib/frontend/handlers/custom_api.dart
+++ app/lib/frontend/handlers/custom_api.dart
@@ -177,8 +177,9 @@ Future<shelf.Response> apiPackageMetricsHandler(shelf.Request request) async {
     return jsonResponse({}, status: 404, pretty: isPrettyJson(request));
   }
   final packageName = parts[3];
-  final data = await scoreCardBackend.getScoreCardData(packageName, null,
-      onlyCurrent: false);
+  final packageVersion = request.requestedUri.queryParameters['version'];
+  final data = await scoreCardBackend
+      .getScoreCardData(packageName, packageVersion, onlyCurrent: false);
   if (data == null) {
     return jsonResponse({}, status: 404, pretty: isPrettyJson(request));
   }
