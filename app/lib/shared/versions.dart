4f7dccf9c6b594c9b592af3ab7457141eedf09ef
Istvan Soos <istvan.soos@gmail.com>
2019-02-27 10:16:48 +0100

    Upgrade runtime SDK

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -34,7 +34,7 @@ final Version semanticRuntimeVersion = new Version.parse(runtimeVersion);
 final String gcBeforeRuntimeVersion = '2018.12.05';
 
 // keep in-sync with SDK version in .travis.yml, .mono_repo.yml and Dockerfile
-final String runtimeSdkVersion = '2.2.0-dev.2.1';
+final String runtimeSdkVersion = '2.2.0';
 final String toolEnvSdkVersion = '2.2.0';
 
 // keep in-sync with app/pubspec.yaml
