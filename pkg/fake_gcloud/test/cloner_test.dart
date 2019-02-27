ace8ddc28b8dec8fd2e60bea7506a62b8da8ecba
Istvan Soos <istvan.soos@gmail.com>
2019-02-27 13:38:24 +0100

    dartfmt

--- pkg/fake_gcloud/test/cloner_test.dart
+++ pkg/fake_gcloud/test/cloner_test.dart
@@ -6,7 +6,9 @@ void main() {
   final cloner = Cloner();
 
   test('sample', () {
-    final x = cloner.clone(Sample()..type = 't1'..count = 1);
+    final x = cloner.clone(Sample()
+      ..type = 't1'
+      ..count = 1);
     expect(x, isNotNull);
     expect(x.type, 't1');
     expect(x.count, 1);
