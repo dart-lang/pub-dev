d4cf8b0e57f73541d512fa66bc6603e850a7ce94
Istvan Soos <istvan.soos@gmail.com>
2019-03-06 17:18:23 +0100

    Reject uploads on case insensitive filename matches.

--- app/lib/shared/utils.dart
+++ app/lib/shared/utils.dart
@@ -51,8 +51,7 @@ Future<T> withTempDirectory<T>(Future<T> func(Directory dir),
 }
 
 Future<List<String>> listTarball(String path) async {
-  // List files up-to 4 directory levels:
-  final args = ['--exclude=*/*/*/*/*', '-tzf', path];
+  final args = ['-tzf', path];
   final result = await Process.run('tar', args);
   if (result.exitCode != 0) {
     _logger.warning('The "tar $args" command failed:\n'
