f6ba564cec795ca94ad3bdb429160f31b73d0602
Kevin Moore <kevmoo@google.com>
2019-04-23 16:56:42 -0700

    use latest mono_repo

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Created with package:mono_repo v2.0.0
+# Created with package:mono_repo v2.1.0
 
 if [[ -z ${PKGS} ]]; then
   echo -e '\033[31mPKGS environment variable must be set!\033[0m'
@@ -16,26 +16,26 @@ EXIT_CODE=0
 for PKG in ${PKGS}; do
   echo -e "\033[1mPKG: ${PKG}\033[22m"
   pushd "${PKG}" || exit $?
-  pub get || exit $?
+  pub upgrade --no-precompile || exit $?
 
   for TASK in "$@"; do
+    echo
+    echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
     case ${TASK} in
-    dartanalyzer) echo
-      echo -e '\033[1mTASK: dartanalyzer\033[22m'
-      echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
+    dartanalyzer)
+      echo 'dartanalyzer --fatal-infos --fatal-warnings .'
       dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
       ;;
-    dartfmt) echo
-      echo -e '\033[1mTASK: dartfmt\033[22m'
-      echo -e 'dartfmt -n --set-exit-if-changed .'
+    dartfmt)
+      echo 'dartfmt -n --set-exit-if-changed .'
       dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
       ;;
-    test) echo
-      echo -e '\033[1mTASK: test\033[22m'
-      echo -e 'pub run test --run-skipped'
+    test)
+      echo 'pub run test --run-skipped'
       pub run test --run-skipped || EXIT_CODE=$?
       ;;
-    *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+    *)
+      echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
       EXIT_CODE=1
       ;;
     esac
