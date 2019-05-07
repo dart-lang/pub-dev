06c94dabe33d40503c5bb55921a11ca2997f0b0b
István Soós <isoos@users.noreply.github.com>
2019-05-07 16:26:46 +0200

    Upgrade: pana, Dart SDK, Flutter SDK (#2269)
    * Upgrade: pana, Dart SDK, Flutter SDK
    
    * fix changelog

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -16,7 +16,7 @@ final RegExp runtimeVersionPattern = RegExp(r'\d{4}\.\d{2}\.\d{2}');
 /// Increment the version when a change is significant enough to trigger
 /// reprocessing, including: version change in pana, dartdoc, or the SDKs,
 /// or when an feature or bugfix should be picked up by the analysis ASAP.
-const String runtimeVersion = '2019.05.03';
+const String runtimeVersion = '2019.05.08';
 final Version semanticRuntimeVersion = Version.parse(runtimeVersion);
 
 /// The version which marks the earliest version of the data which we'd like to
@@ -25,6 +25,7 @@ final Version semanticRuntimeVersion = Version.parse(runtimeVersion);
 ///
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens:
+/// - 2019.05.08
 /// - 2019.05.03
 /// - 2019.04.17
 /// - 2019.04.02
@@ -33,13 +34,13 @@ final String gcBeforeRuntimeVersion = '2019.03.05';
 
 // keep in-sync with SDK version in .travis.yml, .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '2.2.0';
-final String toolEnvSdkVersion = '2.2.0';
+final String toolEnvSdkVersion = '2.3.0';
 
 // keep in-sync with app/pubspec.yaml
-final String panaVersion = '0.12.14';
+final String panaVersion = '0.12.15';
 final Version semanticPanaVersion = Version.parse(panaVersion);
 
-final String flutterVersion = '1.4.7';
+final String flutterVersion = '1.5.8';
 final Version semanticFlutterVersion = Version.parse(flutterVersion);
 
 // keep in-sync with pkg/pub_dartdoc/pubspec.yaml
