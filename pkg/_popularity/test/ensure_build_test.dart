7707730ce20106b89ca0c4323ca704b32e127fb5
István Soós <isoos@users.noreply.github.com>
2019-07-05 14:22:04 +0200

    Fix ensure_build_test.dart in the relevant packages. (#2448)

--- pkg/_popularity/test/ensure_build_test.dart
+++ pkg/_popularity/test/ensure_build_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -8,5 +8,6 @@ import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure_build', () => expectBuildClean(packageRelativeDirectory: 'app'));
+  test('ensure_build',
+      () => expectBuildClean(packageRelativeDirectory: 'pkg/_popularity'));
 }
