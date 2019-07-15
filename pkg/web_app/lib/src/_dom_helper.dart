71d0145dc3567dd42607787eada04d437e7599e1
Istvan Soos <istvan.soos@gmail.com>
2019-07-15 16:27:45 +0200

    Use server-provided templates for login/profile

--- pkg/web_app/lib/src/_dom_helper.dart
+++ pkg/web_app/lib/src/_dom_helper.dart
@@ -4,36 +4,6 @@
 
 import 'dart:html';
 
-/// Creates a DOM Element.
-Element elem(
-  String tag, {
-  String id,
-  Map<String, String> attrs,
-  Iterable<String> classes,
-  String text,
-  List<Element> children,
-  dynamic Function(MouseEvent event) onClick,
-}) {
-  final elem = Element.tag(tag);
-  if (id != null) {
-    elem.id = id;
-  }
-  if (attrs != null) {
-    elem.attributes.addAll(attrs);
-  }
-  if (classes != null) {
-    elem.classes.addAll(classes);
-  }
-  if (text != null) {
-    elem.text = text;
-  }
-  children?.forEach(elem.append);
-  if (onClick != null) {
-    elem.onClick.listen(onClick);
-  }
-  return elem;
-}
-
 /// Show/hide an element.
 void updateDisplay(Element elem, bool show, {String display}) {
   elem.style.display = show ? display : 'none';
