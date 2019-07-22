b9278f0fd24633d94b53c86e9e16864a086acc99
István Soós <isoos@users.noreply.github.com>
2019-07-22 13:45:20 +0200

    Update generated code. (#2504)

--- pkg/_popularity/lib/popularity.g.dart
+++ pkg/_popularity/lib/popularity.g.dart
@@ -8,38 +8,44 @@ part of package_popularity;
 
 PackagePopularity _$PackagePopularityFromJson(Map<String, dynamic> json) {
   return PackagePopularity(
-      DateTime.parse(json['date_first'] as String),
-      DateTime.parse(json['date_last'] as String),
-      (json['items'] as Map<String, dynamic>).map(
-        (k, e) => MapEntry(k, VoteTotals.fromJson(e as Map<String, dynamic>)),
-      ));
+    DateTime.parse(json['date_first'] as String),
+    DateTime.parse(json['date_last'] as String),
+    (json['items'] as Map<String, dynamic>).map(
+      (k, e) => MapEntry(k, VoteTotals.fromJson(e as Map<String, dynamic>)),
+    ),
+  );
 }
 
 Map<String, dynamic> _$PackagePopularityToJson(PackagePopularity instance) =>
     <String, dynamic>{
       'date_first': instance.dateFirst.toIso8601String(),
       'date_last': instance.dateLast.toIso8601String(),
-      'items': instance.items
+      'items': instance.items,
     };
 
 VoteTotals _$VoteTotalsFromJson(Map<String, dynamic> json) {
-  return VoteTotals(VoteData.fromJson(json['flutter'] as Map<String, dynamic>),
-      VoteData.fromJson(json['notFlutter'] as Map<String, dynamic>));
+  return VoteTotals(
+    VoteData.fromJson(json['flutter'] as Map<String, dynamic>),
+    VoteData.fromJson(json['notFlutter'] as Map<String, dynamic>),
+  );
 }
 
 Map<String, dynamic> _$VoteTotalsToJson(VoteTotals instance) =>
     <String, dynamic>{
       'flutter': instance.flutter,
-      'notFlutter': instance.notFlutter
+      'notFlutter': instance.notFlutter,
     };
 
 VoteData _$VoteDataFromJson(Map<String, dynamic> json) {
-  return VoteData(json['votes_direct'] as int, json['votes_dev'] as int,
-      json['votes_total'] as int);
+  return VoteData(
+    json['votes_direct'] as int,
+    json['votes_dev'] as int,
+    json['votes_total'] as int,
+  );
 }
 
 Map<String, dynamic> _$VoteDataToJson(VoteData instance) => <String, dynamic>{
       'votes_direct': instance.direct,
       'votes_dev': instance.dev,
-      'votes_total': instance.total
+      'votes_total': instance.total,
     };
--- pkg/client_data/lib/page_data.g.dart
+++ pkg/client_data/lib/page_data.g.dart
@@ -8,23 +8,26 @@ part of 'page_data.dart';
 
 PageData _$PageDataFromJson(Map<String, dynamic> json) {
   return PageData(
-      pkgData: json['pkgData'] == null
-          ? null
-          : PkgData.fromJson(json['pkgData'] as Map<String, dynamic>));
+    pkgData: json['pkgData'] == null
+        ? null
+        : PkgData.fromJson(json['pkgData'] as Map<String, dynamic>),
+  );
 }
 
-Map<String, dynamic> _$PageDataToJson(PageData instance) =>
-    <String, dynamic>{'pkgData': instance.pkgData};
+Map<String, dynamic> _$PageDataToJson(PageData instance) => <String, dynamic>{
+      'pkgData': instance.pkgData,
+    };
 
 PkgData _$PkgDataFromJson(Map<String, dynamic> json) {
   return PkgData(
-      package: json['package'] as String,
-      version: json['version'] as String,
-      isDiscontinued: json['isDiscontinued'] as bool);
+    package: json['package'] as String,
+    version: json['version'] as String,
+    isDiscontinued: json['isDiscontinued'] as bool,
+  );
 }
 
 Map<String, dynamic> _$PkgDataToJson(PkgData instance) => <String, dynamic>{
       'package': instance.package,
       'version': instance.version,
-      'isDiscontinued': instance.isDiscontinued
+      'isDiscontinued': instance.isDiscontinued,
     };
