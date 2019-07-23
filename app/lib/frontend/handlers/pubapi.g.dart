9a83ff9601ab35c5fa92fb614507bed820f6daeb
Jonas Finnemann Jensen <jonasfj@google.com>
2019-07-23 13:58:41 +0200

    Rename to ApiReponseException (#2509)
    * Rename to ApiReponseException
    
    * Fix typo
    
    * Update pubspec.lock

--- app/lib/frontend/handlers/pubapi.g.dart
+++ app/lib/frontend/handlers/pubapi.g.dart
@@ -13,8 +13,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.listPackageVersions(request, package);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/packages/<package>/versions/<version>',
@@ -23,8 +25,10 @@ Router _$PubApiRouter(PubApi service) {
       final _$result =
           await service.packageVersionInfo(request, package, version);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/packages/<package>/versions/<version>.tar.gz',
@@ -32,8 +36,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.fetchPackage(request, package, version);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/packages/<package>/versions/<version>.tar.gz',
@@ -41,16 +47,20 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.fetchPackage(request, package, version);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/packages/versions/new', (Request request) async {
     try {
       final _$result = await service.getPackageUploadUrl(request);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/packages/versions/newUploadFinish',
@@ -58,8 +68,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.packageUploadCallback(request);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('POST', r'/api/packages/<package>/uploaders',
@@ -67,8 +79,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.addUploader(request, package);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('DELETE', r'/api/packages/<package>/uploaders/<email>',
@@ -76,8 +90,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.removeUploader(request, package, email);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('POST', r'/api/publisher/<publisherId>',
@@ -85,8 +101,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.createPublisher(request, publisherId);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/publisher/<publisherId>',
@@ -94,8 +112,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.publisherInfo(request, publisherId);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('PUT', r'/api/publisher/<publisherId>',
@@ -108,8 +128,10 @@ Router _$PubApiRouter(PubApi service) {
             return UpdatePublisherRequest.fromJson(o);
           }));
       return $utilities.jsonResponse(_$result.toJson());
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('POST', r'/api/publisher/<publisherId>/invite-member',
@@ -118,8 +140,10 @@ Router _$PubApiRouter(PubApi service) {
       final _$result =
           await service.invitePublisherMember(request, publisherId);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/publisher/<publisherId>/members',
@@ -127,8 +151,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.listPublisherMembers(request, publisherId);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/publisher/<publisherId>/members/<userId>',
@@ -137,8 +163,10 @@ Router _$PubApiRouter(PubApi service) {
       final _$result =
           await service.publisherMemberInfo(request, publisherId, userId);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('PUT', r'/api/publisher/<publisherId>/members/<userId>',
@@ -147,8 +175,10 @@ Router _$PubApiRouter(PubApi service) {
       final _$result =
           await service.updatePublisherMember(request, publisherId, userId);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('DELETE', r'/api/publisher/<publisherId>/members/<userId>',
@@ -157,8 +187,10 @@ Router _$PubApiRouter(PubApi service) {
       final _$result =
           await service.removePublisherMember(request, publisherId, userId);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/account/consent/<consentId>',
@@ -166,8 +198,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.consentInfo(request, consentId);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('PUT', r'/api/account/consent/<consentId>',
@@ -175,8 +209,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.resolveConsent(request, consentId);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/account/options/packages/<package>',
@@ -184,8 +220,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.accountPackageOptions(request, package);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/documentation/<package>',
@@ -193,24 +231,30 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.documentation(request, package);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/history', (Request request) async {
     try {
       final _$result = await service.history(request);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/packages', (Request request) async {
     try {
       final _$result = await service.listPackages(request);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/packages/<package>/metrics',
@@ -218,8 +262,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.packageMetrics(request, package);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/packages/<package>/options',
@@ -227,8 +273,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.packageOptions(request, package);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('PUT', r'/api/packages/<package>/options',
@@ -236,32 +284,40 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.setPackageOptions(request, package);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/api/search', (Request request) async {
     try {
       final _$result = await service.search(request);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/debug', (Request request) async {
     try {
       final _$result = await service.debug(request);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/packages.json', (Request request) async {
     try {
       final _$result = await service.packages(request);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   router.add('GET', r'/packages/<package>.json',
@@ -269,8 +325,10 @@ Router _$PubApiRouter(PubApi service) {
     try {
       final _$result = await service.packageJson(request, package);
       return _$result;
-    } on ResponseException catch (e) {
-      return e.asResponse();
+    } on ApiResponseException catch (e) {
+      return e.asApiResponse();
+    } catch (e, st) {
+      return $utilities.unhandledError(e, st);
     }
   });
   return router;
