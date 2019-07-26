a8b02d698458ea091b8fa0f520954a791231d150
Istvan Soos <istvan.soos@gmail.com>
2019-07-26 10:31:10 +0200

    http.Client/shelf.Handler bridge for tests.

--- app/test/shared/test_services.dart
+++ app/test/shared/test_services.dart
@@ -9,15 +9,18 @@ import 'package:gcloud/storage.dart';
 import 'package:gcloud/service_scope.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/testing.dart' as http_testing;
+import 'package:logging/logging.dart';
 import 'package:shelf/shelf.dart' as shelf;
 
 import 'package:pub_dartlang_org/account/backend.dart';
 import 'package:pub_dartlang_org/account/testing/fake_auth_provider.dart';
+import 'package:pub_dartlang_org/frontend/handlers.dart';
 import 'package:pub_dartlang_org/scorecard/backend.dart';
 import 'package:pub_dartlang_org/search/backend.dart';
 import 'package:pub_dartlang_org/search/handlers.dart';
 import 'package:pub_dartlang_org/search/index_simple.dart';
 import 'package:pub_dartlang_org/shared/configuration.dart';
+import 'package:pub_dartlang_org/shared/handler_helpers.dart';
 import 'package:pub_dartlang_org/shared/popularity_storage.dart';
 import 'package:pub_dartlang_org/shared/redis_cache.dart';
 import 'package:pub_dartlang_org/shared/search_client.dart';
@@ -81,8 +84,8 @@ void testWithServices(String name, Future fn()) {
               await searchBackend.loadDocument(lithium.package.name));
           await packageIndex.merge();
 
-          registerSearchClient(SearchClient(http_testing.MockClient(
-              _wrapShelfHandler(searchServiceHandler))));
+          registerSearchClient(
+              SearchClient(httpClient(handler: searchServiceHandler)));
 
           registerScopeExitCallback(searchClient.close);
 
@@ -95,13 +98,37 @@ void testWithServices(String name, Future fn()) {
   });
 }
 
-http_testing.MockClientHandler _wrapShelfHandler(shelf.Handler handler) {
+/// Returns a HTTP client that bridges HTTP requests and shelf handlers without
+/// the actual HTTP transport layer.
+///
+/// If [handler] is not specified, it will use the default frontend handler.
+http_testing.MockClient httpClient({
+  shelf.Handler handler,
+  String authToken,
+}) {
+  handler ??= createAppHandler(null);
+  handler = wrapHandler(
+    Logger.detached('test'),
+    handler,
+    sanitize: true,
+  );
+  return http_testing.MockClient(
+      _wrapShelfHandler(handler, authToken: authToken));
+}
+
+http_testing.MockClientHandler _wrapShelfHandler(
+  shelf.Handler handler, {
+  String authToken,
+}) {
   return (rq) async {
     final shelfRq = shelf.Request(
       rq.method,
       rq.url,
       body: rq.body,
-      headers: rq.headers,
+      headers: <String, String>{
+        if (authToken != null) 'authorization': 'bearer $authToken',
+        ...rq.headers,
+      },
     );
     final rs = await handler(shelfRq);
     return http.Response(
