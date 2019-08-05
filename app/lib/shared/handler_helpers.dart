05b959b9f1d790b9b6f91665182f1cc7f05ec4a5
Istvan Soos <istvan.soos@gmail.com>
2019-08-05 10:06:08 +0200

    Increase HSTS duration to a year.

--- app/lib/shared/handler_helpers.dart
+++ app/lib/shared/handler_helpers.dart
@@ -23,7 +23,7 @@ import 'markdown.dart';
 import 'urls.dart' as urls;
 import 'utils.dart' show fileAnIssueContent, parseCookieHeader;
 
-const _hstsDuration = Duration(hours: 2);
+const _hstsDuration = Duration(days: 365);
 
 Future<void> runHandler(
   Logger logger,
