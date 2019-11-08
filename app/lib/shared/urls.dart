1aad27c1e80dbe085c6c2196bd61f21592e45234
István Soós <isoos@users.noreply.github.com>
2019-11-08 16:12:46 +0100

    PackageLinks.baseUrl (#2999)
    * PackageLinks.baseUrl
    
    * Remove www and .git

--- app/lib/shared/urls.dart
+++ app/lib/shared/urls.dart
@@ -143,6 +143,42 @@ String inferIssueTrackerUrl(String baseUrl) {
   return null;
 }
 
+/// Infer base URL that can be used to link files from.
+String inferBaseUrl({String homepageUrl, String repositoryUrl}) {
+  String baseUrl = repositoryUrl ?? homepageUrl;
+  if (baseUrl == null) return null;
+
+  // In a few cases people specify only a deep repository URL for their
+  // package (e.g. a monorepo for multiple packages). While we default the
+  // base URL to be the repository URL, this check allows us to use the deep
+  // URL for linking.
+  if (homepageUrl != null && homepageUrl.startsWith(baseUrl)) {
+    baseUrl = homepageUrl;
+  }
+
+  // URL cleanup
+  final prefixReplacements = const <String, String>{
+    'http://github.com/': 'https://github.com/',
+    'http://www.github.com/': 'https://github.com/',
+    'https://www.github.com/': 'https://github.com/',
+    'http://gitlab.com/': 'https://gitlab.com/',
+    'http://www.gitlab.com/': 'https://gitlab.com/',
+    'https://www.gitlab.com/': 'https://gitlab.com/',
+  };
+  for (final prefix in prefixReplacements.keys) {
+    if (baseUrl.startsWith(prefix)) {
+      baseUrl = baseUrl.replaceFirst(prefix, prefixReplacements[prefix]);
+    }
+  }
+  if ((baseUrl.startsWith('https://github.com/') ||
+          baseUrl.startsWith('https://gitlab.com/')) &&
+      baseUrl.endsWith('.git')) {
+    baseUrl = baseUrl.substring(0, baseUrl.length - 4);
+  }
+
+  return baseUrl;
+}
+
 /// Infer the hosting/service provider for a given URL.
 String inferServiceProviderName(String url) {
   if (url == null) {
