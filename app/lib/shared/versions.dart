e7be3149acb210643d18d92fbe2c231b37b47ee8
István Soós <isoos@users.noreply.github.com>
2019-11-15 09:06:35 +0100

    Upgrade tool SDK: 2.6.1 (#3026)

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -16,7 +16,7 @@ final RegExp runtimeVersionPattern = RegExp(r'\d{4}\.\d{2}\.\d{2}');
 /// Increment the version when a change is significant enough to trigger
 /// reprocessing, including: version change in pana, dartdoc, or the SDKs,
 /// or when an feature or bugfix should be picked up by the analysis ASAP.
-const String runtimeVersion = '2019.11.01';
+const String runtimeVersion = '2019.11.12';
 final Version semanticRuntimeVersion = Version.parse(runtimeVersion);
 
 /// The version which marks the earliest version of the data which we'd like to
@@ -25,6 +25,7 @@ final Version semanticRuntimeVersion = Version.parse(runtimeVersion);
 ///
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens:
+/// - 2019.11.12
 /// - 2019.11.01
 /// - 2019.10.22
 /// - 2019.10.07
@@ -33,7 +34,7 @@ final String gcBeforeRuntimeVersion = '2019.09.10';
 
 // keep in-sync with SDK version in .travis.yml, .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '2.6.0';
-final String toolEnvSdkVersion = '2.6.0';
+final String toolEnvSdkVersion = '2.6.1';
 
 // keep in-sync with app/pubspec.yaml
 final String panaVersion = '0.12.21';
