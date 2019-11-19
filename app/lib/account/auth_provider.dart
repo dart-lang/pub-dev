4176b528d043dc3752530e9ac961cffafcbd6a46
Istvan Soos <istvan.soos@gmail.com>
2019-11-19 14:49:53 +0100

    Remove unused auth methods.

--- app/lib/account/auth_provider.dart
+++ app/lib/account/auth_provider.dart
@@ -11,15 +11,6 @@ class AuthResult {
 
 /// Authenticates bearer tokens from the `'authentication: bearer'` header.
 abstract class AuthProvider {
-  /// Returns the URL of the authorization endpoint.
-  String authorizationUrl(String redirectUrl, String state);
-
-  /// Validates the authorization [code], and returns the access token.
-  ///
-  /// Returns null on any error, or if the token is expired, or the code is not
-  /// verified.
-  Future<String> authCodeToAccessToken(String redirectUrl, String code);
-
   /// Checks the [token] and returns a verified user information.
   ///
   /// Returns null on any error, or if the token is expired, or the user is not
