1623497d86aa0acf00ca876726eb9b7385903161
Jonas Finnemann Jensen <jonasfj@google.com>
2019-12-03 12:48:37 +0100

    Upgrade dartdoc (#3090)
    * Upgraded dartdoc to 0.29.1
    
    * Fix xml package breakage in 3.6.0

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -16,7 +16,7 @@ final RegExp runtimeVersionPattern = RegExp(r'\d{4}\.\d{2}\.\d{2}');
 /// Increment the version when a change is significant enough to trigger
 /// reprocessing, including: version change in pana, dartdoc, or the SDKs,
 /// or when an feature or bugfix should be picked up by the analysis ASAP.
-const String runtimeVersion = '2019.11.29';
+const String runtimeVersion = '2019.12.02';
 final Version semanticRuntimeVersion = Version.parse(runtimeVersion);
 
 /// The version which marks the earliest version of the data which we'd like to
@@ -44,7 +44,7 @@ final String flutterVersion = '1.9.1+hotfix.6';
 final Version semanticFlutterVersion = Version.parse(flutterVersion);
 
 // keep in-sync with pkg/pub_dartdoc/pubspec.yaml
-final String dartdocVersion = '0.28.8';
+final String dartdocVersion = '0.29.1';
 final Version semanticDartdocVersion = Version.parse(dartdocVersion);
 
 // Version that control the dartdoc serving.
