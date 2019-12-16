ce988f898c85f4d4001b8216d27152b93dbcf088
Sarah Zakarias <zarah@google.com>
2019-12-16 14:32:56 +0100

    remove unused import

--- app/bin/tools/backfill_likes.dart
+++ app/bin/tools/backfill_likes.dart
@@ -7,7 +7,6 @@ import 'package:args/args.dart';
 import 'package:gcloud/db.dart';
 import 'package:pool/pool.dart';
 import 'package:pub_dev/account/models.dart';
-import 'package:pub_dev/package/models.dart';
 import 'package:pub_dev/service/entrypoint/tools.dart';
 
 final _argParser = ArgParser()
