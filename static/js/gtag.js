789cf8c06e6f1313cc8f594d94b800e615e67548
Jonas Finnemann Jensen <jonasfj@google.com>
2020-02-07 15:29:10 +0100

    Added GA events for tracking a few clicks

--- static/js/gtag.js
+++ static/js/gtag.js
@@ -1,4 +1,23 @@
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'UA-26406144-13');
+
+// Setup listening to send Google Analytics events when any element
+// with a 'data-ga-click-event' attribute is clicked.
+window.addEventListener('DOMContentLoaded', function () {
+  function sendEvent(e) {
+    gtag('event', e.currentTarget.dataset.gaClickEvent, {
+      'event_category': 'click',
+      'event_label': 'path:' + window.location.pathname,
+      'value': 1
+    });
+  }
+  function addListeners() {
+    var elements = document.querySelectorAll('[data-ga-click-event]');
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener('click', sendEvent);
+    }
+  }
+  addListeners();
+});
