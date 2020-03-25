6fa5122189a6ad7a94f72b90dd41273c61afa5f5
Istvan Soos <istvan.soos@gmail.com>
2020-03-25 14:45:24 +0100

    Do not autofocus search bar when query text was specified.

--- app/lib/frontend/templates/layout.dart
+++ app/lib/frontend/templates/layout.dart
@@ -178,6 +178,7 @@ String _renderSearchBanner({
   }
   final isExperimental = requestContext.isExperimental;
   return templateCache.renderTemplate('shared/search_banner', {
+    'show_autofocus': queryText == null,
     'show_search_filters_btn': isExperimental && type == PageType.listing,
     'show_details': !isExperimental &&
         (type == PageType.listing || type == PageType.landing),
