18bb480ae76f0b54f7fbd6ad9de1b8d18e0ada3f
Istvan Soos <istvan.soos@gmail.com>
2020-03-30 14:49:40 +0200

    Fix: leftover code for debugging styles.

--- app/lib/search/search_client.dart
+++ app/lib/search/search_client.dart
@@ -59,7 +59,6 @@ class SearchClient {
         // Search request before the service initialization completed.
         return null;
       }
-      result.packages.replaceRange(0, 2, [PackageScore(package: 'http', score: 1.0), PackageScore(package: 'pem', score: 1.0)]);
       return result;
     }
 
