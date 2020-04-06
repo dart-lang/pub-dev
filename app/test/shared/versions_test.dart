a0999318b4b01fb97d25fbb5ef5676aedc78e743
István Soós <isoos@users.noreply.github.com>
2020-04-06 16:32:40 +0200

    Upgrade Flutter SDK and dartdoc (#3495)
    * Upgrade Flutter SDK and dartdoc
    
    * updated test

--- app/test/shared/versions_test.dart
+++ app/test/shared/versions_test.dart
@@ -33,7 +33,7 @@ void main() {
     // This test is a reminder that if pana, the SDK or any of the above
     // versions change, we should also adjust the [runtimeVersion]. Before
     // updating the hash value, double-check if it is being updated.
-    expect(hash, 846359092);
+    expect(hash, 353837370);
   });
 
   test('runtime version should be (somewhat) lexicographically ordered', () {
