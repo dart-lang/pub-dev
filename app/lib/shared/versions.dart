caf457f7350fe3f67d2c3df6c18178b66fbac4e3
Istvan Soos <istvan.soos@gmail.com>
2020-04-24 10:31:19 +0200

    Upgrade Dart analysis SDK to 2.8.0

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -25,7 +25,7 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// reprocessing, including: risk of data corruption in analysis, version change
 /// in pana, dartdoc, or the SDKs, or when an feature or bugfix should be picked
 /// up by the analysis ASAP.
-const String runtimeVersion = '2020.04.22';
+const String runtimeVersion = '2020.04.23';
 final Version semanticRuntimeVersion = Version.parse(runtimeVersion);
 
 /// The version which marks the earliest version of the data which we'd like to
@@ -34,16 +34,16 @@ final Version semanticRuntimeVersion = Version.parse(runtimeVersion);
 ///
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens:
+/// - 2020.04.22
 /// - 2020.04.07
-/// - 2020.03.24
-final String gcBeforeRuntimeVersion = '2020.03.09';
+final String gcBeforeRuntimeVersion = '2020.03.24';
 
 /// The versions which contain data that we should not fall back to.
 final blacklistedRuntimeVersions = ['2019.12.05', '2019.12.05+1'];
 
 // keep in-sync with SDK version in .travis.yml, .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '2.7.0';
-final String toolEnvSdkVersion = '2.7.1';
+final String toolEnvSdkVersion = '2.8.0';
 
 // Value comes from package:pana.
 final String panaVersion = pana.packageVersion;
