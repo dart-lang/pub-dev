2937493802919b8a31e4c192dd2c352d92b3845c
István Soós <isoos@users.noreply.github.com>
2020-05-27 09:57:54 +0200

    Upgrade dartdoc to 0.32.0 (#3659)
    * Upgrade dartdoc to 0.32.0
    
    * Fix versions test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@ Important changes to data-models, configuration and migrations between each
 AppEngine version, listed here to ease deployment and troubleshooting.
 
 ## Next Release (replace with git tag when deployed)
+ * Bumped runtimeVersion to `2020.05.26`.
+ * Upgraded dartdoc to `0.32.0`.
 
 ## `20200526t143103-all`
  * Bumped runtimeVersion to `2020.05.15`.
--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -23,10 +23,10 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2020.05.15', // The current [runtimeVersion].
+  '2020.05.26', // The current [runtimeVersion].
+  '2020.05.15',
   '2020.05.08',
   '2020.05.03',
-  '2020.04.22',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
@@ -63,7 +63,7 @@ final String flutterVersion = '1.17.1';
 final Version semanticFlutterVersion = Version.parse(flutterVersion);
 
 // keep in-sync with pkg/pub_dartdoc/pubspec.yaml
-final String dartdocVersion = '0.31.0';
+final String dartdocVersion = '0.32.0';
 final Version semanticDartdocVersion = Version.parse(dartdocVersion);
 
 // Version that control the dartdoc serving.
