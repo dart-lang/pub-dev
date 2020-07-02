0b554f8ebc617d9b717a5d6613cb0b61aea98f60
István Soós <isoos@users.noreply.github.com>
2020-07-02 12:38:58 +0200

    Upgrade Flutter SDK and pana. (#3764)

--- app/test/shared/versions_test.dart
+++ app/test/shared/versions_test.dart
@@ -33,7 +33,7 @@ void main() {
     // This test is a reminder that if pana, the SDK or any of the above
     // versions change, we should also adjust the [runtimeVersion]. Before
     // updating the hash value, double-check if it is being updated.
-    expect(hash, 48055325);
+    expect(hash, 111136121);
   });
 
   test('accepted runtime versions should be lexicographically ordered', () {
