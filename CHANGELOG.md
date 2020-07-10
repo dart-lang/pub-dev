16acfb61ad148d6a5148bcc759642457ed3cadbc
István Soós <isoos@users.noreply.github.com>
2020-07-10 16:09:11 +0200

    Upgrade pana (#3814)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@ Important changes to data-models, configuration and migrations between each
 AppEngine version, listed here to ease deployment and troubleshooting.
 
 ## Next Release (replace with git tag when deployed)
+ * Bumped runtimeVersion to `2020.07.10`.
+ * Upgraded pana to `0.13.15`
 
 ## `20200707t190831-all`
  * Bumped runtimeVersion to `2020.07.07`.
--- app/test/shared/versions_test.dart
+++ app/test/shared/versions_test.dart
@@ -33,7 +33,7 @@ void main() {
     // This test is a reminder that if pana, the SDK or any of the above
     // versions change, we should also adjust the [runtimeVersion]. Before
     // updating the hash value, double-check if it is being updated.
-    expect(hash, 297326303);
+    expect(hash, 13846371);
   });
 
   test('accepted runtime versions should be lexicographically ordered', () {
