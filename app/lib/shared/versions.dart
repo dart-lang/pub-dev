7fbd9a5402d162bc801193721227cf91c2e36920
Istvan Soos <istvan.soos@gmail.com>
2020-08-07 08:55:06 +0200

    Upgrade runtime SDK.

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,9 +21,9 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2020.08.05', // The current [runtimeVersion].
+  '2020.08.07', // The current [runtimeVersion].
+  '2020.08.05',
   '2020.08.03',
-  '2020.07.21',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
@@ -52,7 +52,7 @@ bool shouldGCVersion(String version) =>
     version.compareTo(gcBeforeRuntimeVersion) < 0;
 
 // keep in-sync with SDK version in .travis.yml, .mono_repo.yml and Dockerfile
-final String runtimeSdkVersion = '2.8.1';
+final String runtimeSdkVersion = '2.9.0';
 final String toolEnvSdkVersion = '2.9.0';
 
 // Value comes from package:pana.
