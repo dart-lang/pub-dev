91b9ae85ba09be6032928f29ed1936d24f2f64c8
Istvan Soos <istvan.soos@gmail.com>
2020-09-29 10:21:32 +0200

    Upgrade analysis Dart SDK

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,9 +21,9 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2020.09.16', // The current [runtimeVersion].
+  '2020.09.28', // The current [runtimeVersion].
+  '2020.09.16',
   '2020.09.09',
-  '2020.09.01',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
@@ -53,7 +53,7 @@ bool shouldGCVersion(String version) =>
 
 // keep in-sync with SDK version in .travis.yml, .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '2.9.0';
-final String toolEnvSdkVersion = '2.9.3';
+final String toolEnvSdkVersion = '2.10.0';
 
 // Value comes from package:pana.
 final String panaVersion = pana.packageVersion;
