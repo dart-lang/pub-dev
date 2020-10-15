67658191f0e4de48014b71a50e24935c691309fe
István Soós <isoos@users.noreply.github.com>
2020-10-15 14:06:38 +0200

    Upgrade Dart analysis SDK (#4145)

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,9 +21,9 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2020.10.09', // The current [runtimeVersion].
+  '2020.10.15', // The current [runtimeVersion].
+  '2020.10.09',
   '2020.10.06',
-  '2020.10.01',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
@@ -53,7 +53,7 @@ bool shouldGCVersion(String version) =>
 
 // keep in-sync with SDK version in .travis.yml, .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '2.10.0';
-final String toolEnvSdkVersion = '2.10.1';
+final String toolEnvSdkVersion = '2.10.2';
 
 // Value comes from package:pana.
 final String panaVersion = pana.packageVersion;
