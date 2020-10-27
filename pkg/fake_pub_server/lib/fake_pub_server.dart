c5e0e8bf01c223208e046b7bc491f5a7c6a8130c
István Soós <isoos@users.noreply.github.com>
2020-10-27 11:24:30 +0100

    HTML validation in fake pub server's responses. (#4179)
    * HTML validation in fake pub server's responses.
    
    * Update app/lib/tool/html/html_validation.dart
    
    Co-authored-by: Jonas Finnemann Jensen <jopsen@gmail.com>
    
    Co-authored-by: Jonas Finnemann Jensen <jopsen@gmail.com>

--- pkg/fake_pub_server/lib/fake_pub_server.dart
+++ pkg/fake_pub_server/lib/fake_pub_server.dart
@@ -27,6 +27,7 @@ import 'package:pub_dev/service/services.dart';
 import 'package:pub_dev/service/spam/backend.dart';
 import 'package:pub_dev/shared/configuration.dart';
 import 'package:pub_dev/shared/handler_helpers.dart';
+import 'package:pub_dev/tool/html/html_validation.dart';
 
 final _logger = Logger('fake_server');
 
@@ -63,11 +64,12 @@ class FakePubServer {
           final server = await IOServer.bind('localhost', port);
           serveRequests(server.server, (request) async {
             return await ss.fork(() async {
-              final rs = await extraHandler(request);
-              if (rs != null) {
-                return rs;
-              } else {
-                return await handler(request);
+              final rs = await extraHandler(request) ?? await handler(request);
+              try {
+                return await _validateAndWrapResponse(request, rs) ?? rs;
+              } catch (e, st) {
+                _logger.severe('Response validation failed', e, st);
+                return shelf.Response.internalServerError();
               }
             }) as shelf.Response;
           });
@@ -83,4 +85,22 @@ class FakePubServer {
     });
     _logger.info('closed');
   }
+}
+
+// Normally this method could be just void, however reading the shelf.Response's
+// body forces us to return a new instance of the response.
+Future<shelf.Response> _validateAndWrapResponse(
+    shelf.Request rq, shelf.Response rs) async {
+  if (rs == null) return null;
+
+  final ct = rs.headers[HttpHeaders.contentTypeHeader];
+  if (ct == null || ct.isEmpty) {
+    // TODO: force headers everywhere and throw exception instead of logging
+    _logger.warning('Content type header is missing for ${rq.requestedUri}.');
+  } else if (ct.contains('text/html')) {
+    final body = await rs.readAsString();
+    parseAndValidateHtml(body);
+    return rs.change(body: body);
+  }
+  return null;
 }
