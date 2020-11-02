133435d13aadb0ee1d6e8ef7f6a5f2be7436c3e1
István Soós <isoos@users.noreply.github.com>
2020-11-02 11:31:59 +0100

    Upgrade Flutter SDK (#4206)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@ Important changes to data-models, configuration and migrations between each
 AppEngine version, listed here to ease deployment and troubleshooting.
 
 ## Next Release (replace with git tag when deployed)
+ * Bumped runtimeVersion to `2020.11.02`.
+ * Upgraded Flutter to `1.22.3`.
 
 ## `20201029t121523-all`
  * Bumped runtimeVersion to `2020.10.28`.
--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,9 +21,9 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2020.10.28', // The current [runtimeVersion].
+  '2020.11.02', // The current [runtimeVersion].
+  '2020.10.28',
   '2020.10.19',
-  '2020.10.15',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
@@ -59,7 +59,7 @@ final String toolEnvSdkVersion = '2.10.3';
 final String panaVersion = pana.packageVersion;
 final Version semanticPanaVersion = Version.parse(panaVersion);
 
-final String flutterVersion = '1.22.2';
+final String flutterVersion = '1.22.3';
 final Version semanticFlutterVersion = Version.parse(flutterVersion);
 
 // keep in-sync with pkg/pub_dartdoc/pubspec.yaml
