fb655a9a7f88a18fbb79e74f3945f4c3830fd10c
István Soós <isoos@users.noreply.github.com>
2020-11-06 13:41:18 +0100

    Upgrade dartdoc (#4218)

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,7 +21,7 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2020.11.02', // The current [runtimeVersion].
+  '2020.11.05', // The current [runtimeVersion].
   '2020.10.28',
   '2020.10.19',
 ];
@@ -63,7 +63,7 @@ final String flutterVersion = '1.22.3';
 final Version semanticFlutterVersion = Version.parse(flutterVersion);
 
 // keep in-sync with pkg/pub_dartdoc/pubspec.yaml
-final String dartdocVersion = '0.35.0';
+final String dartdocVersion = '0.36.2';
 final Version semanticDartdocVersion = Version.parse(dartdocVersion);
 
 // Version that control the dartdoc serving.
