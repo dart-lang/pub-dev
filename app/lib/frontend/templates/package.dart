f496658af1ea686d6f7a3a3d81bb6ac8f4442a17
Istvan Soos <istvan.soos@gmail.com>
2020-11-11 14:09:32 +0100

    Show option only if it is on the latest stable page.

--- app/lib/frontend/templates/package.dart
+++ app/lib/frontend/templates/package.dart
@@ -246,7 +246,8 @@ String _renderPkgTitleContent(PackagePageData data) {
   return templateCache.renderTemplate('pkg/title_content', {
     'package': data.package.name,
     'version': data.version.version,
-    'show_prerelease': data.package.showPrereleaseVersion,
+    'show_prerelease':
+        data.isLatestStable && data.package.showPrereleaseVersion,
     'prerelease_version': data.package.latestPrereleaseVersion,
   });
 }
