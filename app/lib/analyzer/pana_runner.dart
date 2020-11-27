31a02e830982c2ce5f52707a6648d11446e37967
István Soós <isoos@users.noreply.github.com>
2020-11-27 16:41:19 +0100

    Deprecating PanaReport.pkgDependencies (#4301)

--- app/lib/analyzer/pana_runner.dart
+++ app/lib/analyzer/pana_runner.dart
@@ -138,6 +138,8 @@ PanaReport panaReportFromSummary(Summary summary, {List<String> flags}) {
     reportStatus: reportStatus,
     derivedTags: summary?.tags,
     pkgDependencies: summary?.pkgResolution?.dependencies,
+    allDependencies:
+        summary?.pkgResolution?.dependencies?.map((d) => d.package)?.toList(),
     licenseFile: summary?.licenseFile,
     report: summary?.report,
     flags: flags,
