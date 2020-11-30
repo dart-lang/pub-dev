5f1e1f24f17a58a85d67ef8ac6c7239bc3520073
István Soós <isoos@users.noreply.github.com>
2020-11-30 15:01:16 +0100

    Apply suggestions from code review
    Co-authored-by: Jonas Finnemann Jensen <jopsen@gmail.com>

--- app/lib/package/models.dart
+++ app/lib/package/models.dart
@@ -63,15 +63,24 @@ class Package extends db.ExpandoModel<String> {
   @db.IntProperty(required: true)
   int likes;
 
+  /// Key referencing the [PackageVersion] for the latest version of this package, ordered by priority order of
+  /// semantic versioning, hence, deprioritizing prereleases.
   @db.ModelKeyProperty(propertyName: 'latest_version', required: true)
   db.Key latestVersionKey;
 
+  /// [DateTime] when the [PackageVersion] in [latestVersionKey] was published.
   @db.DateTimeProperty()
   DateTime latestPublished;
 
+  /// Reference to latest version of this package ordered by semantic versioning, including
+  /// prerelease versions of this package.
+  ///
+  /// Note. This is **not** the _latest prerelease version_, it is the _latest version_ without deprioritization of
+  /// prereleases. Hence, this might not be a prerelease version, if a newer non-prerelease exists.
   @db.ModelKeyProperty(propertyName: 'latest_dev_version')
   db.Key latestPrereleaseVersionKey;
 
+  /// DateTime at which point the `PackageVersion` referenced in [latestPrereleaseVersionKey] was published.
   @db.DateTimeProperty()
   DateTime latestPrereleasePublished;
 
