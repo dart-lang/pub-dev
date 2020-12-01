a9e23d5466cd0ce7d6b4a1173b860e2ad256fb22
Jonas Finnemann Jensen <jonasfj@google.com>
2020-12-01 15:23:04 +0100

    For one-off publisher creation (#4306)
    * Added a create publisher tool

---
+++ app/bin/tools/create_publisher.dart
@@ -0,0 +1,110 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:args/args.dart';
+import 'package:pub_dev/account/backend.dart';
+
+import 'package:pub_dev/history/models.dart';
+import 'package:pub_dev/publisher/models.dart';
+import 'package:pub_dev/service/entrypoint/tools.dart';
+import 'package:pub_dev/shared/datastore.dart';
+import 'package:pub_dev/shared/exceptions.dart';
+
+final _argParser = ArgParser()
+  ..addFlag('help', abbr: 'h', defaultsTo: false, help: 'Show help.')
+  ..addOption('publisher', help: 'name of publisher to create')
+  ..addOption('member', help: 'email of user to add')
+  ..addOption('admin', help: 'email of pub administrator who authorized this');
+
+/// Sets the private key value.
+Future main(List<String> args) async {
+  final argv = _argParser.parse(args);
+
+  if (argv['help'] as bool == true) {
+    print('Create a publisher using admin rights.');
+    print(_argParser.usage);
+    return;
+  }
+
+  final publisherId = argv['publisher'] as String;
+  final userEmail = argv['member'] as String;
+  final adminEmail = argv['admin'] as String;
+  if (publisherId == null || userEmail == null || adminEmail == null) {
+    print('--publisher, --member and --admin is required!');
+    print(_argParser.usage);
+    return;
+  }
+
+  await withProdServices(() async {
+    final user = await accountBackend.lookupUserByEmail(userEmail);
+    if (user == null) {
+      print('ERROR: unknown user: $userEmail');
+      return;
+    }
+    final admin = await accountBackend.lookupUserByEmail(adminEmail);
+    if (admin == null) {
+      print('ERROR: unknown user: $adminEmail');
+      return;
+    }
+
+    // Create the publisher
+    final now = DateTime.now().toUtc();
+    await withRetryTransaction(dbService, (tx) async {
+      final key = dbService.emptyKey.append(Publisher, id: publisherId);
+      final p = await tx.lookupValue<Publisher>(key, orElse: () => null);
+      if (p != null) {
+        // Check that publisher is the same as what we would create.
+        if (p.created.isBefore(now.subtract(Duration(minutes: 10))) ||
+            p.updated.isBefore(now.subtract(Duration(minutes: 10))) ||
+            p.contactEmail != user.email ||
+            p.description != '' ||
+            p.websiteUrl != _publisherWebsite(publisherId)) {
+          throw ConflictException.publisherAlreadyExists(publisherId);
+        }
+        // Avoid creating the same publisher again, this end-point is idempotent
+        // if we just do nothing here.
+        return;
+      }
+
+      // Create publisher
+      tx.queueMutations(inserts: [
+        Publisher()
+          ..parentKey = dbService.emptyKey
+          ..id = publisherId
+          ..created = now
+          ..description = ''
+          ..contactEmail = user.email
+          ..updated = now
+          ..websiteUrl = _publisherWebsite(publisherId)
+          ..isAbandoned = false,
+        PublisherMember()
+          ..parentKey = dbService.emptyKey.append(Publisher, id: publisherId)
+          ..id = user.userId
+          ..userId = user.userId
+          ..created = now
+          ..updated = now
+          ..role = PublisherMemberRole.admin,
+        History.entry(
+          PublisherCreated(
+            publisherId: publisherId,
+            userId: admin.userId,
+            userEmail: admin.email,
+          ),
+        ),
+        History.entry(
+          MemberJoined(
+            publisherId: publisherId,
+            userId: user.userId,
+            userEmail: user.email,
+            role: PublisherMemberRole.admin,
+          ),
+        ),
+      ]);
+    });
+  });
+}
+
+String _publisherWebsite(String domain) => 'https://$domain/';
