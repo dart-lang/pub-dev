a5658d0b6bc4f47c6a46be656f3be6cdfbcc8dc3
Istvan Soos <istvan.soos@gmail.com>
2021-01-11 20:02:20 +0100

    SDK constraint on the /versions page.

--- app/lib/frontend/templates/package_versions.dart
+++ app/lib/frontend/templates/package_versions.dart
@@ -93,10 +93,13 @@ String renderPkgVersionsPage(
 }
 
 String renderVersionTableRow(PackageVersion version, String downloadUrl) {
+  final sdk = version.pubspec.sdkConstraint?.trim() ?? '';
   final versionData = {
     'package': version.package,
     'version': version.version,
     'version_url': urls.pkgPageUrl(version.package, version: version.version),
+    'has_sdk': sdk.isNotEmpty,
+    'sdk': sdk,
     'short_created': version.shortCreated,
     'dartdocs_url':
         _attr(urls.pkgDocUrl(version.package, version: version.version)),
