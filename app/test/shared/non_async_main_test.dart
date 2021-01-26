d75865e5645398eeef4cccec7856e93c1e3fbf5f
Istvan Soos <istvan.soos@gmail.com>
2021-01-26 12:07:05 +0100

    Fix code coverage + future-proof test.

---
+++ app/test/shared/non_async_main_test.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:test/test.dart';
+
+void main() {
+  // code coverage will call main as test groups, which shouldn't be async
+  test('_test.dart files should have non-async main methods', () {
+    final files = Directory('test')
+        .listSync(recursive: true)
+        .whereType<File>()
+        .where((f) => f.path.endsWith('_test.dart'));
+    for (final file in files) {
+      final lines = file.readAsLinesSync();
+      final main = lines
+          .firstWhere((line) => line.contains(' main(') && line.contains('{'));
+      expect(main.contains('Future<'), isFalse);
+      expect(main.contains(' async '), isFalse);
+      expect(main.contains('void '), isTrue);
+    }
+  });
+}
