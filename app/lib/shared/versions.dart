fe33c1e1bd7d511830894e61d549a08f396b4149
Istvan Soos <istvan.soos@gmail.com>
2021-03-03 10:58:40 +0100

    Upgrade pana

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,9 +21,9 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2021.03.02', // The current [runtimeVersion].
+  '2021.03.03', // The current [runtimeVersion].
+  '2021.03.02',
   '2021.02.24',
-  '2021.02.12',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
