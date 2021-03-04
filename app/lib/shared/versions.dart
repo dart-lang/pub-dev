7c592cea2966eec47294282d59b13c2002341a83
István Soós <isoos@users.noreply.github.com>
2021-03-04 15:55:40 +0100

    Upgrade runtime SDK to 2.12.0 (#4583)

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,7 +21,8 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2021.03.03', // The current [runtimeVersion].
+  '2021.03.04', // The current [runtimeVersion].
+  '2021.03.03',
   '2021.03.02',
   '2021.02.24',
 ];
@@ -52,7 +53,7 @@ bool shouldGCVersion(String version) =>
     version.compareTo(gcBeforeRuntimeVersion) < 0;
 
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
-final String runtimeSdkVersion = '2.10.0';
+final String runtimeSdkVersion = '2.12.0';
 final String toolStableDartSdkVersion = '2.12.0';
 final String toolStableFlutterSdkVersion = '2.0.0';
 final String toolPreviewDartSdkVersion = '2.12.0';
