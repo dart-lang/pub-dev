2b5a996475bcfef706289bea141a0c9ef677d586
Marcin Niemira <marcin.niemira@gmail.com>
2021-03-22 22:35:31 +1100

    check for null

--- app/lib/shared/configuration.dart
+++ app/lib/shared/configuration.dart
@@ -278,7 +278,7 @@ class Configuration {
       return Configuration.prod();
     } else if (env.gcloudProject == 'dartlang-pub-dev') {
       return Configuration.staging();
-    } else if (env.configPath.isEmpty) {
+    } else if (env.configPath?.isEmpty ?? false) {
       throw Exception(
           'Unknown project id: ${env.gcloudProject}. Please setup env var GCLOUD_PROJECT or PUB_SERVER_CONFIG');
     } else if (File(env.configPath).existsSync()) {
