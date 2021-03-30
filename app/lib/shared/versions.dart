305396ac9f0dbaf8bf30567cc4a66890b3fb2547
Istvan Soos <istvan.soos@gmail.com>
2021-03-30 15:00:41 +0200

    Upgrade dartdoc to 0.41.0

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,9 +21,9 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2021.03.19', // The current [runtimeVersion].
+  '2021.03.30', // The current [runtimeVersion].
+  '2021.03.19',
   '2021.03.15',
-  '2021.03.03',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
@@ -63,7 +63,7 @@ final String panaVersion = pana.packageVersion;
 final Version semanticPanaVersion = Version.parse(panaVersion);
 
 // keep in-sync with pkg/pub_dartdoc/pubspec.yaml
-final String dartdocVersion = '0.40.0';
+final String dartdocVersion = '0.41.0';
 final Version semanticDartdocVersion = Version.parse(dartdocVersion);
 
 // Version that control the dartdoc serving.
