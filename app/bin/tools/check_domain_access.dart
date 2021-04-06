553c38653eaa01830df2c18f817751ab2b805e6d
Istvan Soos <istvan.soos@gmail.com>
2021-04-06 18:35:53 +0200

    lookup and connect test + connection reuse

--- app/bin/tools/check_domain_access.dart
+++ app/bin/tools/check_domain_access.dart
@@ -7,33 +7,77 @@ import 'dart:io';
 /// The script checks if the domains accessed by the pub client are available,
 /// and not blocked by any network infrastructure.
 Future<void> main(List<String> args) async {
-  final urls = <String>[
-    // package info
-    'https://pub.dev/api/packages/http',
-    'https://pub.dartlang.org/api/packages/http',
-    // package archive
-    'https://pub.dev/packages/http/versions/0.13.1.tar.gz',
-    'https://pub.dartlang.org/packages/http/versions/0.13.1.tar.gz',
-    'https://storage.googleapis.com/pub-packages/packages/http-0.13.1.tar.gz',
-  ];
+  print('Testing host resolves...');
+  await _checkHosts();
 
+  print('Testing one connection per request...');
+  await _checkUrls();
+
+  print('Testing connection reuse...');
   final iterations = args.isEmpty ? 10 : int.parse(args.first);
-
+  final client = HttpClient();
   for (var i = 0; i < iterations; i++) {
-    print('Iteration #${i + 1}/$iterations...');
-    for (final url in urls) {
-      await _checkUrlAccess(url);
-    }
+    print('- iteration #${i + 1}/$iterations...');
+    await _checkUrls(client: client);
   }
+  client.close();
+
   print('Done.');
 }
 
-Future<void> _checkUrlAccess(String url) async {
-  final client = HttpClient();
-  final rq = await client.getUrl(Uri.parse(url));
+final urls = <String>[
+  // package info
+  'https://pub.dev/api/packages/http',
+  'https://pub.dartlang.org/api/packages/http',
+  // package archive
+  'https://pub.dev/packages/http/versions/0.13.1.tar.gz',
+  'https://pub.dartlang.org/packages/http/versions/0.13.1.tar.gz',
+  'https://storage.googleapis.com/pub-packages/packages/http-0.13.1.tar.gz',
+];
+final uris = urls.map((u) => Uri.parse(u)).toList();
+final hosts = uris.map((u) => u.host).toSet().toList();
+
+Future<void> _checkHosts() async {
+  for (final host in hosts) {
+    for (final type in [InternetAddressType.IPv4, InternetAddressType.IPv6]) {
+      final typeStr = type == InternetAddressType.IPv4 ? 'IPv4' : 'IPv6';
+      try {
+        final addresses = await InternetAddress.lookup(host, type: type);
+        final failed = <InternetAddress>[];
+        for (final address in addresses) {
+          try {
+            final s = await Socket.connect(address, 443);
+            await s.close();
+          } catch (_) {
+            failed.add(address);
+          }
+        }
+        final successCount = addresses.length - failed.length;
+        print(
+            '- $host $typeStr resolve and connect succeeded: $successCount / ${addresses.length}');
+      } catch (e) {
+        print('- $host $typeStr lookup failed: $e');
+      }
+    }
+  }
+}
+
+Future<void> _checkUrls({HttpClient client}) async {
+  for (final uri in uris) {
+    await _checkUrlGetContent(uri, client: client);
+  }
+}
+
+Future<void> _checkUrlGetContent(Uri uri, {HttpClient client}) async {
+  final closeClient = client == null;
+  client ??= HttpClient();
+  final rq = await client.getUrl(uri);
   final rs = await rq.close();
   final bodyList = await rs.toList();
   final bodyLength = bodyList.map((e) => e.length).reduce((a, b) => a + b);
-  if (bodyLength <= 0) throw Exception('No body for $url');
-  if (rs.statusCode != 200) throw Exception('Failed to fetch $url');
+  if (bodyLength <= 0) throw Exception('No body for $uri');
+  if (rs.statusCode != 200) throw Exception('Failed to fetch $uri');
+  if (closeClient) {
+    client.close();
+  }
 }
