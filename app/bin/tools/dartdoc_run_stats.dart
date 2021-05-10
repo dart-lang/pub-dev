462dc023bfa3a92c9852e7cb8e474b34a13b8190
István Soós <isoos@users.noreply.github.com>
2021-05-10 10:47:19 +0200

    Aggregate size stats from DartdocRun (#4782)

--- app/bin/tools/dartdoc_run_stats.dart
+++ app/bin/tools/dartdoc_run_stats.dart
@@ -11,30 +11,46 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/args.dart';
-import 'package:meta/meta.dart';
 
 import 'package:pub_dev/dartdoc/models.dart';
 import 'package:pub_dev/service/entrypoint/tools.dart';
 import 'package:pub_dev/shared/datastore.dart';
 import 'package:pub_dev/shared/versions.dart';
 
+final _mb = 1024 * 1024;
+
 Future<void> main(List<String> args) async {
   final ArgParser parser = ArgParser()
     ..addOption('output', help: 'The report output file (or stdout otherwise)');
   final ArgResults argv = parser.parse(args);
 
-  final packageTops = <String, DartdocEntry>{};
+  final durations = <String, DartdocEntry>{};
+  final archives = <String, DartdocEntry>{};
+  final totals = <String, DartdocEntry>{};
 
   await withToolRuntime(() async {
     final query = dbService.query<DartdocRun>()
       ..filter('runtimeVersion =', runtimeVersion);
     var scanned = 0;
     await for (final dr in query.run()) {
-      final prev = packageTops[dr.package];
       final entry = dr.entry;
-      if (prev == null || prev.runDuration < entry.runDuration) {
-        packageTops[dr.package] = entry;
+
+      final prevDuration = durations[dr.package];
+      if (prevDuration == null ||
+          prevDuration.runDuration < entry.runDuration) {
+        durations[dr.package] = entry;
       }
+
+      final prevArchive = archives[dr.package];
+      if (prevArchive == null || prevArchive.archiveSize < entry.archiveSize) {
+        archives[dr.package] = entry;
+      }
+
+      final prevTotal = totals[dr.package];
+      if (prevTotal == null || prevTotal.totalSize < entry.totalSize) {
+        totals[dr.package] = entry;
+      }
+
       scanned++;
       if (scanned % 5000 == 0) {
         print('Scanned $scanned DartdocRun entities.');
@@ -42,37 +58,72 @@ Future<void> main(List<String> args) async {
     }
   });
 
-  final entries = packageTops.values.toList()
+  final topDurations = durations.values.toList()
     ..sort((a, b) => -a.runDuration.compareTo(b.runDuration));
+  final topArchives = archives.values.toList()
+    ..sort((a, b) => -a.archiveSize.compareTo(b.archiveSize));
+  final topTotals = totals.values.toList()
+    ..sort((a, b) => -a.totalSize.compareTo(b.totalSize));
 
   final topSeconds = <String, int>{};
-  for (final e in entries.take(100)) {
+  for (final e in topDurations.take(100)) {
     topSeconds[e.packageName] = e.runDuration.inSeconds;
   }
 
-  Map<String, int> sumBy({
-    @required int top,
+  Map<String, int> sum(
+    Iterable<DartdocEntry> entries,
     String Function(DartdocEntry e) keyFn,
-  }) {
-    final map = <String, int>{};
-    for (final e in entries.take(top)) {
+  ) {
+    final counts = <String, int>{};
+    for (final e in topDurations) {
       final key = keyFn(e) ?? '';
-      map[key] = (map[key] ?? 0) + 1;
+      counts[key] = (counts[key] ?? 0) + 1;
     }
-    return map;
+    return counts;
+  }
+
+  Map<String, int> buckets(
+    Iterable<DartdocEntry> entries,
+    int Function(DartdocEntry) valueFn, [
+    int resolution = 1,
+  ]) {
+    final counts = <String, int>{};
+    for (final e in entries) {
+      final value = valueFn(e);
+      final k = (value ~/ resolution) * resolution;
+      final key = '$k';
+      counts[key] = (counts[key] ?? 0) + 1;
+    }
+    return counts;
   }
 
   final report = <String, dynamic>{
     'slowest-100': {
       'seconds': topSeconds,
-      'usesFlutter': entries.take(100).where((e) => e.usesFlutter).length,
-      'dart-version': sumBy(top: 100, keyFn: (e) => e.sdkVersion),
-      'flutter-version': sumBy(top: 100, keyFn: (e) => e.flutterVersion),
+      'usesFlutter': topDurations.take(100).where((e) => e.usesFlutter).length,
+      'dart-version': sum(topDurations.take(100), (e) => e.sdkVersion),
+      'flutter-version': sum(topDurations.take(100), (e) => e.flutterVersion),
     },
     'slowest-1k': {
-      'usesFlutter': entries.take(1000).where((e) => e.usesFlutter).length,
-      'dart-version': sumBy(top: 1000, keyFn: (e) => e.sdkVersion),
-      'flutter-version': sumBy(top: 1000, keyFn: (e) => e.flutterVersion),
+      'usesFlutter': topDurations.take(1000).where((e) => e.usesFlutter).length,
+      'dart-version': sum(topDurations.take(1000), (e) => e.sdkVersion),
+      'flutter-version': sum(topDurations.take(1000), (e) => e.flutterVersion),
+    },
+    'archive-100': {
+      '10MB': buckets(topArchives.take(100), (e) => e.archiveSize ~/ _mb, 10),
+      '100MB': buckets(topArchives.take(100), (e) => e.archiveSize ~/ _mb, 100),
+    },
+    'archive-500': {
+      '10MB': buckets(topArchives.take(500), (e) => e.archiveSize ~/ _mb, 10),
+      '100MB': buckets(topArchives.take(500), (e) => e.archiveSize ~/ _mb, 100),
+    },
+    'total-100': {
+      '10MB': buckets(topTotals.take(100), (e) => e.totalSize ~/ _mb, 10),
+      '100MB': buckets(topTotals.take(100), (e) => e.totalSize ~/ _mb, 100),
+    },
+    'total-500': {
+      '10MB': buckets(topTotals.take(500), (e) => e.totalSize ~/ _mb, 10),
+      '100MB': buckets(topTotals.take(500), (e) => e.totalSize ~/ _mb, 100),
     },
   };
 
