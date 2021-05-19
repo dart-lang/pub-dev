fc6c0a2982ee1b42ec6f53e9026e79a6e62c7b86
Istvan Soos <istvan.soos@gmail.com>
2021-05-19 10:25:47 +0200

    Upgrade GitHub workflows with mono_repo 4.0.0

--- tool/ci.sh
+++ tool/ci.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-# Created with package:mono_repo v3.4.6
+# Created with package:mono_repo v4.0.0
 
 # Support built in commands on windows out of the box.
+# When it is a flutter repo (check the pubspec.yaml for "sdk: flutter")
+# then "flutter" is called instead of "pub".
+# This assumes that the Flutter SDK has been installed in a previous step.
 function pub() {
-  if [[ $TRAVIS_OS_NAME == "windows" ]]; then
-    command pub.bat "$@"
+  if grep -Fq "sdk: flutter" "${PWD}/pubspec.yaml"; then
+    if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+      command flutter.bat pub "$@"
+    else
+      command flutter pub "$@"
+    fi
   else
-    command pub "$@"
+    if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+      command pub.bat "$@"
+    else
+      command pub "$@"
+    fi
   fi
 }
 function dartfmt() {
@@ -58,7 +69,59 @@ for PKG in ${PKGS}; do
       echo
       echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
       case ${TASK} in
-      command)
+      command_00)
+        echo 'pub run test'
+        pub run test || EXIT_CODE=$?
+        ;;
+      command_01)
+        echo 'pub run test --test-randomize-ordering-seed=random'
+        pub run test --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        ;;
+      command_02)
+        echo 'dartanalyzer --fatal-infos .'
+        dartanalyzer --fatal-infos . || EXIT_CODE=$?
+        ;;
+      command_03)
+        echo 'dartfmt --dry-run --set-exit-if-changed .'
+        dartfmt --dry-run --set-exit-if-changed . || EXIT_CODE=$?
+        ;;
+      command_04)
+        echo 'pub run test test/all_test.dart --platform vm'
+        pub run test test/all_test.dart --platform vm || EXIT_CODE=$?
+        ;;
+      command_05)
+        echo 'pub run test test/all_test.dart --platform chrome'
+        pub run test test/all_test.dart --platform chrome || EXIT_CODE=$?
+        ;;
+      command_06)
+        echo 'pub global activate dart_coveralls && dart_coveralls report test/all_test.dart'
+        pub global activate dart_coveralls && dart_coveralls report test/all_test.dart || EXIT_CODE=$?
+        ;;
+      command_07)
+        echo 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 0'
+        xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 0 || EXIT_CODE=$?
+        ;;
+      command_08)
+        echo 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 1'
+        xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 1 || EXIT_CODE=$?
+        ;;
+      command_09)
+        echo 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 2'
+        xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 2 || EXIT_CODE=$?
+        ;;
+      command_10)
+        echo 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 3'
+        xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 3 || EXIT_CODE=$?
+        ;;
+      command_11)
+        echo 'xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 4'
+        xvfb-run -s "-screen 0 1024x768x24" pub run test --preset travis -x phantomjs --total-shards 5 --shard-index 4 || EXIT_CODE=$?
+        ;;
+      command_12)
+        echo 'pub run test --preset travis -x browser'
+        pub run test --preset travis -x browser || EXIT_CODE=$?
+        ;;
+      command_13)
         echo './build.sh'
         ./build.sh || EXIT_CODE=$?
         ;;
@@ -67,6 +130,18 @@ for PKG in ${PKGS}; do
         dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
         ;;
       dartanalyzer_1)
+        echo 'dartanalyzer --fatal-warnings .'
+        dartanalyzer --fatal-warnings . || EXIT_CODE=$?
+        ;;
+      dartanalyzer_2)
+        echo 'dartanalyzer --fatal-warnings --fatal-infos .'
+        dartanalyzer --fatal-warnings --fatal-infos . || EXIT_CODE=$?
+        ;;
+      dartanalyzer_3)
+        echo 'dartanalyzer .'
+        dartanalyzer . || EXIT_CODE=$?
+        ;;
+      dartanalyzer_4)
         echo 'dartanalyzer --fatal-infos --fatal-warnings bin/ lib/'
         dartanalyzer --fatal-infos --fatal-warnings bin/ lib/ || EXIT_CODE=$?
         ;;
@@ -102,19 +177,51 @@ for PKG in ${PKGS}; do
         echo 'pub run test --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '6~7p'`'
         pub run test --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '6~7p'` || EXIT_CODE=$?
         ;;
-      test_07)
+      test_08)
+        echo 'pub run test -x integration --test-randomize-ordering-seed=random'
+        pub run test -x integration --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        ;;
+      test_09)
+        echo 'pub run test -t integration --total-shards 5 --shard-index 0 --test-randomize-ordering-seed=random'
+        pub run test -t integration --total-shards 5 --shard-index 0 --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        ;;
+      test_10)
+        echo 'pub run test -t integration --total-shards 5 --shard-index 1 --test-randomize-ordering-seed=random'
+        pub run test -t integration --total-shards 5 --shard-index 1 --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        ;;
+      test_11)
+        echo 'pub run test -t integration --total-shards 5 --shard-index 2 --test-randomize-ordering-seed=random'
+        pub run test -t integration --total-shards 5 --shard-index 2 --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        ;;
+      test_12)
+        echo 'pub run test -t integration --total-shards 5 --shard-index 3 --test-randomize-ordering-seed=random'
+        pub run test -t integration --total-shards 5 --shard-index 3 --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        ;;
+      test_13)
+        echo 'pub run test -t integration --total-shards 5 --shard-index 4 --test-randomize-ordering-seed=random'
+        pub run test -t integration --total-shards 5 --shard-index 4 --test-randomize-ordering-seed=random || EXIT_CODE=$?
+        ;;
+      test_15)
+        echo 'pub run test --run-skipped -t presubmit-only test/ensure_build_test.dart'
+        pub run test --run-skipped -t presubmit-only test/ensure_build_test.dart || EXIT_CODE=$?
+        ;;
+      test_16)
+        echo 'pub run test -p chrome'
+        pub run test -p chrome || EXIT_CODE=$?
+        ;;
+      test_17)
         echo 'pub run test --run-skipped'
         pub run test --run-skipped || EXIT_CODE=$?
         ;;
-      test_08)
+      test_18)
         echo 'pub run test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '0~3p'`'
         pub run test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '0~3p'` || EXIT_CODE=$?
         ;;
-      test_09)
+      test_19)
         echo 'pub run test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '1~3p'`'
         pub run test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '1~3p'` || EXIT_CODE=$?
         ;;
-      test_10)
+      test_20)
         echo 'pub run test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '2~3p'`'
         pub run test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '2~3p'` || EXIT_CODE=$?
         ;;
