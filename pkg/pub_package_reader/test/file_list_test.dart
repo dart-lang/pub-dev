97599ae0cabe601b845d8c2305729a39935f073f
Istvan Soos <istvan.soos@gmail.com>
2021-06-08 19:13:25 +0200

    Add sdk constraint to tests.

--- pkg/pub_package_reader/test/file_list_test.dart
+++ pkg/pub_package_reader/test/file_list_test.dart
@@ -12,7 +12,8 @@ import '_tar_writer.dart';
 
 void main() {
   final minimalTextFiles = {
-    'pubspec.yaml': 'name: abc\nversion: 1.0.0',
+    'pubspec.yaml':
+        'name: abc\nversion: 1.0.0\nenvironment:\n  sdk: \'>=2.10.0 <3.0.0\'\n',
     'LICENSE': 'Copyright (c) 2021',
     'README.md': 'Example content',
     'CHANGELOG.md': 'Changes',
