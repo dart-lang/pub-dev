143d50a476eeeb6d2658c5a97cfd85e3057899c3
Istvan Soos <istvan.soos@gmail.com>
2021-06-08 17:11:12 +0200

    Upgrade dartdoc to 0.45

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,8 @@ Important changes to data-models, configuration and migrations between each
 AppEngine version, listed here to ease deployment and troubleshooting.
 
 ## Next Release (replace with git tag when deployed)
- * Bumped runtimeVersion to `2021.06.03`.
+ * Bumped runtimeVersion to `2021.06.08`.
+ * Upgraded dartdoc to `0.45.0`.
  * NOTE: Stopped creating `ScoreCardReport` entities.
 
 ## `20210603t155700-all`
--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,7 +21,7 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2021.06.03', // The current [runtimeVersion].
+  '2021.06.08', // The current [runtimeVersion].
   '2021.06.01',
   '2021.05.25',
 ];
@@ -63,7 +63,7 @@ final String panaVersion = pana.packageVersion;
 final Version semanticPanaVersion = Version.parse(panaVersion);
 
 // keep in-sync with pkg/pub_dartdoc/pubspec.yaml
-final String dartdocVersion = '0.44.0';
+final String dartdocVersion = '0.45.0';
 final Version semanticDartdocVersion = Version.parse(dartdocVersion);
 
 // Version that control the dartdoc serving.
