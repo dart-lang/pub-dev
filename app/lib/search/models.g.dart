f85119c948697902e6c8db3feca31e2383e20980
Istvan Soos <istvan.soos@gmail.com>
2021-06-16 12:31:26 +0200

    Generated code

--- app/lib/search/models.g.dart
+++ app/lib/search/models.g.dart
@@ -8,16 +8,21 @@ part of 'models.dart';
 
 SearchSnapshot _$SearchSnapshotFromJson(Map<String, dynamic> json) {
   return SearchSnapshot()
-    ..updated = DateTime.parse(json['updated'] as String)
-    ..documents = (json['documents'] as Map<String, dynamic>).map(
-      (k, e) =>
-          MapEntry(k, PackageDocument.fromJson(e as Map<String, dynamic>)),
+    ..updated = json['updated'] == null
+        ? null
+        : DateTime.parse(json['updated'] as String)
+    ..documents = (json['documents'] as Map<String, dynamic>)?.map(
+      (k, e) => MapEntry(
+          k,
+          e == null
+              ? null
+              : PackageDocument.fromJson(e as Map<String, dynamic>)),
     );
 }
 
 Map<String, dynamic> _$SearchSnapshotToJson(SearchSnapshot instance) =>
     <String, dynamic>{
-      'updated': instance.updated.toIso8601String(),
+      'updated': instance.updated?.toIso8601String(),
       'documents': instance.documents,
     };
 
