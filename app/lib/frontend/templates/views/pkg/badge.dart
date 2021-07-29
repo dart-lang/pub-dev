100f599df599e35ca0f709aa217653bdb3b19969
Istvan Soos <istvan.soos@gmail.com>
2021-07-29 14:47:49 +0200

    fix missing

---
+++ app/lib/frontend/templates/views/pkg/badge.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import '../../../dom/dom.dart' as d;
+
+d.Node renderPackageBadgeNode({
+  required String label,
+  String? title,
+  String? iconUrl,
+}) {
+  return d.span(
+    classes: ['package-badge'],
+    attributes: title != null ? <String, String>{'title': title} : null,
+    children: [
+      if (iconUrl != null) d.img(classes: ['package-badge-icon'], src: iconUrl),
+      d.text(label),
+    ],
+  );
+}
