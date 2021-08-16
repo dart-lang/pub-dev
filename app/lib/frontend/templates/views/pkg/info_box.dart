49609fa4e558e3008ab8c6dcd45bb9b1f156e271
István Soós <isoos@users.noreply.github.com>
2021-08-16 14:45:43 +0200

    Partial migration of package infobox templates. (#5005)
    * Partial migration of package infobox templates.
    
    * for-loop node list creation

---
+++ app/lib/frontend/templates/views/pkg/info_box.dart
@@ -0,0 +1,46 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:pana/models.dart' show LicenseFile;
+import 'package:pubspec_parse/pubspec_parse.dart' as pubspek;
+
+import '../../../../package/overrides.dart' show redirectPackageUrls;
+import '../../../../shared/urls.dart' as urls;
+import '../../../dom/dom.dart' as d;
+
+d.Node? licenseNode({
+  required LicenseFile? licenseFile,
+  required String? licenseUrl,
+}) {
+  if (licenseUrl == null) return null;
+  licenseFile ??= LicenseFile('LICENSE', 'unknown');
+  return d.fragment([
+    d.text('${licenseFile.shortFormatted} ('),
+    d.a(href: licenseUrl, text: licenseFile.path),
+    d.text(')'),
+  ]);
+}
+
+d.Node? dependencyListNode(Map<String, pubspek.Dependency>? dependencies) {
+  if (dependencies == null) return null;
+  final packages = dependencies.keys.toList()..sort();
+  if (packages.isEmpty) return null;
+  final nodes = <d.Node>[];
+  for (final p in packages) {
+    if (nodes.isNotEmpty) {
+      nodes.add(d.text(', '));
+    }
+    final dep = dependencies[p];
+    var href = redirectPackageUrls[p];
+    String? constraint;
+    if (href == null && dep is pubspek.HostedDependency) {
+      href = urls.pkgPageUrl(p);
+      constraint = dep.version.toString();
+    }
+    final node =
+        href == null ? d.text(p) : d.a(href: href, title: constraint, text: p);
+    nodes.add(node);
+  }
+  return d.fragment(nodes);
+}
