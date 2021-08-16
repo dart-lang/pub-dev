8349d9772aa1a9ebeb90ec5a91b49d3546475e41
Istvan Soos <istvan.soos@gmail.com>
2021-08-16 14:57:08 +0200

    Migrate account-related static templates.

---
+++ app/lib/frontend/templates/views/account/unauthenticated.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import '../../../dom/dom.dart' as d;
+
+d.Node unauthenticatedNode() {
+  return d.fragment([
+    d.h1(text: 'Authentication required'),
+    d.p(text: 'You need to be logged in to view this page.'),
+    d.p(
+        text: 'You can sign in using your Google account '
+            'by clicking on the top-right "Sign in" menu.'),
+    d.p(
+        text: 'If you have not signed in on pub.dev before, '
+            'a pub.dev profile will be automatically created upon sign in.'),
+  ]);
+}
