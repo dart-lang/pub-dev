8851a4f08ae46ef749f8d89b229157772e658211
Istvan Soos <istvan.soos@gmail.com>
2021-08-20 13:54:04 +0200

    generated file

--- pkg/pub_dartdoc_data/lib/pub_dartdoc_config.g.dart
+++ pkg/pub_dartdoc_data/lib/pub_dartdoc_config.g.dart
@@ -7,26 +7,25 @@ part of 'pub_dartdoc_config.dart';
 // **************************************************************************
 
 DartdocCustomizerConfig _$DartdocCustomizerConfigFromJson(
-    Map<String, dynamic> json) {
-  return DartdocCustomizerConfig(
-    packageName: json['packageName'] as String,
-    packageVersion: json['packageVersion'] as String,
-    isLatestStable: json['isLatestStable'] as bool,
-    docRootUrl: json['docRootUrl'] as String,
-    latestStableDocumentationUrl:
-        json['latestStableDocumentationUrl'] as String,
-    pubPackagePageUrl: json['pubPackagePageUrl'] as String,
-    dartLogoSvgUrl: json['dartLogoSvgUrl'] as String,
-    githubMarkdownCssUrl: json['githubMarkdownCssUrl'] as String,
-    gtmJsUrl: json['gtmJsUrl'] as String,
-    trustedTargetHosts: (json['trustedTargetHosts'] as List<dynamic>)
-        .map((e) => e as String)
-        .toList(),
-    trustedUrlSchemes: (json['trustedUrlSchemes'] as List<dynamic>)
-        .map((e) => e as String)
-        .toList(),
-  );
-}
+        Map<String, dynamic> json) =>
+    DartdocCustomizerConfig(
+      packageName: json['packageName'] as String,
+      packageVersion: json['packageVersion'] as String,
+      isLatestStable: json['isLatestStable'] as bool,
+      docRootUrl: json['docRootUrl'] as String,
+      latestStableDocumentationUrl:
+          json['latestStableDocumentationUrl'] as String,
+      pubPackagePageUrl: json['pubPackagePageUrl'] as String,
+      dartLogoSvgUrl: json['dartLogoSvgUrl'] as String,
+      githubMarkdownCssUrl: json['githubMarkdownCssUrl'] as String,
+      gtmJsUrl: json['gtmJsUrl'] as String,
+      trustedTargetHosts: (json['trustedTargetHosts'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      trustedUrlSchemes: (json['trustedUrlSchemes'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+    );
 
 Map<String, dynamic> _$DartdocCustomizerConfigToJson(
         DartdocCustomizerConfig instance) =>
