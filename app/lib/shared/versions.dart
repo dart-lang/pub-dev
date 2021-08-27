1a5d3b01f1a554dc5982f276568149ab290bd2a3
Istvan Soos <istvan.soos@gmail.com>
2021-08-27 12:22:23 +0200

    Upgrade dependencies to latest resolvable version.

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -21,9 +21,9 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// Make sure that at least two versions are kept here as the next candidates
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
-  '2021.08.25', // The current [runtimeVersion].
+  '2021.08.27', // The current [runtimeVersion].
+  '2021.08.25',
   '2021.08.17',
-  '2021.08.12',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
