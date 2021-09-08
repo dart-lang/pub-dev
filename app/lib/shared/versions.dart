206e36c09322e729b055f73ce605e9700e72aca9
Istvan Soos <istvan.soos@gmail.com>
2021-09-08 22:14:06 +0200

    revert runtime sdk version

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -52,7 +52,7 @@ bool shouldGCVersion(String version) =>
     version.compareTo(gcBeforeRuntimeVersion) < 0;
 
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
-final String runtimeSdkVersion = '2.14.1';
+final String runtimeSdkVersion = '2.13.4';
 final String toolStableDartSdkVersion = '2.14.1';
 final String toolStableFlutterSdkVersion = '2.5.0';
 final String toolPreviewDartSdkVersion = '2.14.1';
