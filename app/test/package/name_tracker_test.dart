6db02ce007e02c1a0a789582344a38256ad0c8bc
Istvan Soos <istvan.soos@gmail.com>
2021-10-04 17:19:47 +0200

    Consider (approximate) singular/plural name as conflicting.

--- app/test/package/name_tracker_test.dart
+++ app/test/package/name_tracker_test.dart
@@ -6,26 +6,45 @@ import 'package:pub_dev/package/name_tracker.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final nameTracker = NameTracker(null);
-  nameTracker.add('json');
-  nameTracker.add('j_son');
+  group('json', () {
+    final nameTracker = NameTracker(null);
+    // main package
+    nameTracker.add('json');
+    // package was added before publishing was blocked.
+    nameTracker.add('j_son');
 
-  test('new package', () async {
-    expect(await nameTracker.accept('new_package'), isTrue);
+    test('new package can be published', () async {
+      expect(await nameTracker.accept('new_package'), isTrue);
+    });
+
+    test('existing package is accepted and can be published again', () async {
+      expect(await nameTracker.accept('json'), isTrue);
+      expect(await nameTracker.accept('j_son'), isTrue);
+    });
+
+    test('conflicting package: same name', () async {
+      expect(await nameTracker.accept('j_s_o_n'), isFalse);
+      expect(await nameTracker.accept('js_on'), isFalse);
+      expect(await nameTracker.accept('jso_n'), isFalse);
+      expect(await nameTracker.accept('json_'), isFalse);
+      expect(await nameTracker.accept('_json'), isFalse);
+      expect(await nameTracker.accept('_json_'), isFalse);
+      expect(await nameTracker.accept('_j_s_o_n_'), isFalse);
+    });
+
+    test('conflicting package: plural', () async {
+      expect(await nameTracker.accept('jsons'), isFalse);
+      expect(await nameTracker.accept('json__s'), isFalse);
+    });
   });
 
-  test('existing package', () async {
-    expect(await nameTracker.accept('json'), isTrue);
-    expect(await nameTracker.accept('j_son'), isTrue);
-  });
+  group('isolate', () {
+    final nameTracker = NameTracker(null);
+    nameTracker.add('isolates');
 
-  test('conflicting package', () async {
-    expect(await nameTracker.accept('j_s_o_n'), isFalse);
-    expect(await nameTracker.accept('js_on'), isFalse);
-    expect(await nameTracker.accept('jso_n'), isFalse);
-    expect(await nameTracker.accept('json_'), isFalse);
-    expect(await nameTracker.accept('_json'), isFalse);
-    expect(await nameTracker.accept('_json_'), isFalse);
-    expect(await nameTracker.accept('_j_s_o_n_'), isFalse);
+    test('conflicting package: singular', () async {
+      expect(await nameTracker.accept('isolate'), isFalse);
+      expect(await nameTracker.accept('iso_late'), isFalse);
+    });
   });
 }
