aacd481a51462048612df27c79cbcc63d45e4f43
Istvan Soos <istvan.soos@gmail.com>
2021-10-07 09:28:53 +0200

    revert todo

--- app/lib/package/model_properties.dart
+++ app/lib/package/model_properties.dart
@@ -107,6 +107,7 @@ class Pubspec {
   SdkConstraintStatus get _sdkConstraintStatus =>
       SdkConstraintStatus.fromSdkVersion(_inner.environment?['sdk'], name);
 
+  // TODO: migrate uses to SdkConstraintStatus.isDart2Compatible
   bool get supportsOnlyLegacySdk => !_sdkConstraintStatus.isDart2Compatible;
 
   /// Whether the pubspec file contains a flutter.plugin entry.
