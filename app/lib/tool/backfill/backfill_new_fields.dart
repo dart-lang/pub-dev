546b1c97c7e9912a62579b072b49d49a67dbaa73
Istvan Soos <istvan.soos@gmail.com>
2021-11-26 13:00:46 +0100

    Correct published timestamps in Package entities.

--- app/lib/tool/backfill/backfill_new_fields.dart
+++ app/lib/tool/backfill/backfill_new_fields.dart
@@ -24,20 +24,79 @@ Future<void> _backfillPackages() async {
   // during which a new version could be published. To prevent bad updates,
   // the count is not used in such cases, and backfill is re-run every time
   // a race has been detected.
-  var updating = true;
-  while (updating) {
-    updating = false;
-    final query = dbService.query<Package>();
-    await for (final p in query.run()) {
-      final race = await _backfillPackage(p);
-      if (race) updating = true;
+  var wasRace = true;
+  while (wasRace) {
+    wasRace = false;
+
+    await for (final p in dbService.query<Package>().run()) {
+      if (await _packageVersionCount(p)) {
+        wasRace = true;
+      }
+    }
+
+    await for (final p in dbService.query<Package>().run()) {
+      if (await _packagePublishedTimestamps(p)) {
+        wasRace = true;
+      }
     }
   }
 }
 
-/// Returns true if a race has been detected while the Package entity
-/// was being verified and updated.
-Future<bool> _backfillPackage(Package p) async {
+/// Corrects the Package's published timestamps.
+/// Returns true if a race has been detected while the entity was updated.
+Future<bool> _packagePublishedTimestamps(Package p) async {
+  final versions = await packageBackend.listVersionsCached(p.name!);
+  final stable = versions.versions
+      .firstWhere((v) => v.version == p.latestVersion)
+      .published;
+  final prerelease = versions.versions
+      .firstWhere((v) => v.version == p.latestPrereleaseVersion)
+      .published;
+  final preview = versions.versions
+      .firstWhere((v) => v.version == p.latestPreviewVersion)
+      .published;
+  final last = versions.versions
+      .map((e) => e.published!)
+      .reduce((a, b) => a.isBefore(b) ? b : a);
+
+  // check if any update is needed
+  if (p.latestPublished == stable &&
+      p.latestPrereleasePublished == prerelease &&
+      p.latestPreviewPublished == preview &&
+      !p.lastVersionPublished!.isBefore(last)) {
+    return false;
+  }
+
+  return await withRetryTransaction(dbService, (tx) async {
+    final v = await tx.lookupValue<Package>(p.key);
+
+    // sanity checks for parallel updates during the version count
+    if (v.versionCount != p.versionCount ||
+        p.updated != v.updated ||
+        p.lastVersionPublished != v.lastVersionPublished ||
+        p.likes != v.likes ||
+        p.latestPublished != v.latestPublished ||
+        p.latestPrereleasePublished != v.latestPrereleasePublished ||
+        p.latestPreviewPublished != v.latestPreviewPublished ||
+        p.lastVersionPublished != v.lastVersionPublished) {
+      _logger.info('[backfill-published-timestamps-race] "${v.name}"');
+      return true;
+    }
+
+    v.latestPublished = stable;
+    v.latestPrereleasePublished = prerelease;
+    v.latestPreviewPublished = preview;
+    if (v.lastVersionPublished!.isBefore(last)) {
+      v.lastVersionPublished = last;
+    }
+    v.updated = DateTime.now().toUtc();
+    return false;
+  });
+}
+
+/// Backfills the Package's `versionCount` field.
+/// Returns true if a race has been detected while the entity was updated.
+Future<bool> _packageVersionCount(Package p) async {
   final versions = await packageBackend.versionsOfPackage(p.name!);
   final count = versions.length;
   final countUntilLastPublished = versions
