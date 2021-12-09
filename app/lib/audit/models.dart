420219b4c6db1c03fc9bcdc21b3a59275db15660
Sarah Zakarias <zarah@google.com>
2021-12-09 14:57:46 +0100

    Log package retraction (#5340)

--- app/lib/audit/models.dart
+++ app/lib/audit/models.dart
@@ -96,6 +96,8 @@ class AuditLogRecord extends db.ExpandoModel<String> {
   /// This is the list of package version who should have this record show up in
   /// their audit-log.
   ///
+  /// The format of the list entries is `<package>/<version>`.
+  ///
   /// This property **MUST** only be used for querying. The contents of this property is
   /// **NOT** intended for consumption or presentation by other means. It's only here
   /// to make it easy to find [AuditLogRecord]s relevant for a specific package version.
@@ -150,6 +152,30 @@ class AuditLogRecord extends db.ExpandoModel<String> {
       ..users = [user.userId]
       ..packages = [package]
       ..packageVersions = []
+      ..publishers = [];
+  }
+
+  factory AuditLogRecord.packageVersionOptionsUpdated({
+    required String package,
+    required String version,
+    required User user,
+    required List<String> options,
+  }) {
+    final optionsStr = options.map((o) => '`$o`').join(', ');
+    return AuditLogRecord._init()
+      ..kind = AuditLogRecordKind.packageVersionOptionsUpdated
+      ..agent = user.userId
+      ..summary = '`${user.email}` updated $optionsStr of '
+          'package `$package` version `$version`.'
+      ..data = {
+        'package': package,
+        'version': version,
+        'user': user.email,
+        'options': options,
+      }
+      ..users = [user.userId]
+      ..packages = [package]
+      ..packageVersions = ['$package/$version']
       ..publishers = [];
   }
 
@@ -596,6 +622,9 @@ class AuditLogRecord extends db.ExpandoModel<String> {
 abstract class AuditLogRecordKind {
   /// Event that a package was updated with new options
   static const packageOptionsUpdated = 'package-options-updated';
+
+  /// Event that a package version was updated with new options
+  static const packageVersionOptionsUpdated = 'package-version-options-updated';
 
   /// Event that a package was published.
   ///
