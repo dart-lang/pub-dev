30469eef61f3e8aa634bb30d86b7d9d8079d98f5
István Soós <isoos@users.noreply.github.com>
2022-01-05 14:49:39 +0100

    show:hidden checkbox on new search UI (#5380)

--- pkg/pub_integration/test/search_update_test.dart
+++ pkg/pub_integration/test/search_update_test.dart
@@ -174,19 +174,19 @@ void main() {
           expect(i6.openSections, i5.openSections);
           expect(page.url, '$origin/packages?q=platform%3Aandroid+pkg');
 
-          // include discontinued
+          // show hidden
           await page.click('.search-form-section[data-section-tag="advanced"]');
           await Future.delayed(Duration(seconds: 1));
-          await page.click('#search-form-checkbox-discontinued');
+          await page.click('#search-form-checkbox-show-hidden');
           await page.waitForNavigation(wait: Until.networkIdle);
           final i7 = await listingPageInfo(page);
           expect(i7.totalCount, i6.totalCount);
           expect(i7.openSections, ['sdks', 'advanced']);
           expect(page.url,
-              '$origin/packages?q=platform%3Aandroid+pkg&discontinued=1');
+              '$origin/packages?q=platform%3Aandroid+pkg+show%3Ahidden');
 
           // remove discontinued
-          await page.click('#search-form-checkbox-discontinued');
+          await page.click('#search-form-checkbox-show-hidden');
           await page.waitForNavigation(wait: Until.networkIdle);
           expect(page.url, '$origin/packages?q=platform%3Aandroid+pkg');
 
