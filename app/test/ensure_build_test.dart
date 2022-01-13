979d58e73ce3fbb200ba56bac61a85c44baa38c2
Kevin Moore <kevmoo@users.noreply.github.com>
2022-01-13 14:18:38 -0800

    Give ensure_build tests 2x time to finish - they can be slow (#5427)

--- app/test/ensure_build_test.dart
+++ app/test/ensure_build_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@Timeout.factor(2)
 @Tags(['presubmit-only'])
 
 import 'package:build_verify/build_verify.dart';
