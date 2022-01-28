ce53df80bdfbda1aa08c1b54e7ee9a8c898f78a1
István Soós <isoos@users.noreply.github.com>
2022-01-28 16:49:51 +0100

    Log requests with non-appengine context. (#5470)

--- app/lib/shared/utils.dart
+++ app/lib/shared/utils.dart
@@ -354,12 +354,13 @@ String createUuid([List<int>? bytes]) {
 ///
 /// Returns `null` otherwise.
 Map<String, String>? cloudTraceHeaders() {
-  // TODO: remove this once `context` gets fixed to nullable.
   try {
     if (context.traceId == null) return null;
     return {_cloudTraceContextHeader: context.traceId!};
-  } catch (_) {
-    // no-op
+  } catch (e, st) {
+    // TODO: remove try-catch and logging, once `context` gets fixed
+    //       (either in appengine, or in our fake tests).
+    _logger.warning('Request with non-appengine context detected.', e, st);
   }
 }
 
