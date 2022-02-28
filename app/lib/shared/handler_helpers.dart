5e5f120d7e7b99cb7ba2b25f22dd91e8915cb0d9
István Soós <isoos@users.noreply.github.com>
2022-02-28 14:28:39 +0100

    Better override for cache headers in a user session. (#5506)
    * Better override for cache headers in a user session.
    
    * Only override public

--- app/lib/shared/handler_helpers.dart
+++ app/lib/shared/handler_helpers.dart
@@ -274,9 +274,22 @@ shelf.Handler _userSessionWrapper(Logger logger, shelf.Handler handler) {
     }
     shelf.Response rs = await handler(request);
     if (userSessionData != null) {
-      // Indicates that the response is intended for a single user and must not
-      // be stored by a shared cache. A private cache may store the response.
-      rs = rs.change(headers: {HttpHeaders.cacheControlHeader: 'private'});
+      // Responses for a user session are intended for a single user and must not
+      // be stored by a shared, public cache. A private cache may store the response.
+      //
+      // - replacing `public` to `private`
+      // - otherwise, keeping parameters, e.g. max-age
+      final original = rs.headers[HttpHeaders.cacheControlHeader] ?? '';
+      final parts = original
+          .split(',')
+          .map((e) => e.trim())
+          .where((e) => e.isNotEmpty)
+          .map((e) => e == 'public' ? 'private' : e)
+          .toList();
+      if (parts.isNotEmpty) {
+        final newValue = parts.join(', ');
+        rs = rs.change(headers: {HttpHeaders.cacheControlHeader: newValue});
+      }
     }
     return rs;
   };
