a79fdd792d7791ec9aa6a88a168b01460f15c3c1
Istvan Soos <istvan.soos@gmail.com>
2022-03-01 14:24:46 +0100

    Enable direct userId merge in app/bin/tools/user_merger.dart

--- app/bin/tools/user_merger.dart
+++ app/bin/tools/user_merger.dart
@@ -12,6 +12,11 @@ final _argParser = ArgParser()
   ..addOption('concurrency',
       abbr: 'c', defaultsTo: '10', help: 'Number of concurrent processing.')
   ..addOption('oauth-user-id', help: 'The specific OAuthUserId object to fix.')
+  ..addOption('from-user-id',
+      help: 'The User that will be removed. (must be in pair with to-user-id)')
+  ..addOption('to-user-id',
+      help:
+          'The User that will be extended. (must be in pair with from-user-id)')
   ..addFlag('help', abbr: 'h', defaultsTo: false, help: 'Show help.');
 
 int? concurrency;
@@ -20,17 +25,28 @@ Future main(List<String> args) async {
   final argv = _argParser.parse(args);
   if (argv['help'] as bool) {
     print('Usage: dart user_merger.dart --oauth-user-id <id>');
+    print(
+        'Usage: dart user_merger.dart --from-user-id <id> --to-user-id <other-id>');
     print(_argParser.usage);
     return;
   }
 
   concurrency = int.parse(argv['concurrency'] as String);
   final oauthUserId = argv['oauth-user-id'] as String?;
+  final fromUserId = argv['from-user-id'] as String?;
+  final toUserId = argv['to-user-id'] as String?;
 
   await withToolRuntime(() async {
     final userMerger = UserMerger(db: dbService, concurrency: concurrency);
     if (oauthUserId != null) {
+      if (fromUserId != null || toUserId != null) {
+        throw Exception('`from-user-id` or `to-user-id` must not be specified');
+      }
       await userMerger.fixOAuthUserID(oauthUserId);
+    } else if (fromUserId != null && toUserId != null) {
+      await userMerger.mergeUser(fromUserId, toUserId);
+    } else if (fromUserId != null || toUserId != null) {
+      throw Exception('`from-user-id` and `to-user-id` must be used together');
     } else {
       await userMerger.fixAll();
     }
