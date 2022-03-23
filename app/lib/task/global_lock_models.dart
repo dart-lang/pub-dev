934ebb8479b92450c69485a92b997d194f0c989c
Jonas Finnemann Jensen <jonasfj@google.com>
2022-03-23 14:45:28 +0100

    Introduce GlobalLock

---
+++ app/lib/task/global_lock_models.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:pub_dev/shared/datastore.dart' as db;
+
+/// [GlobalLockState] is used for locking in a distributed system.
+///
+///  * `id`, is the `lockId`.
+///  * `GlobalLockState` entities never has a parent.
+@db.Kind(name: 'GlobalLockState', idType: db.IdType.String)
+class GlobalLockState extends db.ExpandoModel<String> {
+  /// Get the lockId
+  String? get lockId => id;
+
+  /// Unique ULID identifying the claim currently holding the lock.
+  ///
+  /// Empty, if not currently locked.
+  @db.StringProperty(required: true)
+  String? claimId;
+
+  /// DateTime at which point the lock becomes free again.
+  @db.DateTimeProperty(required: true)
+  DateTime? lockedUntil;
+}
