f96607a46394e4e4c82dece874b67a734521a062
Jonas Finnemann Jensen <jonasfj@google.com>
2022-03-23 14:48:14 +0100

    Abstractions for managing a pool of VMs executing tasks

---
+++ app/lib/task/cloudcompute/cloudcompute.dart
@@ -0,0 +1,91 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// Interface describing an instance.
+abstract class CloudInstance {
+  /// Unique instance name.
+  String get name;
+
+  /// Zone this instance lives in.
+  String get zone;
+
+  /// [DateTime] of when the instance was created.
+  DateTime get created;
+
+  /// State of the instance.
+  InstanceState get state;
+
+  /// Representation for debugging purposes.
+  @override
+  String toString() => 'CloudInstance(${[
+        'name: $name',
+        'zone: $zone',
+        'created: $created',
+        'state: $state',
+      ].join(',')})';
+}
+
+/// Simplified instance state.
+///
+/// In practice we often don't care if an instance _provisioning_ or _staging_.
+/// If an instance is stuck in the [pending] state it's probably because it's
+/// not going to startup.
+///
+/// Similarly, we don't care if an instance is _booting_, _running_, or if the
+/// process running on the instance crashed without terminating the instance.
+/// In all these cases we want delete the instance if it gets stuck long
+/// enought to exhaust allowed execution time.
+///
+/// We also don't care if the instance is _suspending_, _suspended_, _stopping_,
+/// _stopped_ or _terminated_. In these the cases the instance should be
+/// deleted.
+enum InstanceState {
+  /// Instance is pending provisioning or in the process of being provisioned.
+  pending,
+
+  /// Instance is believed to be running (or booting).
+  running,
+
+  /// Instance has been terminated.
+  terminated,
+}
+
+/// Interface for controlling a cloud compute region.
+abstract class CloudCompute {
+  /// List of zones available.
+  List<String> get zones;
+
+  /// Generate an instance name.
+  String generateInstanceName();
+
+  /// Create an instance running [dockerImage] with given [arguments].
+  ///
+  /// The instance will be running in the given [zone] under the given [name].
+  /// Notice that [name] must be unique and will have implementation specific
+  /// limitations, using [generateInstanceName] is adviced.
+  ///
+  /// The _human readable_ [description] will attached to the instance in
+  /// Cloud Console and intended to assist humans inspecting the system.
+  ///
+  /// The future returned may take a long time to resolve (several minutes).
+  /// Any [Exception] thrown by this method should be considered a reason to
+  /// reduce the request rate on the given zone [zone].
+  Future<CloudInstance> createInstance({
+    required String zone,
+    required String name,
+    required String dockerImage,
+    required List<String> arguments,
+    required String description,
+  });
+
+  /// List instances.
+  Stream<CloudInstance> listInstances();
+
+  /// Delete the instance with [name] from [zone].
+  ///
+  /// If this operation fails, it may throw an [Exception] or discard the error.
+  /// Systems should assume deletion is best-effort and periodically list
+  /// instances to delete instances that are no longer needed.
+  Future<void> delete(String zone, String name);
+}
---
+++ app/test/task/cloudcompute/googlecloudcompute_test.dart
@@ -0,0 +1,86 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Test is fragile because creating instances and waiting for them to start is
+// slow, and something that could throw all sorts of exceptions, we don't want
+// run this as part of normal testing.
+@Tags(['fragile'])
+import 'dart:async';
+import 'dart:io' show Platform;
+
+import 'package:appengine/appengine.dart';
+import 'package:pub_dev/task/cloudcompute/cloudcompute.dart';
+import 'package:pub_dev/task/cloudcompute/googlecloudcompute.dart';
+import 'package:test/test.dart';
+
+import '../../shared/test_services.dart';
+
+void main() {
+  test('CloudCompute from createGoogleCloudCompute()', () async {
+    setupLogging();
+
+    await withAppEngineServices(() async {
+      // Create CloudCompute instance
+      final gce = createGoogleCloudCompute(
+        client: authClientService,
+        project: Platform.environment['GOOGLE_CLOUD_PROJECT']!,
+        poolLabel: 'manual-testing',
+      );
+
+      // Fail if any instances exist
+      var instances = await gce.listInstances().toList();
+      expect(instances, isEmpty);
+
+      // Create instance that terminates
+      scheduleMicrotask(() async {
+        print('creating instance');
+        final instance = await gce.createInstance(
+          name: gce.generateInstanceName(),
+          zone: gce.zones.first,
+          dockerImage: 'busybox:1.31.1',
+          arguments: [
+            'sh',
+            '-c',
+            'whoami; date -R; sleep 5s; date -R; uname -a',
+          ],
+          description: 'test instance that terminates rather quickly',
+        );
+        print('Created instance: ${instance.name}, $instance');
+      });
+
+      // Wait until we have a terminated instance
+      print('### Wait for instance to terminate on its own');
+      while (!instances.any((i) => i.state == InstanceState.terminated)) {
+        instances = await gce.listInstances().toList();
+        print('listInstances():');
+        for (final inst in instances) {
+          print(' - ${inst.name}, state: ${inst.state}');
+        }
+        await Future.delayed(Duration(seconds: 1));
+      }
+      // Delete instances
+      print('### Delete all instances');
+      for (final inst in instances) {
+        await gce.delete(inst.zone, inst.name);
+      }
+
+      // Wait until instances are deleted from listing
+      print('### Wait for instance to disappear from listings');
+      while (instances.isNotEmpty) {
+        instances = await gce.listInstances().toList();
+        print('listInstances():');
+        for (final inst in instances) {
+          print(' - ${inst.name}, state: ${inst.state}');
+        }
+        await Future.delayed(Duration(seconds: 1));
+      }
+    });
+  },
+      timeout: Timeout.parse('30m'),
+      skip: Platform.environment['GOOGLE_CLOUD_PROJECT'] != null &&
+              // Avoid running against production by accident
+              Platform.environment['GOOGLE_CLOUD_PROJECT'] != 'dartlang-pub'
+          ? false
+          : 'createGoogleCloudCompute testing requires GOOGLE_CLOUD_PROJECT');
+}
