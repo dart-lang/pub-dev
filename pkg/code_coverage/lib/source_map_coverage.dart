7744a401ea9d88628b73246084354e5b2add4e8b
Istvan Soos <istvan.soos@gmail.com>
2022-04-12 14:45:14 +0200

    Fix processing code coverage from puppeteer.

--- pkg/code_coverage/lib/source_map_coverage.dart
+++ pkg/code_coverage/lib/source_map_coverage.dart
@@ -9,25 +9,34 @@ import 'package:meta/meta.dart';
 import 'package:path/path.dart';
 import 'package:source_maps/parser.dart';
 
+final _pathHashRegexp = RegExp('/static/(hash-[a-z0-9]+/)');
+
 /// Converts the browser-provided JS and CSS coverage information (using the
 /// compiled source maps) into LCOV format.
 Future<void> main() async {
-  final files = Directory('build/puppeteer').listSync().whereType<File>();
-  for (final file in files) {
-    if (file.path.endsWith('.js.json')) {
-      final name = basename(file.path);
+  final scriptFiles = Directory('../../static/js')
+      .listSync()
+      .map((e) => e.path)
+      .where((e) => e.endsWith('.js.map'))
+      .map((e) => e.substring(5, e.length - 4))
+      .toList();
+  final coverageFiles =
+      Directory('build/puppeteer').listSync(recursive: true).whereType<File>();
+  for (final coverageFile in coverageFiles) {
+    if (coverageFile.path.endsWith('.js.json')) {
+      final name = basename(coverageFile.path);
       await _process(
-        resourceUri: '/static/js/script.dart.js',
-        coveragePath: file.path,
+        staticPaths: scriptFiles,
+        coveragePath: coverageFile.path,
         outputPath: 'build/lcov/puppeteer-$name.info',
       );
     }
 
-    if (file.path.endsWith('.css.json')) {
-      final name = basename(file.path);
+    if (coverageFile.path.endsWith('.css.json')) {
+      final name = basename(coverageFile.path);
       await _process(
-        resourceUri: '/static/css/style.css',
-        coveragePath: file.path,
+        staticPaths: ['/static/css/style.css'],
+        coveragePath: coverageFile.path,
         outputPath: 'build/lcov/puppeteer-$name.info',
       );
     }
@@ -35,77 +44,89 @@ Future<void> main() async {
 }
 
 Future<void> _process({
-  @required Pattern resourceUri,
-  String compiledPath,
-  String mapPath,
   @required String coveragePath,
   @required String outputPath,
+  @required List<String> staticPaths,
 }) async {
-  compiledPath ??= '../..$resourceUri';
-  mapPath ??= '$compiledPath.map';
-
-  final sm = parse(File(mapPath).readAsStringSync()) as SingleMapping;
-
-  // Initialize line counts with 0 for all the known lines.
-  final sourceCoverage = <String, Map<int, int>>{};
-  for (final e in sm.lines.expand((l) => l.entries)) {
-    if (e.sourceUrlId == null) continue;
-    final sourceUrl = sm.urls[e.sourceUrlId];
-    if (sourceUrl.startsWith('org-dartlang-sdk:')) continue;
-    final counts = sourceCoverage.putIfAbsent(sourceUrl, () => <int, int>{});
-    counts[e.sourceLine] = 0;
-  }
-
-  // Returns the source map location for [line] and [column] in the compiled file.
-  // [line] and [column] is starting from 1
-
-  // Returns `null` if the position is invalid or if there is no mapping.
-  TargetEntry _sourceLines(int line, int column) {
-    try {
-      final entries = sm.lines[line - 1].entries;
-      // TargetEntry.column is 0-indexed
-      return entries.lastWhere((e) => e.column < column, orElse: () => null);
-    } catch (_) {
-      return null;
-    }
-  }
-
-  final compiled = File(compiledPath).readAsStringSync();
-  final compiledLines = compiled.split('\n');
-
-  // Returns the compiled code source position for [offset].
-  _Position _compiledPosition(int offset) {
-    int line = 0;
-    while (line < compiledLines.length && offset > compiledLines[line].length) {
-      offset -= compiledLines[line].length + 1;
-      line++;
-    }
-    return _Position(line + 1, offset + 1);
-  }
-
   // load coverages
-  final coverageRoot = json.decode(File(coveragePath).readAsStringSync())
+  final origCoverageRoot = json.decode(File(coveragePath).readAsStringSync())
       as Map<String, dynamic>;
-  final coverageKey = coverageRoot.keys
-      .firstWhere((key) => resourceUri.allMatches(key).isNotEmpty);
-  final coverage = coverageRoot[coverageKey] as Map<String, dynamic>;
-  final rangesList = (coverage['ranges'] as List).cast<Map<String, dynamic>>();
+  final coverageRoot = origCoverageRoot
+      .map((k, v) => MapEntry(k.replaceAll(_pathHashRegexp, '/static/'), v));
 
-  // process coverages
-  rangesList
-      .map((r) => _Range(r['start'] as int, r['end'] as int))
-      .expand((r) => List<int>.generate(r.end - r.start, (i) => i + r.start))
-      .map(_compiledPosition)
-      .map((s) => _sourceLines(s.line, s.column))
-      .where((e) => e != null && e.sourceUrlId != null)
-      .forEach(
-    (e) {
+  // source line coverage counter
+  final sourceCoverage = <String, Map<int, int>>{};
+
+  for (final staticPath in staticPaths) {
+    final compiledPath = '../..$staticPath';
+    final mapPath = '$compiledPath.map';
+
+    final sm = parse(File(mapPath).readAsStringSync()) as SingleMapping;
+
+    // Initialize line counts with 0 for all the known lines.
+    for (final e in sm.lines.expand((l) => l.entries)) {
+      if (e.sourceUrlId == null) continue;
       final sourceUrl = sm.urls[e.sourceUrlId];
-      if (sourceUrl.startsWith('org-dartlang-sdk:')) return;
+      if (sourceUrl.startsWith('org-dartlang-sdk:')) continue;
       final counts = sourceCoverage.putIfAbsent(sourceUrl, () => <int, int>{});
-      counts[e.sourceLine] = 1;
-    },
-  );
+      counts[e.sourceLine] ??= 0;
+    }
+
+    // Returns the source map location for [line] and [column] in the compiled file.
+    // [line] and [column] is starting from 1
+
+    // Returns `null` if the position is invalid or if there is no mapping.
+    TargetEntry _sourceLines(int line, int column) {
+      try {
+        final entries = sm.lines[line - 1].entries;
+        // TargetEntry.column is 0-indexed
+        return entries.lastWhere((e) => e.column < column, orElse: () => null);
+      } catch (_) {
+        return null;
+      }
+    }
+
+    final compiled = File(compiledPath).readAsStringSync();
+    final compiledLines = compiled.split('\n');
+
+    // Returns the compiled code source position for [offset].
+    _Position _compiledPosition(int offset) {
+      int line = 0;
+      while (
+          line < compiledLines.length && offset > compiledLines[line].length) {
+        offset -= compiledLines[line].length + 1;
+        line++;
+      }
+      return _Position(line + 1, offset + 1);
+    }
+
+    final coverageKeys = coverageRoot.keys
+        .where((key) => staticPath.allMatches(key).isNotEmpty)
+        .toList();
+    for (final coverageKey in coverageKeys) {
+      final coverage = coverageRoot[coverageKey] as Map<String, dynamic>;
+      final rangesList =
+          (coverage['ranges'] as List).cast<Map<String, dynamic>>();
+
+      // process coverages
+      rangesList
+          .map((r) => _Range(r['start'] as int, r['end'] as int))
+          .expand(
+              (r) => List<int>.generate(r.end - r.start, (i) => i + r.start))
+          .map(_compiledPosition)
+          .map((s) => _sourceLines(s.line, s.column))
+          .where((e) => e != null && e.sourceUrlId != null)
+          .forEach(
+        (e) {
+          final sourceUrl = sm.urls[e.sourceUrlId];
+          if (sourceUrl.startsWith('org-dartlang-sdk:')) return;
+          final counts =
+              sourceCoverage.putIfAbsent(sourceUrl, () => <int, int>{});
+          counts[e.sourceLine] = 1;
+        },
+      );
+    }
+  }
 
   // format results in the LCOV block format
   final sources = sourceCoverage.keys.toList()..sort();
