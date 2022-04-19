2bb772f89cca7dfc48e3b42313acaa1dfb30c778
Istvan Soos <istvan.soos@gmail.com>
2022-04-19 13:20:35 +0200

    Migrate orphaned like removal tool to periodic task.

---
+++ app/lib/tool/maintenance/remove_orphaned_likes.dart
@@ -0,0 +1,69 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:clock/clock.dart';
+import 'package:logging/logging.dart';
+
+import '../../account/backend.dart';
+import '../../account/models.dart';
+import '../../package/backend.dart';
+import '../../shared/datastore.dart';
+
+final _logger = Logger('remove_orphaned_likes');
+final _minAgeThreshold = Duration(days: 1);
+
+/// Removes Like entities with non-existing Package or User.
+/// Only removes entities that are present for more than a day.
+Future<void> removeOrphanedLikes() async {
+  _logger.info('Scanning for orphaned likes...');
+  final existingUserIds = <String>{};
+  final existingPackages = <String>{};
+
+  Future<bool> isUserIdMissing(String userId) async {
+    if (existingUserIds.contains(userId)) return true;
+    final user = await accountBackend.lookupUserById(userId);
+    if (user != null) {
+      existingUserIds.add(userId);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  Future<bool> isPackageMissing(String package) async {
+    if (existingPackages.contains(package)) return true;
+    final p = await packageBackend.lookupPackage(package);
+    if (p != null) {
+      existingPackages.add(package);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  final counts = await dbService.deleteWithQuery<Like>(
+    dbService.query<Like>(),
+    where: (like) async {
+      final age = clock.now().difference(like.created!);
+      if (age < _minAgeThreshold) {
+        // Do not check likes that are younger than the threshold to prevent eventual consistency issues.
+        return false;
+      }
+
+      if (await isUserIdMissing(like.userId)) {
+        // TODO: investigate if we need to recalculate the like count for the packages.
+        _logger.info(
+            'Removing like for package `${like.package}` because userId `${like.userId}` is missing.');
+        return true;
+      }
+      if (await isPackageMissing(like.package)) {
+        _logger.info(
+            'Removing like for userId `${like.userId}` because package `${like.package}` is missing.');
+        return true;
+      }
+      return false;
+    },
+  );
+  _logger.info('Removed ${counts.deleted} orhaned likes.');
+}
