da53d806f13d72340cccf87060357a8a07c88044
Istvan Soos <istvan.soos@gmail.com>
2022-04-27 14:06:04 +0200

    Upgrade analysis SDKs

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -22,10 +22,10 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2022.04.21',
+  '2022.04.27',
   // Fallback runtime versions.
+  '2022.04.21',
   '2022.03.21',
-  '2022.03.08',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
@@ -56,8 +56,8 @@ bool shouldGCVersion(String version) =>
 final String runtimeSdkVersion = '2.16.1';
 final String toolStableDartSdkVersion = '2.16.2';
 final String toolStableFlutterSdkVersion = '2.10.5';
-final String toolPreviewDartSdkVersion = '2.17.0-266.1.beta';
-final String toolPreviewFlutterSdkVersion = '2.13.0-0.1.pre';
+final String toolPreviewDartSdkVersion = '2.17.0-266.5.beta';
+final String toolPreviewFlutterSdkVersion = '2.13.0-0.2.pre';
 
 final semanticToolStableDartSdkVersion =
     Version.parse(toolStableDartSdkVersion);
