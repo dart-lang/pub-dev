82b3c3ec3bd3a30d3c382b8abf911b7c1046a20c
Istvan Soos <istvan.soos@gmail.com>
2022-05-05 13:46:16 +0200

    import

--- app/lib/frontend/templates/views/pkg/info_box.dart
+++ app/lib/frontend/templates/views/pkg/info_box.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:pana/pana.dart';
-import 'package:pub_dev/frontend/templates/views/pkg/license.dart';
 import 'package:pubspec_parse/pubspec_parse.dart' as pubspek;
 
 import '../../../../package/models.dart';
