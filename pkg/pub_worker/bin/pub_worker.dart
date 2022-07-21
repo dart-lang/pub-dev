01a1309ace8844264d7db73b062671503d68373c
Jonas Finnemann Jensen <jonasfj@google.com>
2022-07-21 11:31:27 +0200

    Add logging to pub_worker

--- pkg/pub_worker/bin/pub_worker.dart
+++ pkg/pub_worker/bin/pub_worker.dart
@@ -6,12 +6,16 @@ import 'dart:async';
 import 'dart:convert' show json;
 import 'dart:io' show exit;
 
+import 'package:clock/clock.dart';
+import 'package:logging/logging.dart';
 import 'package:pub_worker/payload.dart';
 import 'package:pub_worker/src/analyze.dart' show analyze;
 
 void _printUsage() => print('Usage: pub_worker.dart <JSON_PAYLOAD>');
 
 Future<void> main(List<String> args) async {
+  _setupLogging();
+
   if (args.length != 1) {
     _printUsage();
     exit(1);
@@ -31,4 +35,21 @@ Future<void> main(List<String> args) async {
   // process, and this must terminate the container. It is the responsibility
   // of the cloud-init configuration to launch the container and shutdown after
   // the container exits.
+}
+
+/// Setup logging to stdout
+void _setupLogging() {
+  // TODO: Consider reducing the log-level or supporting a --verbosity option.
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    final time = clock.now();
+    for (final line in rec.message.split('\n')) {
+      print('$time [${rec.loggerName}] ${rec.level.name}: $line');
+    }
+    if (rec.error != null) {
+      for (final line in '${rec.error}, ${rec.stackTrace}'.split('\n')) {
+        print('$time [${rec.loggerName}] ${rec.level.name}: ERROR: $line');
+      }
+    }
+  });
 }
