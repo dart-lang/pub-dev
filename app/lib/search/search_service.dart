950464d57822d0bc2e160af24d8bba0d0e8e125d
Istvan Soos <istvan.soos@gmail.com>
2022-08-29 16:22:02 +0200

    additional documentation

--- app/lib/search/search_service.dart
+++ app/lib/search/search_service.dart
@@ -140,6 +140,7 @@ class ServiceSearchQuery {
   final int? minPoints;
   final int? updatedInDays;
 
+  /// The value of the `sort` URL query parameter.
   final SearchOrder? order;
   final int? offset;
   final int? limit;
@@ -244,6 +245,11 @@ class ServiceSearchQuery {
     return map;
   }
 
+  /// The effective sort order to use:
+  /// - input text query's order takes precedence
+  /// - URL query sort [order] is used as a fallback.
+  ///
+  /// TODO: remove this field when [order] is removed.
   late final effectiveOrder = parsedQuery.order ?? order;
   bool get _hasQuery => query != null && query!.isNotEmpty;
   bool get _hasOnlyFreeText => _hasQuery && parsedQuery.hasOnlyFreeText;
