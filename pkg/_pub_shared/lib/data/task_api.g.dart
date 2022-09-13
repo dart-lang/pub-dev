e2a13981d24e85120bc5c78b4c17b00466571686
Jonas Finnemann Jensen <jonasfj@google.com>
2022-09-13 21:48:38 +0200

    Change UploadTaskResultResponse

--- pkg/_pub_shared/lib/data/task_api.g.dart
+++ pkg/_pub_shared/lib/data/task_api.g.dart
@@ -9,24 +9,15 @@ part of 'task_api.dart';
 UploadTaskResultResponse _$UploadTaskResultResponseFromJson(
         Map<String, dynamic> json) =>
     UploadTaskResultResponse(
-      dartdocBlobId: json['dartdocBlobId'] as String,
-      panaLogId: json['panaLogId'] as String,
-      dartdocBlob:
-          UploadInfo.fromJson(json['dartdocBlob'] as Map<String, dynamic>),
-      dartdocIndex:
-          UploadInfo.fromJson(json['dartdocIndex'] as Map<String, dynamic>),
-      panaLog: UploadInfo.fromJson(json['panaLog'] as Map<String, dynamic>),
-      panaReport:
-          UploadInfo.fromJson(json['panaReport'] as Map<String, dynamic>),
+      blobId: json['blobId'] as String,
+      blob: UploadInfo.fromJson(json['blob'] as Map<String, dynamic>),
+      index: UploadInfo.fromJson(json['index'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$UploadTaskResultResponseToJson(
         UploadTaskResultResponse instance) =>
     <String, dynamic>{
-      'dartdocBlobId': instance.dartdocBlobId,
-      'panaLogId': instance.panaLogId,
-      'dartdocBlob': instance.dartdocBlob,
-      'dartdocIndex': instance.dartdocIndex,
-      'panaLog': instance.panaLog,
-      'panaReport': instance.panaReport,
+      'blobId': instance.blobId,
+      'blob': instance.blob,
+      'index': instance.index,
     };
