59c482fe983bce6d5dbf471b3c4d898a2980ab9e
István Soós <isoos@users.noreply.github.com>
2022-09-15 10:13:51 +0200

    Upgrade pana and Flutter SDK (#6044)
    * Upgrade pana
    
    * also Flutter SDK

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -22,10 +22,10 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2022.09.08',
+  '2022.09.14',
   // Fallback runtime versions.
+  '2022.09.08',
   '2022.08.31',
-  '2022.08.26',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
@@ -55,9 +55,9 @@ bool shouldGCVersion(String version) =>
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '2.18.0';
 final String toolStableDartSdkVersion = '2.18.0';
-final String toolStableFlutterSdkVersion = '3.3.1';
+final String toolStableFlutterSdkVersion = '3.3.2';
 final String toolPreviewDartSdkVersion = '2.18.0';
-final String toolPreviewFlutterSdkVersion = '3.3.1';
+final String toolPreviewFlutterSdkVersion = '3.3.2';
 
 final semanticToolStableDartSdkVersion =
     Version.parse(toolStableDartSdkVersion);
