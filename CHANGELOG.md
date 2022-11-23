3ee8865afda0db5c28d09b0d75cca64380d763f8
Istvan Soos <istvan.soos@gmail.com>
2022-11-23 18:15:04 +0100

    Upgrade stable Flutter SDK

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,8 @@ Important changes to data models, configuration, and migrations between each
 AppEngine version, listed here to ease deployment and troubleshooting.
 
 ## Next Release (replace with git tag when deployed)
- * Bumped runtimeVersion to `2022.11.21`.
- * Upgraded stable Flutter analysis SDK to `3.3.8`.
+ * Bumped runtimeVersion to `2022.11.23`.
+ * Upgraded stable Flutter analysis SDK to `3.3.9`.
  * Upgraded preview Flutter analysis SDK to ` 3.6.0-0.1.pre`.
  * Upgraded pana to `0.21.21
  * Upgraded dependencies.
--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -22,7 +22,7 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2022.11.21',
+  '2022.11.23',
   // Fallback runtime versions.
   '2022.11.10',
   '2022.11.04',
@@ -55,7 +55,7 @@ bool shouldGCVersion(String version) =>
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '2.18.0';
 final String toolStableDartSdkVersion = '2.18.4';
-final String toolStableFlutterSdkVersion = '3.3.8';
+final String toolStableFlutterSdkVersion = '3.3.9';
 final String toolPreviewDartSdkVersion = '2.19.0-255.2.beta';
 final String toolPreviewFlutterSdkVersion = '3.6.0-0.1.pre';
 
