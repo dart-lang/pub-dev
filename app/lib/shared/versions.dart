a83c90bdc5f5dffecde163a96bf776c59eafc82b
Istvan Soos <istvan.soos@gmail.com>
2023-01-11 11:39:31 +0100

    Upgrade Dart3 SDK.

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -22,10 +22,10 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2023.01.03',
+  '2023.01.11',
   // Fallback runtime versions.
+  '2023.01.03',
   '2022.12.16',
-  '2022.12.07',
 ];
 
 /// Represents a combined version of the overall toolchain and processing,
