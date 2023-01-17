99f622d9e1eeb2afa83edc649a2e2ff69e292bc3
Sarah Zakarias <zarah@google.com>
2023-01-17 11:48:59 +0100

    Hide previous image in carousel while waiting for the next (#6344)

--- pkg/web_app/lib/src/screenshot_carousel.dart
+++ pkg/web_app/lib/src/screenshot_carousel.dart
@@ -38,7 +38,9 @@ void _setEventForScreenshot() {
 
   void showImage(int index, UIEvent event) {
     event.stopPropagation();
-    imageElement!.src = images[index];
+    hideElement(imageElement!);
+    imageElement.src = images[index];
+
     if (index == images.length - 1) {
       hideElement(next);
     }
@@ -51,6 +53,10 @@ void _setEventForScreenshot() {
     if (index < images.length - 1) {
       showElement(next);
     }
+
+    imageElement.onLoad.listen((event) {
+      showElement(imageElement!);
+    });
   }
 
   int screenshotIndex = 0;
