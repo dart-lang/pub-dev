ce9aa584512202b8ed22efca4cfb8191366fcd31
Istvan Soos <istvan.soos@gmail.com>
2023-02-21 13:19:03 +0100

    Renamed to inviteUrlLogLine.

--- pkg/pub_integration/test/pub_integration_test.dart
+++ pkg/pub_integration/test/pub_integration_test.dart
@@ -29,10 +29,11 @@ void main() {
       final inviteUrlFuture = fakePubServerProcess
           .waitForLine((line) => line.contains('https://pub.dev/consent?id='));
       Future<void> inviteCompleterFn() async {
-        final pageUrl = await inviteUrlFuture.timeout(Duration(seconds: 30));
-        final pageUri = Uri.parse(
-            pageUrl.substring(pageUrl.indexOf('https://pub.dev/consent')));
-        final consentId = pageUri.queryParameters['id'];
+        final inviteUrlLogLine =
+            await inviteUrlFuture.timeout(Duration(seconds: 30));
+        final inviteUri = Uri.parse(inviteUrlLogLine
+            .substring(inviteUrlLogLine.indexOf('https://pub.dev/consent')));
+        final consentId = inviteUri.queryParameters['id'];
 
         // spoofed consent, trying to accept it with a different user
         final rs1 = await httpClient.put(
