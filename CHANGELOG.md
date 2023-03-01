0ce1eedf4c406bfb45252b630681b87e2670e617
Istvan Soos <istvan.soos@gmail.com>
2023-03-01 14:26:33 +0100

    Upgrade analysis SDKs.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,10 @@ Important changes to data models, configuration, and migrations between each
 AppEngine version, listed here to ease deployment and troubleshooting.
 
 ## Next Release (replace with git tag when deployed)
- * Bumped runtimeVersion to `2023.02.28`.
+ * Bumped runtimeVersion to `2023.02.29`.
+ * Upgraded stable Dart analysis SDK to `2.19.3`.
+ * Upgraded stable Flutter analysis SDK to `3.7.5`.
+ * Upgraded preview Dart analysis SDK to `3.0.0-218.1.dev`.
  * Removed `required: true` from the `UserSession.email` field.
 
 ## `20230223t105400-all`
--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -22,7 +22,7 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2023.02.28',
+  '2023.02.29',
   // Fallback runtime versions.
   '2023.02.22',
   '2023.02.14',
@@ -54,9 +54,9 @@ bool shouldGCVersion(String version) =>
 
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '2.19.0';
-final String toolStableDartSdkVersion = '2.19.2';
-final String toolStableFlutterSdkVersion = '3.7.4';
-final String toolPreviewDartSdkVersion = '3.0.0-218.0.dev';
+final String toolStableDartSdkVersion = '2.19.3';
+final String toolStableFlutterSdkVersion = '3.7.5';
+final String toolPreviewDartSdkVersion = '3.0.0-218.1.dev';
 final String toolPreviewFlutterSdkVersion = '3.8.0-10.1.pre';
 
 final semanticToolStableDartSdkVersion =
