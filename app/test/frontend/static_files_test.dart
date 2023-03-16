ef4e7ef170a09316bfcb4196cf4d7eed1931e766
Istvan Soos <istvan.soos@gmail.com>
2023-03-16 13:47:40 +0100

    Update static file test.

--- app/test/frontend/static_files_test.dart
+++ app/test/frontend/static_files_test.dart
@@ -220,7 +220,7 @@ void main() {
       final partsSize = parts
           .map((p) => cache.getFile(p)!.bytes.length)
           .reduce((a, b) => a + b);
-      expect((partsSize / 1024).round(), closeTo(226, 1));
+      expect((partsSize / 1024).round(), closeTo(228, 1));
     });
   });
 
