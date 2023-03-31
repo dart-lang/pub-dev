caf23c7cd3bf0a00aba3c5dda42386048ec52589
Jonas Finnemann Jensen <jonasfj@google.com>
2023-03-31 13:30:44 +0200

    Fix script

--- tool/codegen.sh
+++ tool/codegen.sh
@@ -6,14 +6,14 @@ cd "$SCRIPT_DIR/.."
 
 build() {
   pushd "$1"
-  dart run build_runner build
+  dart run build_runner build --delete-conflicting-outputs
   popd  
 }
 
-#build pkg/api_builder
-#build pkg/pub_dartdoc_data
-#build pkg/pub_worker
-#build pkg/_pub_shared
-#build app
+build pkg/api_builder
+build pkg/pub_dartdoc_data
+build pkg/pub_worker
+build pkg/_pub_shared
+build app
 
 cp app/lib/frontend/handlers/pubapi.client.dart pkg/_pub_shared/lib/src/pubapi.client.dart
