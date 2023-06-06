f1041e8de87a915f8068f2ac235b282fdc640ed2
István Soós <isoos@users.noreply.github.com>
2023-06-06 16:19:31 +0200

    Merging fake server process and headless env in integration tests. (#6657)
    * Merging fake server process and headless env in integration tests.
    
    * Rename to TestContextProvider

---
+++ pkg/pub_integration/lib/src/fake_test_context_provider.dart
@@ -0,0 +1,75 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+import 'dart:isolate';
+
+import 'package:_pub_shared/pubapi.dart';
+import 'package:puppeteer/puppeteer.dart';
+
+import 'fake_pub_server_process.dart';
+import 'fake_test_user.dart';
+import 'headless_env.dart';
+import 'test_scenario.dart';
+
+/// The timeout factor that should be used in integration tests.
+final testTimeoutFactor = 6;
+
+class TestContextProvider {
+  final String pubHostedUrl;
+  final FakePubServerProcess _fakePubServerProcess;
+  final HeadlessGroup _headlessGroup;
+
+  TestContextProvider._(
+      this.pubHostedUrl, this._fakePubServerProcess, this._headlessGroup);
+
+  static Future<TestContextProvider> start() async {
+    final fakePubServerProcess = await FakePubServerProcess.start();
+    await fakePubServerProcess.started;
+    final origin = 'http://localhost:${fakePubServerProcess.port}';
+
+    // creating a unique test name for coverage reports
+    final testName = [
+      Platform.script.pathSegments.last.replaceAll('.', '-'),
+      DateTime.now().millisecondsSinceEpoch,
+      Isolate.current.hashCode,
+    ].join('-');
+
+    final headlessGroup = HeadlessGroup(
+      origin: origin,
+      testName: testName,
+    );
+    return TestContextProvider._(origin, fakePubServerProcess, headlessGroup);
+  }
+
+  Future<void> close() async {
+    await _headlessGroup.close();
+    await _fakePubServerProcess.kill();
+  }
+
+  Future<TestUser> createAnonymousTestUser() async {
+    final headlessEnv = await _headlessGroup.createNewProfile();
+    return TestUser(
+      email: '',
+      api: PubApiClient(pubHostedUrl),
+      withBrowserPage: <T>(Future<T> Function(Page) fn) async {
+        return await headlessEnv.withPage<T>(fn: fn);
+      },
+      readLatestEmail: () async => throw UnimplementedError(),
+      createCredentials: () async => throw UnimplementedError(),
+    );
+  }
+
+  Future<TestUser> createTestUser({
+    required String email,
+    List<String>? scopes,
+  }) async {
+    return await createFakeTestUser(
+      email: email,
+      headlessEnv: await _headlessGroup.createNewProfile(),
+      fakeEmailReader: _fakePubServerProcess.fakeEmailReader,
+      scopes: scopes,
+    );
+  }
+}
--- pkg/pub_integration/test/publisher_test.dart
+++ pkg/pub_integration/test/publisher_test.dart
@@ -4,53 +4,36 @@
 
 import 'package:http/http.dart' as http;
 import 'package:pub_integration/script/publisher.dart';
-import 'package:pub_integration/src/fake_pub_server_process.dart';
-import 'package:pub_integration/src/fake_test_user.dart';
-import 'package:pub_integration/src/headless_env.dart';
+import 'package:pub_integration/src/fake_test_context_provider.dart';
 import 'package:pub_integration/src/pub_puppeteer_helpers.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('publisher', () {
-    late FakePubServerProcess fakePubServerProcess;
-    late final HeadlessGroup headlessGroup;
+    late final TestContextProvider fakeTestScenario;
     final httpClient = http.Client();
 
     setUpAll(() async {
-      fakePubServerProcess = await FakePubServerProcess.start();
-      await fakePubServerProcess.started;
+      fakeTestScenario = await TestContextProvider.start();
     });
 
     tearDownAll(() async {
-      await headlessGroup.close();
-      await fakePubServerProcess.kill();
+      await fakeTestScenario.close();
       httpClient.close();
     });
 
     test('publisher script', () async {
-      // start browser
-      headlessGroup = HeadlessGroup(
-        testName: 'browser',
-        origin: 'http://localhost:${fakePubServerProcess.port}',
-      );
-
       final script = PublisherScript(
-        pubHostedUrl: 'http://localhost:${fakePubServerProcess.port}',
-        adminUser: await createFakeTestUser(
+        pubHostedUrl: fakeTestScenario.pubHostedUrl,
+        adminUser: await fakeTestScenario.createTestUser(
           email: 'user@example.com',
-          headlessEnv: await headlessGroup.createNewProfile(),
-          fakeEmailReader: fakePubServerProcess.fakeEmailReader,
           scopes: [webmastersReadonlyScope],
         ),
-        invitedUser: await createFakeTestUser(
+        invitedUser: await fakeTestScenario.createTestUser(
           email: 'dev@example.com',
-          headlessEnv: await headlessGroup.createNewProfile(),
-          fakeEmailReader: fakePubServerProcess.fakeEmailReader,
         ),
-        unrelatedUser: await createFakeTestUser(
+        unrelatedUser: await fakeTestScenario.createTestUser(
           email: 'somebodyelse@example.com',
-          headlessEnv: await headlessGroup.createNewProfile(),
-          fakeEmailReader: fakePubServerProcess.fakeEmailReader,
         ),
       );
       await script.verify();
