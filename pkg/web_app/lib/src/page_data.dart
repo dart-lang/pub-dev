dd4a3dfa9eb4aa9b2bfa64fa2255f6873c8cd8f4
Parker Lougheed <parlough@gmail.com>
2023-08-15 01:37:20 -0500

    Remove page data print (#6911)

--- pkg/web_app/lib/src/page_data.dart
+++ pkg/web_app/lib/src/page_data.dart
@@ -6,27 +6,22 @@ import 'dart:html';
 
 import 'package:_pub_shared/data/page_data.dart';
 
-PageData? _data;
-
 /// The server-provided config/data for the current page.
 ///
 /// This is the <meta name="pub-page-data" content="[json-data]"> embedded in
 /// the `head` section of the HTML page.
-PageData get pageData {
-  return _data ??= _extractData() ?? PageData();
-}
+final PageData pageData = _extractData();
 
-PageData? _extractData() {
+PageData _extractData() {
   final meta = document.head?.querySelector('meta[name="pub-page-data"]');
   if (meta != null) {
     try {
       final text = meta.attributes['content']!;
       final map = pageDataJsonCodec.decode(text) as Map<String, dynamic>;
-      print(map);
       return PageData.fromJson(map);
     } catch (_) {
       // ignore exception
     }
   }
-  return null;
+  return PageData();
 }
