82ae3f13cd8f621dd38ccd2527c40b54c4a4a697
Istvan Soos <istvan.soos@gmail.com>
2023-08-16 16:45:54 +0200

    Remove experimental condition (dead code) from ScoreCard data loading.

--- app/lib/frontend/handlers/experimental.dart
+++ app/lib/frontend/handlers/experimental.dart
@@ -51,9 +51,6 @@ class ExperimentalFlags {
   /// Whether to return ScoreCard data from sandboxed output.
   bool get showSandboxedDartdoc => true;
 
-  /// Whether to return ScoreCard data from sandboxed output.
-  bool get showSandboxedScoreCard => true;
-
   bool get isEmpty => _enabled.isEmpty;
 
   bool isEnabled(String flag) {
