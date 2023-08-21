b789313a2d0b4cfdba78d1a83f614e421efbd323
Sarah Zakarias <zarah@google.com>
2023-08-21 14:25:05 +0200

    Replace with dummy data

--- app/test/service/security_advisory/sync_security_advisories_test.dart
+++ app/test/service/security_advisory/sync_security_advisories_test.dart
@@ -17,21 +17,17 @@ void main() {
         'service', 'security_advisory', 'testdata', 'adv1'));
     final (osvs, failedFiles) = await loadAdvisoriesFromDir(dataDir1);
     expect(failedFiles, isEmpty);
-    expect(osvs.length, 4);
+    expect(osvs.length, 2);
 
     await updateAdvisories(osvs);
     var list = await securityAdvisoryBackend.listAdvisories();
-    expect(list.length, 4);
+    expect(list.length, 2);
 
-    var adv = await securityAdvisoryBackend.lookupById('GHSA-4xh4-v2pq-jvhm');
+    var adv = await securityAdvisoryBackend.lookupById('GHSA-4rgh-jx4f-qfcq');
     expect(adv, isNotNull);
-    adv = await securityAdvisoryBackend.lookupById('GHSA-9324-jv53-9cc8');
+    adv = await securityAdvisoryBackend.lookupById('GHSA-1234-1234-1234');
     expect(adv, isNotNull);
-    adv = await securityAdvisoryBackend.lookupById('GHSA-4xh4-v2pq-jvhm');
-    expect(adv, isNotNull);
-    adv = await securityAdvisoryBackend.lookupById('GHSA-9f2c-xxfm-32mj');
-    expect(adv, isNotNull);
-    adv = await securityAdvisoryBackend.lookupById('GHSA-jwpw-q68h-r678');
+    adv = await securityAdvisoryBackend.lookupById('GHSA-5678-5678-5678');
     expect(adv, isNull);
 
     final dataDir2 = Directory(path.join(Directory.current.path, 'test',
@@ -39,22 +35,18 @@ void main() {
     final (updatedOsvs, updatedFailedFiles) =
         await loadAdvisoriesFromDir(dataDir2);
     expect(updatedFailedFiles, isEmpty);
-    expect(updatedOsvs.length, 4);
+    expect(updatedOsvs.length, 2);
 
     await updateAdvisories(updatedOsvs);
 
     list = await securityAdvisoryBackend.listAdvisories();
-    expect(list.length, 4);
+    expect(list.length, 2);
 
-    adv = await securityAdvisoryBackend.lookupById('GHSA-4xh4-v2pq-jvhm');
+    adv = await securityAdvisoryBackend.lookupById('GHSA-4rgh-jx4f-qfcq');
     expect(adv, isNotNull);
-    adv = await securityAdvisoryBackend.lookupById('GHSA-9324-jv53-9cc8');
+    adv = await securityAdvisoryBackend.lookupById('GHSA-5678-5678-5678');
     expect(adv, isNotNull);
-    adv = await securityAdvisoryBackend.lookupById('GHSA-4xh4-v2pq-jvhm');
-    expect(adv, isNotNull);
-    adv = await securityAdvisoryBackend.lookupById('GHSA-9f2c-xxfm-32mj');
+    adv = await securityAdvisoryBackend.lookupById('GHSA-1234-1234-1234');
     expect(adv, isNull);
-    adv = await securityAdvisoryBackend.lookupById('GHSA-jwpw-q68h-r678');
-    expect(adv, isNotNull);
   });
 }
