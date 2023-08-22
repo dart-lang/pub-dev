38f8e2b65a3b9a92ab7806b840dece66b47d9ce2
Jonas Finnemann Jensen <jonasfj@google.com>
2023-08-22 13:23:43 +0200

    Revert "Fix Flutter index URL (#6930)" (#6951)
    This reverts commit 953cc244a5ae566dbc94498ef15f5102f0b8c23c.

--- app/lib/search/flutter_sdk_mem_index.dart
+++ app/lib/search/flutter_sdk_mem_index.dart
@@ -81,7 +81,7 @@ Future<SdkMemIndex?> _createFlutterSdkMemIndex() async {
     final content = DartdocIndex.parseJsonText(
       await searchBackend.fetchSdkIndexContentAsString(
         baseUri: index.baseUri,
-        relativePath: 'flutter/index.json',
+        relativePath: 'index.json',
       ),
     );
     await index.addDartdocIndex(content, allowedLibraries: _allowedLibraries);
