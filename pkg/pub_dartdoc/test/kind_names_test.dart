26e8c4da9040e1f0a3a129fc9c8960add02efede
Istvan Soos <istvan.soos@gmail.com>
2023-10-17 15:08:45 +0200

    explain why test is here

--- pkg/pub_dartdoc/test/kind_names_test.dart
+++ pkg/pub_dartdoc/test/kind_names_test.dart
@@ -8,6 +8,10 @@ import 'package:test/test.dart';
 
 void main() {
   test('kind names', () {
+    // The `index.json`-parsing code should not depend on `package:dartdoc` in
+    // the long term (e.g. when we want to call SDK-specific dartdoc and/or
+    // non-customized dartdoc), however, we still need these values.
+    // This test ensures that the mapping is still correct.
     final expected = <int, String>{};
     for (final k in Kind.values) {
       expected[k.index] = k.toString();
