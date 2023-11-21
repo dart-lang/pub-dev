c6348ba0af4d40940aafb9e538bf14361f013b41
István Soós <isoos@users.noreply.github.com>
2023-11-21 15:17:38 +0100

    Remove pkg/pub_dartdoc (#7209)

--- pkg/pub_dartdoc/bin/pub_dartdoc.dart
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-import 'package:pub_dartdoc/pub_dartdoc.dart';
-
-Future<void> main(List<String> arguments) async => await pubDartDoc(arguments);
