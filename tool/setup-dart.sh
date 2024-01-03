167979353bbf640f612dcf802bc526032366c4f1
Istvan Soos <istvan.soos@gmail.com>
2024-01-03 14:00:58 +0100

    Also detect raw/hash/<hash> URLs as version parameters.

--- tool/setup-dart.sh
+++ tool/setup-dart.sh
@@ -8,22 +8,38 @@ then
   exit 1
 fi
 
+# Expected version argument formats:
+# - stable/raw/hash/<hash>
+# - 3.2.5 (stable/release/<version>)
+# - 3.2.5-beta (beta/release/<version>)
+# - 3.2.5-dev (dev/release/<version>)
 if [[ -z "$2" ]];
 then
   echo "Version argument is missing."
   exit 1
 fi
 
-CHANNEL="stable"
-if [[ "$2" == *beta ]]
+# Infer the download URL
+if [[ "$2" == */raw/hash/* ]]
 then
-  CHANNEL="beta"
-elif [[ "$2" == *dev ]]
+  DOWNLOAD_URL="https://storage.googleapis.com/dart-archive/channels/$2/sdk/dartsdk-linux-x64-release.zip"
+elif [[ "$2" == *.* ]]
 then
-  CHANNEL="dev"
-elif [[ "$2" == "latest" ]]
+  CHANNEL="stable"
+  if [[ "$2" == *beta ]]
+  then
+    CHANNEL="beta"
+  elif [[ "$2" == *dev ]]
+  then
+    CHANNEL="dev"
+  fi
+  DOWNLOAD_URL="https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/$2/sdk/dartsdk-linux-x64-release.zip"
+fi
+
+if [[ -z "$DOWNLOAD_URL" ]];
 then
-  CHANNEL="be"
+  echo "Unable to infer download URL."
+  exit 1
 fi
 
 # Create a temporary directory to extract the archive to.
@@ -35,7 +51,7 @@ fi
 
 # Download and extract Dart SDK
 cd "$WORK_DIR"
-curl -sS "https://storage.googleapis.com/dart-archive/channels/$CHANNEL/raw/$2/sdk/dartsdk-linux-x64-release.zip" >dartsdk.zip
+curl -sS "$DOWNLOAD_URL" >dartsdk.zip
 unzip -q dartsdk.zip
 rm -f dartsdk.zip
 
