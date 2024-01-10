683bce15c5675338f11658ea1a0f995d6c92a867
István Soós <isoos@users.noreply.github.com>
2024-01-10 10:55:27 +0100

    Upgrade analysis SDKs. (#7370)

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -24,7 +24,7 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const _acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2024.01.09',
+  '2024.01.10',
   // Fallback runtime versions.
   '2023.12.18',
   '2023.12.08',
@@ -63,8 +63,8 @@ bool shouldGCVersion(String version) =>
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '3.2.0';
 final String toolStableDartSdkVersion = '3.2.3';
-final String toolStableFlutterSdkVersion = '3.16.4';
-final String toolPreviewDartSdkVersion = '3.3.0-174.2.beta';
+final String toolStableFlutterSdkVersion = '3.16.5';
+final String toolPreviewDartSdkVersion = '3.3.0-279.1.beta';
 final String toolPreviewFlutterSdkVersion = '3.18.0-0.2.pre';
 
 final semanticToolStableDartSdkVersion =
