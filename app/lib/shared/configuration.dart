63e4b668b9b7748fa8fd5da80c486138843d5987
István Soós <isoos@users.noreply.github.com>
2024-01-15 15:03:45 +0100

    Disable email notification on package published events. (#7392)

--- app/lib/shared/configuration.dart
+++ app/lib/shared/configuration.dart
@@ -454,6 +454,9 @@ class Configuration {
   late final isProduction = projectId == 'dartlang-pub';
   late final isNotProduction = !isProduction;
   late final isStaging = projectId == 'dartlang-pub-dev';
+
+  /// NOTE: email notification on package published is temporarily disabled.
+  late final isPublishedEmailNotificationEnabled = isNotProduction;
 }
 
 /// Data structure to describe an admin user.
