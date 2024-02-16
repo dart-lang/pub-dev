efcf5451b865f30a965907e5393a1cf133e1a23f
István Soós <isoos@users.noreply.github.com>
2024-02-16 12:34:47 +0100

    Upgrade Flutter analysis SDK. (#7485)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@ Important changes to data models, configuration, and migrations between each
 AppEngine version, listed here to ease deployment and troubleshooting.
 
 ## Next Release (replace with git tag when deployed)
+ * Bumped runtimeVersion to `2024.02.16`.
+ * Upgraded stable Flutter analysis SDK to `3.19.0`.
+ * Upgraded preview Flutter analysis SDK to `3.20.0-1.1.pre`.
 
 ## `20240213t125700-all`
  * Bumped runtimeVersion to `2024.02.13`.
--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -24,10 +24,10 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const _acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2024.02.13',
+  '2024.02.16',
   // Fallback runtime versions.
+  '2024.02.13',
   '2024.02.09',
-  '2024.02.05',
 ];
 
 /// Sets the current runtime versions.
@@ -63,9 +63,9 @@ bool shouldGCVersion(String version) =>
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '3.2.0';
 final String toolStableDartSdkVersion = '3.3.0';
-final String toolStableFlutterSdkVersion = '3.16.9';
+final String toolStableFlutterSdkVersion = '3.19.0';
 final String toolPreviewDartSdkVersion = '3.4.0-131.0.dev';
-final String toolPreviewFlutterSdkVersion = '3.19.0-0.4.pre';
+final String toolPreviewFlutterSdkVersion = '3.20.0-1.1.pre';
 
 final semanticToolStableDartSdkVersion =
     Version.parse(toolStableDartSdkVersion);
