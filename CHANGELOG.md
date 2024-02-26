eccf79b81f8b47f1dc682c8c408e865e8eee8e89
Istvan Soos <istvan.soos@gmail.com>
2024-02-26 09:54:31 +0100

    Fix SDK selection and upgrade Flutter SDK

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@ Important changes to data models, configuration, and migrations between each
 AppEngine version, listed here to ease deployment and troubleshooting.
 
 ## Next Release (replace with git tag when deployed)
+ * Bumped runtimeVersion to `2024.02.26`.
+ * Upgraded stable Flutter analysis SDK to `3.19.1`.
  * Note: Analysis SDK directories with separate config homes.
 
 ## `20240223t113900-all`
--- pkg/pub_worker/test/utils_test.dart
+++ pkg/pub_worker/test/utils_test.dart
@@ -1,3 +1,4 @@
+import 'package:pub_semver/pub_semver.dart';
 import 'package:pub_worker/src/utils.dart';
 import 'package:test/test.dart';
 
@@ -17,5 +18,37 @@ void main() {
     expect(stripTrailingSlashes('test'), 'test');
     expect(stripTrailingSlashes('test/'), 'test');
     expect(stripTrailingSlashes('test//'), 'test');
+  });
+
+  test('needs newer SDK', () {
+    expect(
+      needsNewerSdk(sdkVersion: null, constraint: null),
+      false,
+    );
+
+    expect(
+      needsNewerSdk(
+          sdkVersion: null, constraint: VersionConstraint.parse('^3.0.0')),
+      false,
+    );
+
+    expect(
+      needsNewerSdk(sdkVersion: Version.parse('3.0.0'), constraint: null),
+      false,
+    );
+
+    expect(
+      needsNewerSdk(
+          sdkVersion: Version.parse('3.0.0'),
+          constraint: VersionConstraint.parse('^3.0.0')),
+      false,
+    );
+
+    expect(
+      needsNewerSdk(
+          sdkVersion: Version.parse('3.0.0'),
+          constraint: VersionConstraint.parse('^3.0.1')),
+      true,
+    );
   });
 }
