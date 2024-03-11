baed9c86472391b9a582db46d35db74a3cb0e11b
Sarah Zakarias <zarah@google.com>
2024-03-11 11:05:26 +0100

    rebase

--- app/lib/tool/backfill/backfill_new_fields.dart
+++ app/lib/tool/backfill/backfill_new_fields.dart
@@ -16,7 +16,5 @@ final _logger = Logger('backfill_new_fields');
 /// CHANGELOG.md must be updated with the new fields, and the next
 /// release could remove the backfill from here.
 Future<void> backfillNewFields() async {
-  _logger.info('Resyncing all security advisories...');
-  // This will backfill the `pub_display_url` in the `database_specific` field on the `SecurityAdvisory` entity.
-  await syncSecurityAdvisories(resync: true);
+  _logger.info('Nothing to do.');
 }
