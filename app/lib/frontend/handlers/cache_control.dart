539e60af03496b4fd4c67ace7dd28db5fec2c211
Jonas Finnemann Jensen <jonasfj@google.com>
2024-03-27 15:09:43 +0100

    Avoid unmodifable map

--- app/lib/frontend/handlers/cache_control.dart
+++ app/lib/frontend/handlers/cache_control.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:collection';
 import 'dart:io' show HttpDate;
 import 'package:clock/clock.dart';
 import 'package:shelf/shelf.dart' as shelf;
