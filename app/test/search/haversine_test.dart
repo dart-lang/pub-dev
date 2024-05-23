d38af100f06722db4dabf21fea7e63934d22d145
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-05-23 08:36:42 -0400

    spelling: vincenty
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- app/test/search/haversine_test.dart
+++ app/test/search/haversine_test.dart
@@ -153,7 +153,7 @@ var gcd = new GreatCircleDistance.fromDegrees(latitude1: lat1, longitude1: lon1,
 
 print('Distance from location 1 to 2 using the Haversine formula is: ${gcd.haversineDistance()}');
 print('Distance from location 1 to 2 using the Spherical Law of Cosines is: ${gcd.sphericalLawOfCosinesDistance()}');
-print('Distance from location 1 to 2 using the Vicenty`s formula is: ${gcd.vincentyDistance()}');
+print('Distance from location 1 to 2 using the Vincenty`s formula is: ${gcd.vincentyDistance()}');
 ```
 
 Check Wikipedia for detailed description on [Great-circle distance](https://en.wikipedia.org/wiki/Great-circle_distance)
