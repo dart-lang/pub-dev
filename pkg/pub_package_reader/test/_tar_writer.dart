756499cc1985927e75eeee57b25b2bf6282e2a4c
István Soós <isoos@users.noreply.github.com>
2024-06-13 11:26:18 +0200

    Fix test failures (#7808)

--- pkg/pub_package_reader/test/_tar_writer.dart
+++ pkg/pub_package_reader/test/_tar_writer.dart
@@ -17,14 +17,23 @@ Future<void> writeTarGzFile(
   await () async* {
     if (textFiles != null) {
       for (final e in textFiles.entries) {
-        yield TarEntry.data(TarHeader(name: e.key), utf8.encode(e.value));
+        yield TarEntry.data(
+            TarHeader(
+              name: e.key,
+              mode: 420, // 644₈
+            ),
+            utf8.encode(e.value));
       }
     }
     if (symlinks != null) {
       for (final e in symlinks.entries) {
         yield TarEntry.data(
             TarHeader(
-                name: e.key, linkName: e.value, typeFlag: TypeFlag.symlink),
+              name: e.key,
+              linkName: e.value,
+              typeFlag: TypeFlag.symlink,
+              mode: 420, // 644₈
+            ),
             Uint8List(0));
       }
     }
