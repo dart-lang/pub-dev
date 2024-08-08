f023ec48089ce2114724e34c9401239ec7a64211
István Soós <isoos@users.noreply.github.com>
2024-08-08 10:01:09 +0200

    Dark theme: early initialization and OS/browser-level setting detection. (#7911)

--- pkg/web_app/lib/script.dart
+++ pkg/web_app/lib/script.dart
@@ -26,19 +26,31 @@ void main() {
   window.onPageShow.listen((_) {
     adjustQueryTextAfterPageShow();
   });
+  _setupDarkTheme();
+}
 
-  // For now we are reusing dartdoc's theme switcher and local storage-based
-  // setting to replace the body-level theme classname.
-  window.onLoad.listen((_) {
-    final classes = document.body?.classes;
-    if (classes != null && classes.contains('-experimental-dark-mode')) {
-      final colorTheme = window.localStorage['colorTheme'];
-      if (colorTheme == 'true') {
-        classes.remove('light-theme');
-        classes.add('dark-theme');
-      }
+void _setupDarkTheme() {
+  // Only trigger dark mode when the experiment is enabled on the server.
+  final classes = document.body?.classes;
+  if (classes != null && classes.contains('-experimental-dark-mode')) {
+    // Detects OS or browser-level theme preference by using media queries.
+    bool mediaPrefersDarkScheme = false;
+    try {
+      mediaPrefersDarkScheme =
+          window.matchMedia('(prefers-color-scheme: dark)').matches;
+    } catch (_) {
+      // ignore errors e.g. when media query matching is not supported
     }
-  });
+
+    // Detects whether the dartdoc control was set to use dark theme
+    final dartdocDarkThemeIsSet = window.localStorage['colorTheme'] == 'true';
+
+    // Switch the top-level style marker to use dark theme instead of the light theme default.
+    if (mediaPrefersDarkScheme || dartdocDarkThemeIsSet) {
+      classes.remove('light-theme');
+      classes.add('dark-theme');
+    }
+  }
 }
 
 void _setupAllEvents() {
