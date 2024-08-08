5f160ee0f9d9e0f09b99d2dbe5770950b485afe8
Sigurd Meldgaard <sigurdm@google.com>
2024-08-08 09:33:29 +0000

    Make build-done message send to inline thread

--- tool/.deploy.sh
+++ tool/.deploy.sh
@@ -13,7 +13,7 @@ message() {
     CHAT_TOKEN=$(gcloud --project=dartlang-pub secrets versions access latest --secret=google-chat-pub-dev-hackers-token)
     CHAT_ID="AAAAkQUOtE8"
     THREAD_KEY="${TAG_NAME%-all}"
-    curl -H 'Content-Type: application/json' -X POST "https://chat.googleapis.com/v1/spaces/$CHAT_ID/messages?key=$CHAT_KEY&token=$CHAT_TOKEN&threadKey=$THREAD_KEY" --data "{\"text\": \"$1\"}"
+    curl -H 'Content-Type: application/json' -X POST "https://chat.googleapis.com/v1/spaces/$CHAT_ID/messages?key=$CHAT_KEY&token=$CHAT_TOKEN&threadKey=$THREAD_KEY&messageReplyOption=REPLY_MESSAGE_FALLBACK_TO_NEW_THREAD" --data "{\"text\": \"$1\"}"
   fi
 }
 
