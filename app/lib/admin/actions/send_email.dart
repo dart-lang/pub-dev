ba55c1d68e1ceb9729abe7ba5e4451466d7a7ba8
Istvan Soos <istvan.soos@gmail.com>
2024-08-08 17:37:30 +0200

    remove unused import

--- app/lib/admin/actions/send_email.dart
+++ app/lib/admin/actions/send_email.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:collection/collection.dart';
-
 import '../../account/agent.dart';
 import '../../account/backend.dart';
 import '../../package/backend.dart';
