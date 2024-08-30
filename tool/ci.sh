448df9a8f128f68f605fe7ddcaa92f915468a009
Istvan Soos <istvan.soos@gmail.com>
2024-08-30 13:52:57 +0200

    more shards

--- tool/ci.sh
+++ tool/ci.sh
@@ -136,38 +136,42 @@ for PKG in ${PKGS}; do
         dart test --run-skipped || EXIT_CODE=$?
         ;;
       test_10)
-        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '0~6p'`'
-        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '0~6p'` || EXIT_CODE=$?
+        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '0~7p'`'
+        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '0~7p'` || EXIT_CODE=$?
         ;;
       test_11)
-        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '1~6p'`'
-        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '1~6p'` || EXIT_CODE=$?
+        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '1~7p'`'
+        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '1~7p'` || EXIT_CODE=$?
         ;;
       test_12)
-        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '2~6p'`'
-        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '2~6p'` || EXIT_CODE=$?
+        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '2~7p'`'
+        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '2~7p'` || EXIT_CODE=$?
         ;;
       test_13)
-        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '3~6p'`'
-        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '3~6p'` || EXIT_CODE=$?
+        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '3~7p'`'
+        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '3~7p'` || EXIT_CODE=$?
         ;;
       test_14)
-        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '4~6p'`'
-        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '4~6p'` || EXIT_CODE=$?
+        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '4~7p'`'
+        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '4~7p'` || EXIT_CODE=$?
         ;;
       test_15)
-        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '5~6p'`'
-        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '5~6p'` || EXIT_CODE=$?
+        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '5~7p'`'
+        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '5~7p'` || EXIT_CODE=$?
         ;;
       test_16)
+        echo 'dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '6~7p'`'
+        dart test -j1 --run-skipped `find test -name "*_test\\.dart" | sort | sed -n '6~7p'` || EXIT_CODE=$?
+        ;;
+      test_17)
         echo 'dart test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '0~3p'`'
         dart test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '0~3p'` || EXIT_CODE=$?
         ;;
-      test_17)
+      test_18)
         echo 'dart test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '1~3p'`'
         dart test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '1~3p'` || EXIT_CODE=$?
         ;;
-      test_18)
+      test_19)
         echo 'dart test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '2~3p'`'
         dart test --run-skipped --concurrency=1 `find test -name "*_test\\.dart" | sort | sed -n '2~3p'` || EXIT_CODE=$?
         ;;
