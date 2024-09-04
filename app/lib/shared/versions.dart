24143576d9cf26a55c1804c40a9797e68198b266
István Soós <isoos@users.noreply.github.com>
2024-09-04 10:46:17 +0200

    Upgrade analysis SDK + dartdoc (#8014)

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -24,10 +24,10 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const _acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2024.08.27',
+  '2024.09.04',
   // Fallback runtime versions.
+  '2024.08.27',
   '2024.08.20',
-  '2024.08.09',
 ];
 
 /// Sets the current runtime versions.
@@ -62,7 +62,7 @@ bool shouldGCVersion(String version) =>
 
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '3.5.0';
-final String toolStableDartSdkVersion = '3.5.1';
+final String toolStableDartSdkVersion = '3.5.2';
 final String toolStableFlutterSdkVersion = '3.24.1';
 
 final semanticToolStableDartSdkVersion =
@@ -74,4 +74,4 @@ final semanticToolStableFlutterSdkVersion =
 final String panaVersion = pana.packageVersion;
 
 // keep in-sync with pkg/pub-worker/lib/src/bin/pana_wrapper.dart
-final String dartdocVersion = '8.0.14';
+final String dartdocVersion = '8.1.0';
