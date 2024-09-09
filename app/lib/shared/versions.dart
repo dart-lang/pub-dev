4bd6419f281127978f5ca12dc6e8a9bc878fb968
Parker Lougheed <parlough@gmail.com>
2024-09-09 16:53:41 -0500

    Update to 3.5 language version

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -61,7 +61,7 @@ bool shouldGCVersion(String version) =>
     version.compareTo(gcBeforeRuntimeVersion) < 0;
 
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
-final String runtimeSdkVersion = '3.5.0';
+final String runtimeSdkVersion = '3.5.2';
 final String toolStableDartSdkVersion = '3.5.2';
 final String toolStableFlutterSdkVersion = '3.24.1';
 
