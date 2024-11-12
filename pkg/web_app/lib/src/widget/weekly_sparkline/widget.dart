cf6935888ad853644a395a29ece7f2c3df78eec6
Sarah Zakarias <zarah@google.com>
2024-11-12 14:29:59 +0100

    Format download count number in weekly sparkline tooltip (#8266)

--- pkg/web_app/lib/src/widget/weekly_sparkline/widget.dart
+++ pkg/web_app/lib/src/widget/weekly_sparkline/widget.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:_pub_shared/format/encoding.dart';
+import 'package:_pub_shared/format/number_format.dart';
 import 'package:web/web.dart';
 
 void create(HTMLElement element, Map<String, String> options) {
@@ -145,8 +146,7 @@ void drawChart(Element svg, HTMLDivElement toolTip, HTMLDivElement chartSubText,
     final coords = computeCoordinates(selectedDay.date, selectedDay.downloads);
     sparklineSpot.setAttribute('cy', '${coords.$2}');
     sparklineCursor.setAttribute('transform', 'translate(${coords.$1}, 0)');
-
-    toolTip.text = '${selectedDay.downloads}';
+    toolTip.text = '${formatWithThousandSeperators(selectedDay.downloads)}';
 
     final startDate = selectedDay.date.subtract(Duration(days: 7));
     chartSubText.text =
