48bf87e9ff48f964aaa7af357af4755a383df302
Istvan Soos <istvan.soos@gmail.com>
2024-12-04 17:55:08 +0100

    fix-imports

--- app/test/frontend/handlers/documentation_test.dart
+++ app/test/frontend/handlers/documentation_test.dart
@@ -2,9 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:gcloud/db.dart';
 import 'package:pub_dev/frontend/handlers/documentation.dart';
-import 'package:pub_dev/package/backend.dart';
 import 'package:pub_dev/shared/urls.dart';
 import 'package:test/test.dart';
 
--- app/test/frontend/handlers/package_test.dart
+++ app/test/frontend/handlers/package_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:gcloud/db.dart';
-import 'package:pub_dev/package/models.dart';
 import 'package:pub_dev/tool/test_profile/models.dart';
 import 'package:test/test.dart';
 
