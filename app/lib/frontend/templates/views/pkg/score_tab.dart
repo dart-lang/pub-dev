e153f5a4b5fd08d0cbaaf0d3da4d9fbf68f0db49
Sarah Zakarias <zarah@google.com>
2024-12-10 12:22:17 +0000

    change name

--- app/lib/frontend/templates/views/pkg/score_tab.dart
+++ app/lib/frontend/templates/views/pkg/score_tab.dart
@@ -80,7 +80,7 @@ d.Node scoreTabNode({
         ),
         _reportNode(report),
         if (card.weeklyVersionDownloads != null &&
-            requestContext.experimentalFlags.showDownloadCountsChart)
+            requestContext.experimentalFlags.showDownloadCountsVersionChart)
           _downloadsChart(card.weeklyVersionDownloads!),
         if (toolEnvInfo != null) toolEnvInfo,
       ]),
