37e2ddfdee9a3f30e27d4670d932c751967825ed
Istvan Soos <istvan.soos@gmail.com>
2024-12-30 17:35:35 +0100

    Upgrade to dartdoc 8.3.1

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -24,11 +24,10 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const _acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2024.12.17',
+  '2024.12.30',
   // Fallback runtime versions.
+  '2024.12.17',
   '2024.12.11',
-  '2024.12.09',
-  '2024.12.04',
 ];
 
 /// Sets the current runtime versions.
@@ -75,4 +74,4 @@ final semanticToolStableFlutterSdkVersion =
 final String panaVersion = pana.packageVersion;
 
 // keep in-sync with pkg/pub-worker/lib/src/bin/pana_wrapper.dart
-final String dartdocVersion = '8.3.0';
+final String dartdocVersion = '8.3.1';
