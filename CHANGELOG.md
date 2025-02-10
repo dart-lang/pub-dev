e76fcac09afc5d1cea0f6a78690ab2c53dd5f6bf
István Soós <isoos@users.noreply.github.com>
2025-02-10 12:22:20 +0100

    Upgrade analysis Dart SDK (#8550)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,8 @@ Important changes to data models, configuration, and migrations between each
 AppEngine version, listed here to ease deployment and troubleshooting.
 
 ## Next Release (replace with git tag when deployed)
- * Bump runtimeVersion to `2025.02.07`.
+ * Bump runtimeVersion to `2025.02.10`.
+ * Upgraded stable Dart analysis SDK to `3.7.0`
  * Upgraded stable Flutter analysis SDK to `3.27.4`.
 
 ## `20250206t125800-all`
--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -24,7 +24,7 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const _acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2025.02.07',
+  '2025.02.10',
   // Fallback runtime versions.
   '2025.01.31',
   '2025.01.24',
@@ -62,7 +62,7 @@ bool shouldGCVersion(String version) =>
 
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
 final String runtimeSdkVersion = '3.6.0';
-final String toolStableDartSdkVersion = '3.6.2';
+final String toolStableDartSdkVersion = '3.7.0';
 final String toolStableFlutterSdkVersion = '3.27.4';
 
 final semanticToolStableDartSdkVersion =
