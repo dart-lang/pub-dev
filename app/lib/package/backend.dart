ae720b294d4014b45d25c4667ccd59950bd331d0
Istvan Soos <istvan.soos@gmail.com>
2025-05-01 15:44:53 +0200

    Check the saved file length + md5 hash at upload processing.

--- app/lib/package/backend.dart
+++ app/lib/package/backend.dart
@@ -915,6 +915,19 @@ class PackageBackend {
       _logger.info('Examining tarball content ($guid).');
       final sw = Stopwatch()..start();
       final file = File(filename);
+      final fileLength = await file.length();
+      if (fileLength != info.length) {
+        _logger.warning(
+            'Saved file length missmatch ($fileLength != ${info.length}).');
+        throw InvalidInputException(
+            'Failed to save uploaded file: length missmatch.');
+      }
+      final md5Hash = (await file.openRead().transform(md5).single).bytes;
+      if (!md5Hash.byteToByteEquals(info.md5Hash)) {
+        _logger.warning('Saved file md5 missmatch.');
+        throw InvalidInputException(
+            'Failed to save uploaded file: md5 missmatch.');
+      }
       final sha256Hash = (await file.openRead().transform(sha256).single).bytes;
       final archive = await summarizePackageArchive(
         filename,
