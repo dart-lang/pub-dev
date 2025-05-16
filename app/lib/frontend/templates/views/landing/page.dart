60290d73f0e4a6b8daba9906f1b790adf2eadf3c
Sarah Zakarias <zarah@google.com>
2025-05-16 15:16:41 +0200

    Remove View all link in Trending section (#8763)

--- app/lib/frontend/templates/views/landing/page.dart
+++ app/lib/frontend/templates/views/landing/page.dart
@@ -48,7 +48,6 @@ d.Node landingPageNode({
         title: 'Trending packages',
         info: d.text('Top trending packages in the last 30 days'),
         content: miniListNode('most-trending', trendingPackages!),
-        viewAllUrl: urls.listingByTrending(),
         viewAllEvent: 'landing-trending-view-all',
         viewAllTitle: 'Search trending packages',
       )
@@ -126,12 +125,12 @@ d.Node _block({
   required String title,
   required d.Node info,
   required d.Node content,
-  required String viewAllUrl,
+  String? viewAllUrl,
   required String viewAllEvent,
   String viewAllLabel = 'View all',
   required String viewAllTitle,
 }) {
-  final isExternalUrl = !viewAllUrl.startsWith('/');
+  final isExternalUrl = viewAllUrl != null && !viewAllUrl.startsWith('/');
   return d.div(
     classes: ['home-block', 'home-block-$shortId'],
     children: [
@@ -148,18 +147,19 @@ d.Node _block({
           d.h1(classes: ['home-block-title'], text: title),
           d.p(classes: ['home-block-context-info'], child: info),
           content,
-          d.div(
-            classes: ['home-block-view-all'],
-            child: d.a(
-              classes: ['home-block-view-all-link'],
-              href: viewAllUrl,
-              target: isExternalUrl ? '_blank' : null,
-              rel: isExternalUrl ? 'noopener nofollow' : 'nofollow',
-              text: viewAllLabel,
-              title: viewAllTitle,
-              attributes: {'data-ga-click-event': viewAllEvent},
+          if (viewAllUrl != null)
+            d.div(
+              classes: ['home-block-view-all'],
+              child: d.a(
+                classes: ['home-block-view-all-link'],
+                href: viewAllUrl,
+                target: isExternalUrl ? '_blank' : null,
+                rel: isExternalUrl ? 'noopener nofollow' : 'nofollow',
+                text: viewAllLabel,
+                title: viewAllTitle,
+                attributes: {'data-ga-click-event': viewAllEvent},
+              ),
             ),
-          ),
         ],
       ),
       // image: after content
