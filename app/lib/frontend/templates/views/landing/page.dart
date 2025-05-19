52232e73eadb026ce2868861efddf6fd0a6722c7
Sarah Zakarias <zarah@google.com>
2025-05-19 09:14:19 +0000

    remove import

--- app/lib/frontend/templates/views/landing/page.dart
+++ app/lib/frontend/templates/views/landing/page.dart
@@ -4,7 +4,6 @@
 
 import 'package:_pub_shared/search/search_form.dart';
 import 'package:_pub_shared/search/tags.dart';
-import 'package:pub_dev/frontend/request_context.dart';
 
 import '../../../../package/models.dart';
 import '../../../../service/youtube/backend.dart';
