033788c3747145cfe82ec2020a2709461a3985ec
Istvan Soos <istvan.soos@gmail.com>
2025-05-21 20:52:02 +0200

    Upgrade runtime SDK to 3.8.0

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -61,7 +61,7 @@ bool shouldGCVersion(String version) =>
     version.compareTo(gcBeforeRuntimeVersion) < 0;
 
 // keep in-sync with SDK version in .mono_repo.yml and Dockerfile
-final String runtimeSdkVersion = '3.7.0';
+final String runtimeSdkVersion = '3.8.0';
 final String toolStableDartSdkVersion = '3.8.0';
 final String toolStableFlutterSdkVersion = '3.32.0';
 
