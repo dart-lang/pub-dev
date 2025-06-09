d81d869c398efc7253babaa0c1c2c61867676d99
Istvan Soos <istvan.soos@gmail.com>
2025-06-09 12:18:36 +0200

    Delete all Secret entries.

--- app/lib/tool/backfill/backfill_new_fields.dart
+++ app/lib/tool/backfill/backfill_new_fields.dart
@@ -4,6 +4,7 @@
 
 import 'package:logging/logging.dart';
 import 'package:pub_dev/package/models.dart';
+import 'package:pub_dev/service/secret/models.dart';
 import 'package:pub_dev/shared/datastore.dart';
 
 final _logger = Logger('backfill_new_fields');
@@ -14,6 +15,9 @@ final _logger = Logger('backfill_new_fields');
 /// CHANGELOG.md must be updated with the new fields, and the next
 /// release could remove the backfill from here.
 Future<void> backfillNewFields() async {
+  _logger.info('Deleting Secret entries');
+  await dbService.deleteWithQuery(dbService.query<Secret>());
+
   _logger.info('Backfill admin deleted fields...');
   await for (final e in dbService.query<Package>().run()) {
     if (e.isAdminDeleted == null) {
