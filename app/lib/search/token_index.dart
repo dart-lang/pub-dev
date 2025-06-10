8a8873c4a43b74aea434ab6c540f28b7930df29a
Istvan Soos <istvan.soos@gmail.com>
2025-06-10 10:24:06 +0200

    doc for the two callback methods

--- app/lib/search/token_index.dart
+++ app/lib/search/token_index.dart
@@ -177,6 +177,8 @@ abstract class _AllocationPool<T> {
     _pool.add(item);
   }
 
+  /// Executes [fn] and provides a pool item in the callback.
+  /// The item will be released to the pool after [fn] completes.
   R withPoolItem<R>({
     required R Function(T array) fn,
   }) {
@@ -186,6 +188,9 @@ abstract class _AllocationPool<T> {
     return r;
   }
 
+  /// Executes [fn] and provides a getter function that can be used to
+  /// acquire new pool items while the [fn] is being executed. The
+  /// acquired items will be released back to the pool after [fn] completes.
   R withItemGetter<R>(R Function(T Function() itemFn) fn) {
     List<T>? items;
     T itemFn() {
