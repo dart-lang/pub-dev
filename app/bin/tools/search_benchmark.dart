7abfe996aae2982a6cdef86444c7054f8fb8b952
Istvan Soos <istvan.soos@gmail.com>
2025-06-25 16:25:58 +0200

    Expose memory allocations in search benchmarks.

--- app/bin/tools/search_benchmark.dart
+++ app/bin/tools/search_benchmark.dart
@@ -2,14 +2,21 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+import 'dart:io';
+
 import 'package:_pub_shared/search/search_form.dart';
 import 'package:pub_dev/search/search_service.dart';
 import 'package:pub_dev/search/updater.dart';
 
 /// Loads a search snapshot and executes queries on it, benchmarking their total time to complete.
 Future<void> main(List<String> args) async {
+  print('Started. Current memory: ${ProcessInfo.currentRss ~/ 1024} KiB,  '
+      'max memory: ${ProcessInfo.maxRss ~/ 1024} KiB');
   // Assumes that the first argument is a search snapshot file.
   final index = await loadInMemoryPackageIndexFromFile(args.first);
+  print('Loaded. Current memory: ${ProcessInfo.currentRss ~/ 1024} KiB,  '
+      'max memory: ${ProcessInfo.maxRss ~/ 1024} KiB');
 
   // NOTE: please add more queries to this list, especially if there is a performance bottleneck.
   final queries = [
@@ -34,4 +41,6 @@ Future<void> main(List<String> args) async {
   }
   sw.stop();
   print('${(sw.elapsedMilliseconds / count).toStringAsFixed(2)} ms/request');
+  print('Done. Current memory: ${ProcessInfo.currentRss ~/ 1024} KiB,  '
+      'max memory: ${ProcessInfo.maxRss ~/ 1024} KiB');
 }
