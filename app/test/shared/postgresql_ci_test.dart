7251823de4f9567d629ea6537b760833ba57538e
Istvan Soos <istvan.soos@gmail.com>
2025-10-02 13:10:32 +0200

    Postgres instance on CI.

---
+++ app/test/shared/postgresql_ci_test.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:clock/clock.dart';
+import 'package:postgres/postgres.dart';
+import 'package:pub_dev/shared/env_config.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Postgresql connection on CI', () {
+    test('connects to CI instance', () async {
+      final pubPostgresUrl = envConfig.pubPostgresUrl;
+      if (pubPostgresUrl == null) {
+        markTestSkipped('PUB_POSTGRES_URL was not specified.');
+        return;
+      }
+      try {
+        await Connection.open(
+          Endpoint(host: 'localhost', database: 'postgres'),
+        );
+      } catch (e, st) {
+        print(e);
+        print(st);
+      }
+      final conn = await Connection.openFromUrl(pubPostgresUrl);
+      final rs1 = await conn.execute('SELECT 1;');
+      expect(rs1[0][0], 1);
+
+      final dbName =
+          'pubtemp_${clock.now().millisecondsSinceEpoch.toRadixString(36)}';
+      await conn.execute('CREATE DATABASE $dbName');
+      await conn.execute('DROP DATABASE $dbName');
+    });
+  });
+}
