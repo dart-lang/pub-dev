7bca5c6c28f75c97b10d011ef6239ca49ea44c9b
Istvan Soos <istvan.soos@gmail.com>
2025-10-17 19:18:56 +0200

    Local postgresql with custom dynamic database support for development+CI.

---
+++ app/tool/docker-postgres-timeout-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+set -e
+
+/usr/bin/timeout 6h /usr/local/bin/docker-entrypoint.sh $*
