c739eaec990f81fcf220669b3571398a7754a3c6
Istvan Soos <istvan.soos@gmail.com>
2025-10-21 11:49:40 +0200

    Catch on PgException

--- app/lib/service/services.dart
+++ app/lib/service/services.dart
@@ -14,6 +14,7 @@ import 'package:gcloud/service_scope.dart';
 import 'package:gcloud/storage.dart';
 import 'package:googleapis_auth/auth_io.dart' as auth;
 import 'package:logging/logging.dart';
+import 'package:postgres/postgres.dart';
 import 'package:pub_dev/database/database.dart';
 import 'package:pub_dev/package/api_export/api_exporter.dart';
 import 'package:pub_dev/search/handlers.dart';
@@ -253,7 +254,7 @@ Future<R> _withPubServices<R>(FutureOr<R> Function() fn) async {
 
         try {
           await PrimaryDatabase.tryRegisterInScope();
-        } catch (e, st) {
+        } on PgException catch (e, st) {
           if (envConfig.isRunningInAppengine) {
             // ignore setup issues for now
             _logger.warning('Could not connect to Postgresql database.', e, st);
