5d9b0e0fa84da0c52b3ede176151fd7bdab0e5a5
Jonas Finnemann Jensen <jonasfj@google.com>
2025-11-17 10:42:05 +0100

    Simplify tests

--- app/test/task/clock_control_test.dart
+++ app/test/task/clock_control_test.dart
@@ -8,94 +8,97 @@ import 'package:clock/clock.dart';
 import 'package:pub_dev/task/clock_control.dart';
 import 'package:test/test.dart';
 
+void testWithClockControl(
+  String name,
+  FutureOr<void> Function(ClockController clockControl) fn,
+) {
+  test(name, () async {
+    await withClockControl(fn);
+  });
+}
+
 void main() {
-  group('ClockController', () {
-    test('real + elapsed time', () async {
-      await withClockControl((clockControl) async {
-        var counter = -1;
-        final done = Completer();
-        scheduleMicrotask(() async {
+  testWithClockControl('real + elapsed time', (clockControl) async {
+    var counter = -1;
+    final done = Completer();
+    scheduleMicrotask(() async {
+      counter++;
+      for (var i = 0; i < 3; i++) {
+        // assuming some latency cannot be controlled
+        await Future.delayed(Duration(milliseconds: 10), () {
           counter++;
-          for (var i = 0; i < 3; i++) {
-            // assuming some latency cannot be controlled
-            await Future.delayed(Duration(milliseconds: 10), () {
-              counter++;
-            });
-            // expected to speed time
-            await clock.delayed(Duration(minutes: 1));
-          }
-          counter += 100;
-          done.complete();
         });
-
-        // microtask hasn't started
-        expect(counter, -1);
-
-        // starting microtask but before 10ms timeout happens
-        await Future.delayed(Duration.zero);
-        expect(counter, 0);
-
-        // loop
-        await Future.delayed(Duration(milliseconds: 20));
-        expect(counter, 1);
-        await clockControl.elapse(minutes: 1, seconds: 1);
-
-        // loop
-        await Future.delayed(Duration(milliseconds: 20));
-        expect(counter, 2);
-        await clockControl.elapse(minutes: 1, seconds: 1);
-
-        // loop
-        await Future.delayed(Duration(milliseconds: 20));
-        expect(counter, 3);
-        await clockControl.elapse(minutes: 1, seconds: 1);
-        expect(counter, 103);
-
-        await done.future;
-      });
+        // expected to speed time
+        await clock.delayed(Duration(minutes: 1));
+      }
+      counter += 100;
+      done.complete();
     });
 
-    test('wrapped real + elapsed time', () async {
-      await withClockControl((clockControl) async {
-        var counter = -1;
-        final done = Completer();
-        scheduleMicrotask(() async {
-          counter++;
-          for (var i = 0; i < 3; i++) {
-            await clock.run(() async {
-              // assuming some latency cannot be controlled
-              await Future.delayed(Duration(milliseconds: 10), () {
-                counter++;
-              });
-            });
-            // expected to speed time
-            await clock.delayed(Duration(minutes: 1));
-          }
-          counter += 100;
-          done.complete();
+    // microtask hasn't started
+    expect(counter, -1);
+
+    // starting microtask but before 10ms timeout happens
+    await Future.delayed(Duration.zero);
+    expect(counter, 0);
+
+    // loop
+    await Future.delayed(Duration(milliseconds: 20));
+    expect(counter, 1);
+    await clockControl.elapse(minutes: 1, seconds: 1);
+
+    // loop
+    await Future.delayed(Duration(milliseconds: 20));
+    expect(counter, 2);
+    await clockControl.elapse(minutes: 1, seconds: 1);
+
+    // loop
+    await Future.delayed(Duration(milliseconds: 20));
+    expect(counter, 3);
+    await clockControl.elapse(minutes: 1, seconds: 1);
+    expect(counter, 103);
+
+    await done.future;
+  });
+
+  testWithClockControl('wrapped real + elapsed time', (clockControl) async {
+    var counter = -1;
+    final done = Completer();
+    scheduleMicrotask(() async {
+      counter++;
+      for (var i = 0; i < 3; i++) {
+        await clock.run(() async {
+          // assuming some latency cannot be controlled
+          await Future.delayed(Duration(milliseconds: 10), () {
+            counter++;
+          });
         });
-
-        // microtask hasn't started
-        expect(counter, -1);
-
-        // starting microtask but before 10ms timeout happens
-        await Future.delayed(Duration.zero);
-        expect(counter, 0);
-
-        // loop
-        await clockControl.elapse(minutes: 1, seconds: 1);
-        expect(counter, 1);
-
-        // loop
-        await clockControl.elapse(minutes: 1, seconds: 1);
-        expect(counter, 2);
-
-        // loop
-        await clockControl.elapse(minutes: 1, seconds: 1);
-        expect(counter, 103);
-
-        await done.future;
-      });
+        // expected to speed time
+        await clock.delayed(Duration(minutes: 1));
+      }
+      counter += 100;
+      done.complete();
     });
+
+    // microtask hasn't started
+    expect(counter, -1);
+
+    // starting microtask but before 10ms timeout happens
+    await Future.delayed(Duration.zero);
+    expect(counter, 0);
+
+    // loop
+    await clockControl.elapse(minutes: 1, seconds: 1);
+    expect(counter, 1);
+
+    // loop
+    await clockControl.elapse(minutes: 1, seconds: 1);
+    expect(counter, 2);
+
+    // loop
+    await clockControl.elapse(minutes: 1, seconds: 1);
+    expect(counter, 103);
+
+    await done.future;
   });
 }
