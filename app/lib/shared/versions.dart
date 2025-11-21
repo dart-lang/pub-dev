552978e599ca07e857181a3c1336bfbf4dee1983
Istvan Soos <istvan.soos@gmail.com>
2025-11-21 10:19:09 +0100

    Upgrade analysis and runtime SDKs + dependencies.

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -24,10 +24,10 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const _acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2025.11.18',
+  '2025.11.21',
   // Fallback runtime versions.
+  '2025.11.18',
   '2025.11.13',
-  '2025.10.31',
 ];
 
 /// Sets the current runtime versions.
@@ -61,9 +61,9 @@ bool shouldGCVersion(String version) =>
     version.compareTo(gcBeforeRuntimeVersion) < 0;
 
 // keep in-sync with SDK version in .github/workflows/all-test.yml and Dockerfile.app
-final String runtimeSdkVersion = '3.9.0';
-final String toolStableDartSdkVersion = '3.10.0';
-final String toolStableFlutterSdkVersion = '3.38.1';
+final String runtimeSdkVersion = '3.10.1';
+final String toolStableDartSdkVersion = '3.10.1';
+final String toolStableFlutterSdkVersion = '3.38.2';
 
 final semanticToolStableDartSdkVersion = Version.parse(
   toolStableDartSdkVersion,
