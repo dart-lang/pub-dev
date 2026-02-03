397fcbdaab3c43b9952f70f5c68564dea6a5201d
Istvan Soos <istvan.soos@gmail.com>
2026-02-03 12:53:25 +0100

    Upgrade pana to 0.23.9 + enable more sandboxed subprocess.

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -24,10 +24,10 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const _acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2026.01.30',
+  '2026.02.03',
   // Fallback runtime versions.
+  '2026.01.30',
   '2026.01.20',
-  '2026.01.13',
 ];
 
 /// Sets the current runtime versions.
