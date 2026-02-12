ae54e992741cca6c509001885a0d0423b110ae8f
István Soós <isoos@users.noreply.github.com>
2026-02-12 13:56:24 +0100

    Upgrade Flutter analysis SDK (#9217)

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -24,7 +24,7 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const _acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2026.02.11',
+  '2026.02.12',
   // Fallback runtime versions.
   '2026.02.10',
   '2026.02.04',
@@ -63,7 +63,7 @@ bool shouldGCVersion(String version) =>
 // keep in-sync with SDK version in .github/workflows/all-test.yml and Dockerfile.app
 final String runtimeSdkVersion = '3.10.1';
 final String toolStableDartSdkVersion = '3.11.0';
-final String toolStableFlutterSdkVersion = '3.38.9';
+final String toolStableFlutterSdkVersion = '3.41.0';
 
 final semanticToolStableDartSdkVersion = Version.parse(
   toolStableDartSdkVersion,
