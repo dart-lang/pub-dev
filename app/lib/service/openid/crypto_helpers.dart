4b387ac6d1f0cb66129b928178fcb884cdb44fe3
Sigurd Meldgaard <sigurdm@google.com>
2026-05-28 13:04:09 +0200

    Revert "signature_verifier in Dart+webcrypto (#9408)" (#9413)
    This reverts commit 2b9a4b8f979e84be33cc84c37da9bafb904e147d.

--- app/lib/service/openid/crypto_helpers.dart
+++ app/lib/service/openid/crypto_helpers.dart
@@ -63,7 +63,7 @@ Future<bool> verifyTextWithRsaSignature({
     await publicKeyFile.writeAsString(publicKey.asPemString);
     final pr = await runConstrained([
       signatureVerifierPath ??
-          './dist/bin/signature_verifier', // This implements RSA signature verification - we use this instead of openssl!
+          './signature_verifier', // This implements RSA signature verification - we use this instead of openssl!
       'dgst',
       '-sha256',
       '-verify',
