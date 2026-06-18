62fe75a4b7faebfc9e2c6e23bac9ec378a9806b2
István Soós <isoos@users.noreply.github.com>
2026-06-18 10:35:17 +0200

    Migrate material checkbox to plain HTML. (#9438)

--- pkg/web_app/lib/script.dart
+++ pkg/web_app/lib/script.dart
@@ -14,6 +14,7 @@ import 'src/mobile_nav.dart';
 import 'src/page_updater.dart';
 import 'src/screenshot_carousel.dart';
 import 'src/search.dart';
+import 'src/widget/core/checkbox.dart';
 import 'src/widget/core/text_area.dart';
 import 'src/widget/widget.dart' show setupWidgets;
 
@@ -40,6 +41,7 @@ void _setupAllEvents() {
   setupLikesList();
   setupScreenshotCarousel();
   setupWidgets();
+  setupCheckboxes();
   setupTextAreas();
   _setupDarkThemeButton();
 }
