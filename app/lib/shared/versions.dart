05625e4661eb33cf33443027fb07f2fe9c07c6a4
István Soós <isoos@users.noreply.github.com>
2026-06-25 15:30:17 +0200

    Upgrade pana (#9453)

--- app/lib/shared/versions.dart
+++ app/lib/shared/versions.dart
@@ -24,7 +24,7 @@ final RegExp runtimeVersionPattern = RegExp(r'^\d{4}\.\d{2}\.\d{2}$');
 /// when the version switch happens.
 const _acceptedRuntimeVersions = <String>[
   // The current [runtimeVersion].
-  '2026.06.23',
+  '2026.06.25',
   // Fallback runtime versions.
   '2026.06.18',
   '2026.06.03',
