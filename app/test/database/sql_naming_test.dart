b1c5e583f989c660623b2e5983ae5c76e7b3e469
Istvan Soos <istvan.soos@gmail.com>
2026-08-10 12:13:05 +0200

    Naming policy test: SQL timestamps should end with _at.

---
+++ app/test/database/sql_naming_test.dart
@@ -0,0 +1,55 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+import 'package:test/test.dart';
+
+/// Columns that don't follow the `_at` suffix convention for `timestamptz`
+/// columns, but can't be renamed because the migration that created them has
+/// already been applied, or they were exempted from the rule.
+const _exemptedTimestampColumns = {
+  '000001_task.sql': {'last_dependency_changed', 'finished'},
+  '000003_user_session.sql': {'created', 'expires'},
+};
+
+final _timestamptzColumnPattern = RegExp(r'"(\w+)"\s+timestamptz');
+
+void main() {
+  final migrationsDir = Directory(
+    path.join(Directory.current.path, 'migrations'),
+  );
+
+  final sqlFiles =
+      migrationsDir
+          .listSync()
+          .whereType<File>()
+          .where((f) => f.path.endsWith('.sql'))
+          .toList()
+        ..sort((a, b) => a.path.compareTo(b.path));
+
+  test('found migration files', () {
+    expect(sqlFiles, isNotEmpty);
+  });
+
+  test('timestamptz columns end with `_at`', () {
+    for (final file in sqlFiles) {
+      final basename = path.basename(file.path);
+      final excluded = _exemptedTimestampColumns[basename] ?? const {};
+      for (final line in file.readAsLinesSync()) {
+        final match = _timestamptzColumnPattern.firstMatch(line);
+        if (match == null) continue;
+        final columnName = match.group(1)!;
+        if (excluded.contains(columnName)) continue;
+        expect(
+          columnName.endsWith('_at'),
+          isTrue,
+          reason:
+              'timestamptz column "$columnName" in $basename should end with `_at`.',
+        );
+      }
+    }
+  });
+}
