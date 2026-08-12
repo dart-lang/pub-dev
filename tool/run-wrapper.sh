ecdfacccffd560ec2c13c0b3b5df1158095a615d
Sigurd Meldgaard <sigurdm@google.com>
2026-08-12 08:31:10 +0200

    Fix exit code interpolation in run-wrapper.sh (#9511)

--- tool/run-wrapper.sh
+++ tool/run-wrapper.sh
@@ -11,7 +11,7 @@ if [ "$EXIT_CODE" -eq 0 ]; then
   echo '{"message": "[pub-run-wrapper-exited]", "severity": "NOTICE", "component": "pub-run-wrapper"}' >&2
   sleep 3
 else
-  echo '{"message": "[pub-run-wrapper-failed] exit code $EXIT_CODE", "severity": "CRITICAL", "component": "pub-run-wrapper"}' >&2
+  echo '{"message": "[pub-run-wrapper-failed] exit code '"$EXIT_CODE"'", "severity": "CRITICAL", "component": "pub-run-wrapper"}' >&2
   sleep 10
 fi
 
