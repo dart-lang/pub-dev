1e59ae16aeff3871f9c74e08fb379dc975939282
Sigurd Meldgaard <sigurdm@google.com>
2026-08-14 09:03:47 +0000

    fix: prevent IntegrityChecker out-of-memory timeouts by ensuring streaming backpressure using parallelForEach

--- app/lib/shared/integrity.dart
+++ app/lib/shared/integrity.dart
@@ -13,7 +13,7 @@ import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
 import 'package:meta/meta.dart';
-import 'package:pool/pool.dart';
+import 'parallel_foreach.dart';
 import 'package:pub_dev/database/database.dart';
 import 'package:pub_dev/database/schema.dart';
 import 'package:retry/retry.dart';
@@ -88,34 +88,32 @@ class _BaseIntegrityChecker {
     Duration? timeLimit,
   }) async* {
     timeLimit ??= const Duration(minutes: 15);
-    final pool = Pool(_concurrency);
-    final futures = <Future<List<String>>>[];
-    try {
-      await for (final item in streamFn()) {
-        final taskFuture = pool.withResource(() async {
-          final f = itemFn(item).toList();
-          try {
-            return await f.timeout(timeLimit!);
-          } on TimeoutException catch (e, st) {
-            _logger.pubNoticeShout(
-              'integrity-check-timeout',
-              'Integrity check operation timed out.',
-              e,
-              st,
-            );
-            rethrow;
+    final controller = StreamController<String>();
+
+    unawaited(
+      streamFn().parallelForEach(_concurrency, (item) async {
+        try {
+          final list = await itemFn(item).toList().timeout(timeLimit!);
+          for (final e in list) {
+            controller.add(e);
           }
-        });
-        futures.add(taskFuture);
-      }
-      for (final f in futures) {
-        for (final item in await f) {
-          yield item;
+        } on TimeoutException catch (e, st) {
+          _logger.pubNoticeShout(
+            'integrity-check-timeout',
+            'Integrity check operation timed out.',
+            e,
+            st,
+          );
+          controller.addError(e, st);
+        } catch (e, st) {
+          controller.addError(e, st);
         }
-      }
-    } finally {
-      await pool.close();
-    }
+      })
+      .catchError(controller.addError)
+      .whenComplete(controller.close),
+    );
+
+    yield* controller.stream;
   }
 
   Stream<String> _queryWithPool<R extends Model>(
